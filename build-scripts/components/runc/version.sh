3731dac85aece3e6323f40603a16bff64ce5e621
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2023-11-16 18:31:38 +0200

    document runc version

--- build-scripts/components/runc/version.sh
+++ build-scripts/components/runc/version.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Containerd version is defined in ../containerd/version.sh
+# Match with version of github.com/opencontainers/runc dependency in https://github.com/containerd/containerd/blob/$containerd_version/go.mod
 echo "v1.1.5"
