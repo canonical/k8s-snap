c1f67af30e4f8a10bdcbba30245044af66439b7b
Benjamin Schimke <benjamin.schimke@canonical.com>
2023-12-07 13:36:01 +0100

    Add API v1 definitions for clustering
    Adds the req/resp type definition for cluster related endpoints.

---
+++ src/k8s/api/v1/cluster.go
@@ -0,0 +1,35 @@
+package v1
+
+// GetClusterStatusRequest is used to request the current status of the cluster.
+type GetClusterStatusRequest struct{}
+
+// GetClusterStatusResponse is the response for "GET 1.0/k8sd/cluster".
+type GetClusterStatusResponse struct {
+	ClusterStatus ClusterStatus `json:"status"`
+}
+
+// ClusterMember holds information about a node in the k8s cluster.
+type ClusterMember struct {
+	Name        string `json:"name"`
+	Address     string `json:"address"`
+	Role        string `json:"role"`
+	Fingerprint string `json:"fingerprint"`
+	Status      string `json:"status"`
+}
+
+// ClusterStatus holds information about the cluster, e.g. its current members
+type ClusterStatus struct {
+	Members []ClusterMember `json:"members"`
+	// TODO(KU-145): add components status here
+}
+
+// HaClusterFormed returns true if the cluster is in high-availability mode (more than two voter nodes).
+func (c ClusterStatus) HaClusterFormed() bool {
+	voters := 0
+	for _, member := range c.Members {
+		if member.Role == "voter" {
+			voters++
+		}
+	}
+	return voters > 2
+}
