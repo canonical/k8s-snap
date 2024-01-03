c326c64378c558cc2e974fe75d8af5380aabc10a
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-03 18:27:32 +0200

    Single Kubernetes binary (#42)

--- build-scripts/components/kubernetes/build.sh
+++ build-scripts/components/kubernetes/build.sh
@@ -5,7 +5,11 @@ mkdir -p "${INSTALL}"
 
 export KUBE_GIT_VERSION_FILE="${PWD}/.version.sh"
 
-for app in kubectl kube-apiserver kubelet kube-scheduler kube-proxy kube-controller-manager; do
-  make WHAT="cmd/${app}" KUBE_STATIC_OVERRIDES="${app}"
+for app in kubernetes; do
+  make WHAT="cmd/${app}" KUBE_STATIC_OVERRIDES="${app}" GOFLAGS="-tags=providerless"
   cp _output/bin/"${app}" "${INSTALL}/${app}"
+done
+
+for app in kubectl kubelet kube-proxy kube-controller-manager kube-scheduler kube-apiserver; do
+  ln -sf ./kubernetes "${INSTALL}/${app}"
 done
