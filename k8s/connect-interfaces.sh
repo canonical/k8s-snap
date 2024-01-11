b081d374b71016cd65f9fdefc557d9a08dd592e8
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-11 11:50:56 +0200

    cleanup snap interfaces connection
    - remove unused account-control interface (we do not edit group memberships)
    - tidy up connect-interfaces.sh script

--- k8s/connect-interfaces.sh
+++ k8s/connect-interfaces.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 
-set -u
+set -ue
 
 if [ "$EUID" -ne 0 ]
 then echo "Please run this script as root."
   exit 1
 fi
 
-for i in account-control \
-         docker-privileged \
-         kubernetes-support \
-         k8s-journald \
-         k8s-kubelet \
-         k8s-kubeproxy \
-         network \
-         network-bind \
-         network-control \
-         network-observe \
-         firewall-control \
-         process-control \
-         kernel-module-observe \
-         mount-observe \
-         hardware-observe \
-         system-observe \
-         home \
-         opengl \
-         home-read-all \
-         login-session-observe \
-         log-observe
-do
-  snap connect k8s:$i
+declare -a INTERFACES=(
+  docker-privileged
+  kubernetes-support
+  network
+  network-bind
+  network-control
+  network-observe
+  firewall-control
+  process-control
+  kernel-module-observe
+  mount-observe
+  hardware-observe
+  system-observe
+  home
+  opengl
+  home-read-all
+  login-session-observe
+  log-observe
+)
+
+for if in "${INTERFACES[@]}"; do
+  bash -x -c "snap connect 'k8s:${if}'"
 done
