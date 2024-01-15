da896868e45216e7db8ef3a975141780e2954961
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-01-15 13:05:47 +0100

    fixup! Add k8s kubectl REST endpoint and CLI command

--- src/k8s/pkg/utils/sys.go
+++ src/k8s/pkg/utils/sys.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 )
@@ -11,19 +10,13 @@ import (
 // runCommand executes a command with a given context.
 // runCommand returns nil if the command completes successfully and the exit code is 0.
 func RunCommand(ctx context.Context, command ...string) error {
-	return RunCommandWithCustomWriter(ctx, os.Stdout, os.Stderr, command...)
-}
-
-// RunCommandWithCustomWriter executes a command with a given context and allows to set a custom writer
-// RunCommandWithCustomWriter returns nil if the command completes successfully and the exit code is 0.
-func RunCommandWithCustomWriter(ctx context.Context, stdout io.Writer, stderr io.Writer, command ...string) error {
 	var args []string
 	if len(command) > 1 {
 		args = command[1:]
 	}
 	cmd := exec.CommandContext(ctx, command[0], args...)
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("command %v failed with exit code %d: %w", command, cmd.ProcessState.ExitCode(), err)
 	}
