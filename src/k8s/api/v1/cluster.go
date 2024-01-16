3e55983ac1c84d1f47763dd7a2335146b829f6f5
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-01-16 14:53:08 +0100

    Add k8s client and ClusterReady check
    This commit introduces a client to query the k8s cluster.
    It wraps the official go-k8s client library and introduces the
    `ClusterReady` function.

--- src/k8s/api/v1/cluster.go
+++ src/k8s/api/v1/cluster.go
@@ -45,17 +45,19 @@ type ExtraServiceArgs map[string]map[string]string
 
 // ClusterMember holds information about a node in the k8s cluster.
 type ClusterMember struct {
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	Role        string `json:"role"`
-	Fingerprint string `json:"fingerprint"`
-	Status      string `json:"status"`
+	Name        string `mapstructure:"name,omitempty"`
+	Address     string `mapstructure:"address,omitempty"`
+	Role        string `mapstructure:"role,omitempty"`
+	Fingerprint string `mapstructure:"fingerprint,omitempty"`
+	Status      string `mapstructure:"status,omitempty"`
 }
 
 // ClusterStatus holds information about the cluster, e.g. its current members
 type ClusterStatus struct {
-	Members    []ClusterMember `json:"members"`
-	Components []Component     `json:"components"`
+	// Ready is true if at least one node in the cluster is in READY state.
+	Ready      bool            `mapstructure:"ready,omitempty"`
+	Members    []ClusterMember `mapstructure:"members,omitempty"`
+	Components []Component     `mapstructure:"components,omitempty"`
 }
 
 // HaClusterFormed returns true if the cluster is in high-availability mode (more than two voter nodes).
