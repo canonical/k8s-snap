6f64c4e29b2bb8790cb43b295e88ed0023d0f1f8
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-18 15:11:08 +0200

    improve error handling on CLI (#65)

--- src/k8s/cmd/k8s/main.go
+++ src/k8s/cmd/k8s/main.go
@@ -1,14 +1,9 @@
 package k8s
 
-import (
-	"os"
-
-	"github.com/sirupsen/logrus"
-)
+import "os"
 
 func Main() {
-	if err := rootCmd.Execute(); err != nil {
-		logrus.WithError(err).Print("k8s command failed")
+	if rootCmd.Execute() != nil {
 		os.Exit(1)
 	}
 }
