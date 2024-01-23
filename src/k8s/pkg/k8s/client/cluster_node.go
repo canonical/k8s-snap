18e4e01e83e5c82479d17d61e609abecde68695f
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-24 01:17:35 +0200

    remove extraneous endpoints

--- src/k8s/pkg/k8s/client/cluster_node.go
+++ src/k8s/pkg/k8s/client/cluster_node.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"fmt"
 	"time"
-
-	apiv1 "github.com/canonical/k8s/api/v1"
-	"github.com/canonical/lxd/shared/api"
 )
 
-// JoinNode calls "POST 1.0/k8sd/cluster/<node>"
 func (c *Client) JoinNode(ctx context.Context, name string, address string, token string) error {
 	if err := c.m.Ready(30); err != nil {
 		return fmt.Errorf("cluster did not come up in time: %w", err)
@@ -20,19 +16,6 @@ func (c *Client) JoinNode(ctx context.Context, name string, address string, toke
 	return nil
 }
 
-// RemoveNode calls "DELETE 1.0/k8sd/cluster/<node>"
 func (c *Client) RemoveNode(ctx context.Context, name string, force bool) error {
-	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
-	request := apiv1.RemoveNodeRequest{
-		Force: force,
-	}
-	var response apiv1.RemoveNodeResponse
-	err := c.mc.Query(queryCtx, "DELETE", api.NewURL().Path("k8sd", "cluster", name), request, &response)
-	if err != nil {
-		clientURL := c.mc.URL()
-		return fmt.Errorf("failed to query DELETE k8sd/cluster/{name} endpoint on %q: %w", clientURL.String(), err)
-	}
-	return nil
+	return c.mc.DeleteClusterMember(ctx, name, force)
 }
