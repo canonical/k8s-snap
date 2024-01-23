aa03b5886f5ad6787fa2cf989478152314e13b7b
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-24 01:22:44 +0200

    remove unused APIs

--- src/k8s/api/v1/cluster.go
+++ src/k8s/api/v1/cluster.go
@@ -14,12 +14,6 @@ type GetClusterStatusResponse struct {
 	ClusterStatus ClusterStatus `json:"status"`
 }
 
-// InitClusterRequest is used to initialize a k8s cluster.
-type InitClusterRequest struct{}
-
-// InitClusterResponse is the response for "POST 1.0/k8sd/cluster".
-type InitClusterResponse struct{}
-
 // GetKubeConfigRequest is used to ask for the admin kubeconfig
 type GetKubeConfigRequest struct{}
 
@@ -27,27 +21,6 @@ type GetKubeConfigRequest struct{}
 type GetKubeConfigResponse struct {
 	KubeConfig string `json:"kubeconfig"`
 }
-
-// JoinClusterRequest is used to request the configuration to join the k8s cluster.
-type JoinClusterRequest struct {
-	Token string `json:"token"`
-}
-
-// JoinClusterResponse is the response for "POST 1.0/k8sd/cluster/join".
-type JoinClusterResponse struct {
-	// ExtraServiceArgs overwrites the configuration of
-	// kube services on the joining node so that they can connect with
-	// the master node services.
-	// TODO: use named arguments (e.g. ExtraKubeletArgs) instead.
-	ExtraServiceArgs ExtraServiceArgs `json:"extraServiceArgs"`
-}
-
-// ExtraServiceArgs specify k8s service arguments that should be overwritten.
-//
-//	ServiceName:{
-//	 "--argument": "value"
-//	}
-type ExtraServiceArgs map[string]map[string]string
 
 // ClusterMember holds information about a node in the k8s cluster.
 type ClusterMember struct {
