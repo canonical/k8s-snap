815cd0a9dd82c616b85ca56d7dfe4837a8b33500
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-24 10:02:19 +0200

    typo

--- src/k8s/cmd/k8s/k8s_cluster.go
+++ src/k8s/cmd/k8s/k8s_cluster.go
@@ -14,7 +14,7 @@ var (
 func init() {
 	rootCmd.PersistentFlags().StringVar(&clusterCmdOpts.stateDir, "state-dir", path.Join(os.Getenv("SNAP_COMMON"), "/var/lib/k8sd/state"), "Directory with the dqlite datastore")
 
-	// By default, the storage dir is set to a fixed directory in the snap.
+	// By default, the state dir is set to a fixed directory in the snap.
 	// This shouldn't be overwritten by the user.
 	rootCmd.Flags().MarkHidden("state-dir")
 }
