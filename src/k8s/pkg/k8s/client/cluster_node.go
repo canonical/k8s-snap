7238bb1918f56684d81fab3ccf4bf6fdbefdc06f
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-01-26 15:28:47 +0200

    support join worker node

--- src/k8s/pkg/k8s/client/cluster_node.go
+++ src/k8s/pkg/k8s/client/cluster_node.go
@@ -4,14 +4,34 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/canonical/k8s/pkg/k8sd/types"
 )
+
+func (c *Client) joinWorkerNode(ctx context.Context, name, address, token string) error {
+	return c.m.NewCluster(name, address, map[string]string{"workerToken": token}, time.Second*180)
+}
+
+func (c *Client) joinControlPlaneNode(ctx context.Context, name, address, token string) error {
+	return c.m.JoinCluster(name, address, token, nil, time.Second*180)
+}
 
 func (c *Client) JoinNode(ctx context.Context, name string, address string, token string) error {
 	if err := c.m.Ready(30); err != nil {
 		return fmt.Errorf("cluster did not come up in time: %w", err)
 	}
-	if err := c.m.JoinCluster(name, address, token, nil, time.Second*180); err != nil {
-		return fmt.Errorf("failed to join k8sd cluster: %w", err)
+
+	// differentiate between control plane and worker node tokens
+	info := &types.InternalWorkerNodeToken{}
+	if info.Decode(token) == nil {
+		// valid worker node token
+		if err := c.joinWorkerNode(ctx, name, address, token); err != nil {
+			return fmt.Errorf("failed to join k8sd cluster as worker: %w", err)
+		}
+	} else {
+		if err := c.joinControlPlaneNode(ctx, name, address, token); err != nil {
+			return fmt.Errorf("failed to join k8sd cluster as control plane: %w", err)
+		}
 	}
 	return nil
 }
