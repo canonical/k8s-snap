155cd9504549d88b6663787046d2728510fe98b4
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-01 11:09:18 +0200

    Bump containerd to v1.6.28 and runc to v1.1.12

--- build-scripts/components/runc/version.sh
+++ build-scripts/components/runc/version.sh
@@ -2,4 +2,4 @@
 
 # Containerd version is defined in ../containerd/version.sh
 # Match with version of github.com/opencontainers/runc dependency in https://github.com/containerd/containerd/blob/$containerd_version/go.mod
-echo "v1.1.5"
+echo "v1.1.12"
