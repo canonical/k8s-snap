d06f61cbe2b7061b41223241484fdf17864e6a32
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-08 18:16:00 +0200

    fixup kubelet args

--- src/k8s/pkg/k8sd/setup/kubelet.go
+++ src/k8s/pkg/k8sd/setup/kubelet.go
@@ -26,9 +26,15 @@ func Kubelet(snap snap.Snap, hostname string, nodeIP net.IP, clusterDNS string, 
 		"--read-only-port":               "0",
 		"--tls-cipher-suites":            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256",
 		"--serialize-image-pulls":        "false",
-		"--cluster-domain":               clusterDomain,
-		"--cluster-dns":                  clusterDNS,
-		"--cloud-provider":               cloudProvider,
+	}
+	if cloudProvider != "" {
+		args["--cloud-provider"] = cloudProvider
+	}
+	if clusterDNS != "" {
+		args["--cluster-dns"] = clusterDNS
+	}
+	if clusterDomain != "" {
+		args["--cluster-domain"] = clusterDomain
 	}
 	if nodeIP != nil {
 		args["--node-ip"] = nodeIP.String()
