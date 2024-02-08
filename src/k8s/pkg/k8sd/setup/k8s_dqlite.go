5bbcbecbe9277a53ff768f2300b7cd661ac96ae2
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-08 17:20:47 +0200

    fixup k8s-dqlite args

--- src/k8s/pkg/k8sd/setup/k8s_dqlite.go
+++ src/k8s/pkg/k8sd/setup/k8s_dqlite.go
@@ -6,6 +6,7 @@ import (
 	"path"
 
 	"github.com/canonical/k8s/pkg/snap"
+	snaputil "github.com/canonical/k8s/pkg/snap/util"
 	"gopkg.in/yaml.v2"
 )
 
@@ -17,11 +18,18 @@ type k8sDqliteInit struct {
 func K8sDqlite(snap snap.Snap, address string, cluster []string) error {
 	b, err := yaml.Marshal(&k8sDqliteInit{Address: address, Cluster: cluster})
 	if err != nil {
-		return fmt.Errorf("failed to create k8s-dqlite init.yaml file for address=%s cluster=%v: %w", address, cluster, err)
+		return fmt.Errorf("failed to create init.yaml file for address=%s cluster=%v: %w", address, cluster, err)
 	}
 
 	if err := os.WriteFile(path.Join(snap.K8sDqliteStateDir(), "init.yaml"), b, 0600); err != nil {
-		return fmt.Errorf("failed to write k8s-dqlite init.yaml: %w", err)
+		return fmt.Errorf("failed to write init.yaml: %w", err)
+	}
+
+	if _, err := snaputil.UpdateServiceArguments(snap, "containerd", map[string]string{
+		"--storage-dir": snap.K8sDqliteStateDir(),
+		"--listen":      fmt.Sprintf("unix://%s", path.Join(snap.K8sDqliteStateDir(), "k8s-dqlite.sock")),
+	}, nil); err != nil {
+		return fmt.Errorf("failed to write arguments file: %w", err)
 	}
 	return nil
 }
