7c2b4386ff5b4ada47128aa3c0f94b27984ca894
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-12 12:13:59 +0200

    drop unused code

--- src/k8s/cmd/k8sd/k8sd.go
+++ src/k8s/cmd/k8sd/k8sd.go
@@ -22,31 +22,6 @@ var (
 		Use:   "k8sd",
 		Short: "Canonical Kubernetes orchestrator and clustering daemon",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// snap := &mock.Snap{
-			// 	Mock: mock.Mock{
-			// 		Strict:                      false,
-			// 		UID:                         os.Getuid(),
-			// 		GID:                         os.Getgid(),
-			// 		KubernetesConfigDir:         "testdata/etc/kubernetes",
-			// 		KubernetesPKIDir:            "testdata/etc/kubernetes/pki",
-			// 		KubeletRootDir:              "testdata/var/lib/kubelet",
-			// 		CNIConfDir:                  "testdata/etc/cni/net.d",
-			// 		CNIBinDir:                   "testdata/opt/cni/bin",
-			// 		CNIPlugins:                  []string{"plugin1", "plugin2"},
-			// 		CNIPluginsBinary:            "bin/static/k8s",
-			// 		ContainerdConfigDir:         "testdata/etc/containerd",
-			// 		ContainerdExtraConfigDir:    "testdata/etc/containerd/conf.d",
-			// 		ContainerdRegistryConfigDir: "testdata/etc/containerd/hosts.d",
-			// 		ContainerdRootDir:           "testdata/var/lib/containerd",
-			// 		ContainerdSocketDir:         "testdata/run",
-			// 		ContainerdStateDir:          "testdata/containerd-run",
-			// 		K8sdStateDir:                "testdata/var/lib/k8sd/state",
-			// 		K8sDqliteStateDir:           "testdata/var/lib/k8s-dqlite",
-			// 		ServiceArgumentsDir:         "testdata/args",
-			// 		ServiceExtraConfigDir:       "testdata/args/conf.d",
-			// 	},
-			// }
-
 			snap := snap.NewSnap(os.Getenv("SNAP"), os.Getenv("SNAP_COMMON"))
 			app, err := app.New(cmd.Context(), app.Config{
 				Debug:      rootCmdOpts.logDebug,
--- src/k8s/pkg/utils/cert/config.go
+++
@@ -1,8 +0,0 @@
-package cert
-
-const (
-	// The path where kubernetes services certificates will live in.
-	KubePkiPath = "/etc/kubernetes/pki"
-	// The path where K8s Dqlite certificates will live in.
-	K8sDqlitePkiPath = "/var/lib/k8s-dqlite"
-)
