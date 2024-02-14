27f993c62ec40912491ef7feed4ab1ecb244f9cf
Adam Dyess <adam.dyess@canonical.com>
2024-02-14 09:20:46 -0600

    test with 3 cp nodes, to establish quorum -- remove the non-leader

--- tests/e2e/tests/test_clustering.py
+++ tests/e2e/tests/test_clustering.py
@@ -76,21 +76,19 @@ def test_worker_nodes(instances: List[harness.Instance]):
 
 @pytest.mark.node_count(4)
 def test_control_plane_replacement(instances: List[harness.Instance]):
-    cp_1, cp_2, w_1, cp_r = instances
+    cp1, cp2, cp3, cp_r = instances
 
-    # Add a Control-Plane
-    token = add_node(cp_1, cp_2)
-    join_cluster(cp_2, token)
-     # Add a Worker
-    token = add_node(cp_1, w_1, "--worker")
-    join_cluster(w_1, token)
+    # Add a Control-Planes
+    for inst in (cp2, cp3):
+        token = add_node(cp1, inst)
+        join_cluster(inst, token)
 
-    util.wait_until_k8s_ready(cp_1, [cp_1, cp_2, w_1])
+    util.wait_until_k8s_ready(cp1, [cp1, cp2, cp3])
 
-    # Remove a Control-Plane
-    cp_1.delete_instance()
-    util.wait_until_k8s_ready(cp_2, [cp_2, w_1])
+    # Remove leader Control-Plane
+    cp1.delete_instance()
+    util.wait_until_k8s_ready(cp2, [cp2, cp3])
 
-    token = add_node(cp_2, cp_r)
+    token = add_node(cp2, cp_r)
     join_cluster(cp_r, token)
-    util.wait_until_k8s_ready(cp_2, [cp_2, cp_r, w_1])
+    util.wait_until_k8s_ready(cp2, [cp2, cp3, cp_r])
