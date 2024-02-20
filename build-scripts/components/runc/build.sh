86d23504d77ac2f37acffb84a18e4e57c090de38
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-20 13:19:23 +0200

    Disable runc static PIE builds on arm64

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+VERSION="${2}"
+
 export INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
+
+# Ensure `runc --version` prints the right commit hash from upstream
+export COMMIT="$(git describe --long --always "${VERSION}")"
 
 make BUILDTAGS="seccomp apparmor" EXTRA_LDFLAGS="-s -w" static
 cp runc "${INSTALL}/runc"
