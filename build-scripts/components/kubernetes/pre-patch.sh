941067bb8f295bc8e58b7a3c859301e5544d0747
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-02-26 16:25:23 +0200

    Minor QOL fixes (#158)
    * prevent dirty kubernetes version on rebuilds
    
    * make snap hooks executable
    
    * always use helper run() on the e2e test

--- build-scripts/components/kubernetes/pre-patch.sh
+++ build-scripts/components/kubernetes/pre-patch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
-# Ensure clean Kubernetes version
 KUBE_ROOT="${PWD}"
+
+# Remove .version.sh from previous runs (if any)
+[ -f "${KUBE_ROOT}/.version.sh" ] && rm "${KUBE_ROOT}/.version.sh"
+
+# Ensure clean Kubernetes version
 source "${KUBE_ROOT}/hack/lib/version.sh"
 kube::version::get_version_vars
-kube::version::save_version_vars "${PWD}/.version.sh"
+kube::version::save_version_vars "${KUBE_ROOT}/.version.sh"
