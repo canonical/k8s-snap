2029b7a2640c8c41974b4b95637d4cf7e4f60965
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-02-27 16:39:09 +0000

    Change tests to use gomega and add failure test

--- src/k8s/pkg/snap/snap_test.go
+++ src/k8s/pkg/snap/snap_test.go
@@ -2,9 +2,12 @@ package snap
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 	"testing"
+
+	. "github.com/onsi/gomega"
 )
 
 func TestServiceName(t *testing.T) {
@@ -29,39 +32,60 @@ func TestServiceName(t *testing.T) {
 	}
 }
 
-func TestServiceStartStop(t *testing.T) {
-	mockRunner := &MockRunner{}
-	s := NewSnap("testdir", "testdir", "testdir", WithCommandRunner(mockRunner.Run))
+func TestServices(t *testing.T) {
+	t.Run("Start", func(t *testing.T) {
+		g := NewWithT(t)
+		mockRunner := &MockRunner{}
+		snap := NewSnap("testdir", "testdir", WithCommandRunner(mockRunner.Run))
 
-	tests := []struct {
-		name            string
-		action          func(ctx context.Context, service string) error
-		service         string
-		expectedCommand string
-	}{
-		{
-			name:            "StartService",
-			action:          s.StartService,
-			service:         "test-service",
-			expectedCommand: "snapctl start --enable k8s.test-service",
-		},
-		{
-			name:            "StopService",
-			action:          s.StopService,
-			service:         "test-service",
-			expectedCommand: "snapctl stop --disable k8s.test-service",
-		},
-	}
+		err := snap.StartService(context.Background(), "test-service")
+		g.Expect(err).To(BeNil())
+		g.Expect(mockRunner.CalledWithCommand).To(ConsistOf("snapctl start --enable k8s.test-service"))
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			mockRunner.CalledWithCommand = []string{} // Resetting the commands for each test case
-			tc.action(context.Background(), tc.service)
-			if lastCmd := mockRunner.CalledWithCommand[0]; lastCmd != tc.expectedCommand {
-				t.Fatalf("Expected command %q, but %q was called instead for service %s", tc.expectedCommand, lastCmd, tc.service)
-			}
+		t.Run("Fail", func(t *testing.T) {
+			g := NewWithT(t)
+			mockRunner.Err = fmt.Errorf("some error")
+
+			err := snap.StartService(context.Background(), "test-service")
+			g.Expect(err).NotTo(BeNil())
 		})
-	}
+	})
+
+	t.Run("Stop", func(t *testing.T) {
+		g := NewWithT(t)
+		mockRunner := &MockRunner{}
+		snap := NewSnap("testdir", "testdir", WithCommandRunner(mockRunner.Run))
+
+		err := snap.StopService(context.Background(), "test-service")
+		g.Expect(err).To(BeNil())
+		g.Expect(mockRunner.CalledWithCommand).To(ConsistOf("snapctl stop --disable k8s.test-service"))
+
+		t.Run("Fail", func(t *testing.T) {
+			g := NewWithT(t)
+			mockRunner.Err = fmt.Errorf("some error")
+
+			err := snap.StartService(context.Background(), "test-service")
+			g.Expect(err).NotTo(BeNil())
+		})
+	})
+
+	t.Run("Restart", func(t *testing.T) {
+		g := NewWithT(t)
+		mockRunner := &MockRunner{}
+		snap := NewSnap("testdir", "testdir", WithCommandRunner(mockRunner.Run))
+
+		err := snap.RestartService(context.Background(), "test-service")
+		g.Expect(err).To(BeNil())
+		g.Expect(mockRunner.CalledWithCommand).To(ConsistOf("snapctl restart k8s.test-service"))
+
+		t.Run("Fail", func(t *testing.T) {
+			g := NewWithT(t)
+			mockRunner.Err = fmt.Errorf("some error")
+
+			err := snap.StartService(context.Background(), "service")
+			g.Expect(err).NotTo(BeNil())
+		})
+	})
 }
 
 // MockRunner is a mock implementation of CommandRunner.
