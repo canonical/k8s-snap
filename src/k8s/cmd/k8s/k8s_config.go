63e63a633ea302813f526c06f6e3d55de0972ab5
Kevin W Monroe <kevin.monroe@canonical.com>
2024-02-29 01:04:46 -0600

    add k8s config --server arg and restrict on workers

--- src/k8s/cmd/k8s/k8s_config.go
+++ src/k8s/cmd/k8s/k8s_config.go
@@ -2,21 +2,40 @@ package k8s
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/canonical/k8s/cmd/k8s/errors"
+	"github.com/canonical/k8s/pkg/snap"
+	snaputil "github.com/canonical/k8s/pkg/snap/util"
 	"github.com/spf13/cobra"
 )
 
+var (
+	configCmdOpts struct {
+		server string
+	}
+)
+
 func newKubeConfigCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:     "config",
+	configCmd := &cobra.Command{
+		Use:     "config --server <server>",
 		Short:   "Generate a kubeconfig that can be used to access the Kubernetes cluster",
 		Hidden:  true,
 		PreRunE: chainPreRunHooks(hookSetupClient),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			defer errors.Transform(&err, nil)
+			snap := snap.NewSnap(os.Getenv("SNAP"), os.Getenv("SNAP_COMMON"))
 
-			adminConfig, err := k8sdClient.KubeConfig(cmd.Context())
+			isWorker, err := snaputil.IsWorker(snap)
+			if err != nil {
+				return fmt.Errorf("failed to check if node is a worker: %w", err)
+			}
+
+			if isWorker {
+				return fmt.Errorf("this action is restricted on workers")
+			}
+
+			adminConfig, err := k8sdClient.KubeConfig(cmd.Context(), configCmdOpts.server)
 			if err != nil {
 				return fmt.Errorf("failed to get admin config: %w", err)
 			}
@@ -25,4 +44,6 @@ func newKubeConfigCmd() *cobra.Command {
 			return nil
 		},
 	}
+	configCmd.PersistentFlags().StringVar(&configCmdOpts.server, "server", "", "Specify a custom cluster server address for the kubeconfig")
+	return configCmd
 }
