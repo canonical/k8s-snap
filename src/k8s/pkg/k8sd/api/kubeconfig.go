62a4ede5c1af9cdd2c672083bb2b5a17943a7689
Kevin W Monroe <kevin.monroe@canonical.com>
2024-02-29 01:36:04 -0600

    modify kubeconfig server with request param if given; otherwise use hostname

--- src/k8s/pkg/k8sd/api/kubeconfig.go
+++ src/k8s/pkg/k8sd/api/kubeconfig.go
@@ -1,25 +1,39 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 
 	apiv1 "github.com/canonical/k8s/api/v1"
 	"github.com/canonical/lxd/lxd/response"
 	"github.com/canonical/microcluster/state"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func getKubeconfig(s *state.State, r *http.Request) response.Response {
-	// TODO: Render a new kubeconfig instead of reading the existing one
-	//       when the config can be altered via request parameters.
-	config, err := os.ReadFile("/etc/kubernetes/admin.conf")
+	req := apiv1.GetKubeConfigRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return response.BadRequest(fmt.Errorf("failed to parse request: %w", err))
+	}
+	server := req.Server
+	if req.Server == "" {
+		server = fmt.Sprintf("https://%s:6443", s.Address().Hostname())
+	}
+
+	config := clientcmd.GetConfigFromFileOrDie("/etc/kubernetes/admin.conf")
+	if _, ok := config.Clusters["k8s"]; !ok {
+		return response.InternalError(fmt.Errorf("failed to read 'k8s' cluster data from kubeconfig"))
+	}
+	config.Clusters["k8s"].Server = server
+
+	bConfig, err := clientcmd.Write(*config)
 	if err != nil {
-		return response.InternalError(fmt.Errorf("failed to read admin kubeconfig: %w", err))
+		return response.InternalError(fmt.Errorf("failed to serialize modified kubeconfig: %w", err))
 	}
 
 	result := apiv1.GetKubeConfigResponse{
-		KubeConfig: string(config),
+		KubeConfig: string(bConfig),
 	}
 	return response.SyncResponse(true, &result)
 }
