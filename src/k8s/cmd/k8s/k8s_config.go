7776d0cb5e0364387f7372da20226f0fbf3cee38
Kevin W Monroe <kevin.monroe@canonical.com>
2024-02-29 23:33:59 -0600

    remove worker restriction (handled elsewhere)

--- src/k8s/cmd/k8s/k8s_config.go
+++ src/k8s/cmd/k8s/k8s_config.go
@@ -2,11 +2,8 @@ package k8s
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/canonical/k8s/cmd/k8s/errors"
-	"github.com/canonical/k8s/pkg/snap"
-	snaputil "github.com/canonical/k8s/pkg/snap/util"
 	"github.com/spf13/cobra"
 )
 
@@ -24,16 +21,6 @@ func newKubeConfigCmd() *cobra.Command {
 		PreRunE: chainPreRunHooks(hookSetupClient),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			defer errors.Transform(&err, nil)
-			snap := snap.NewSnap(os.Getenv("SNAP"), os.Getenv("SNAP_COMMON"))
-
-			isWorker, err := snaputil.IsWorker(snap)
-			if err != nil {
-				return fmt.Errorf("failed to check if node is a worker: %w", err)
-			}
-
-			if isWorker {
-				return fmt.Errorf("this action is restricted on workers")
-			}
 
 			adminConfig, err := k8sdClient.KubeConfig(cmd.Context(), configCmdOpts.server)
 			if err != nil {
