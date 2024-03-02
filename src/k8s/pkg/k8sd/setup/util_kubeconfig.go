d4daaf069340418a463797e39c73ff660c34441f
Kevin W Monroe <kevin.monroe@canonical.com>
2024-03-02 14:39:54 -0600

    replace kubeconfig template logic with createConfig + WriteToFile

--- src/k8s/pkg/k8sd/setup/util_kubeconfig.go
+++ src/k8s/pkg/k8sd/setup/util_kubeconfig.go
@@ -1,43 +1,18 @@
 package setup
 
 import (
-	"encoding/base64"
 	"fmt"
-	"io"
-	"os"
 
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
-
-var (
-	kubeconfigTemplate = mustTemplate("kubeconfig")
-)
-
-type kubeconfigTemplateConfig struct {
-	CA    string
-	URL   string
-	Token string
-}
-
-// renderKubeconfig writes a kubeconfig to the specified writer.
-func renderKubeconfig(writer io.Writer, token string, url string, caPEM string) error {
-	if err := kubeconfigTemplate.Execute(writer, kubeconfigTemplateConfig{
-		CA:    base64.StdEncoding.EncodeToString([]byte(caPEM)),
-		URL:   url,
-		Token: token,
-	}); err != nil {
-		return fmt.Errorf("failed to render template: %w", err)
-	}
-	return nil
-}
 
 // createConfig generates a Config suitable for our k8s environment.
 func createConfig(token string, url string, caPEM string) *clientcmdapi.Config {
 	config := clientcmdapi.NewConfig()
 	config.Clusters["k8s"] = &clientcmdapi.Cluster{
 		CertificateAuthorityData: []byte(caPEM),
-		Server:                   url,
+		Server:                   fmt.Sprintf("https://%s", url),
 	}
 	config.AuthInfos["k8s-user"] = &clientcmdapi.AuthInfo{
 		Token: token,
@@ -53,12 +28,12 @@ func createConfig(token string, url string, caPEM string) *clientcmdapi.Config {
 
 // Kubeconfig writes a kubeconfig file to disk.
 func Kubeconfig(path string, token string, url string, caPEM string) error {
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	config := createConfig(token, url, caPEM)
+	err := clientcmd.WriteToFile(*config, path)
 	if err != nil {
-		return fmt.Errorf("failed to open file: %w", err)
+		return fmt.Errorf("failed to write kubeconfig: %w", err)
 	}
-	defer file.Close()
-	return renderKubeconfig(file, token, url, caPEM)
+	return nil
 }
 
 // KubeconfigString provides a stringified kubeconfig.
