a2d707e6d84284512cd7324e06565c5febad6ac9
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-03-04 14:33:15 +0000

    Revert "Initial test implementation"
    This reverts commit d453ac371cba38c2f42a5a5cc17d8db8ccfb139e.

--- src/k8s/pkg/k8sd/setup/kubelet.go
+++ src/k8s/pkg/k8sd/setup/kubelet.go
@@ -29,21 +29,6 @@ var kubeletWorkerLabels = []string{
 	"node-role.kubernetes.io/worker=",
 }
 
-// GetKubeletTLSCipherSuites returns the kubelet TLS cipher suites.
-func GetKubeletTLSCipherSuites() []string {
-	return kubeletTLSCipherSuites
-}
-
-// GetKubeletControlPlaneLabels returns the kubelet control plane labels.
-func GetKubeletControlPlaneLabels() []string {
-	return kubeletControlPlaneLabels
-}
-
-// GetKubeletWorkerLabels returns the kubelet worker labels.
-func GetKubeletWorkerLabels() []string {
-	return kubeletWorkerLabels
-}
-
 // KubeletControlPlane configures kubelet on a control plane node.
 func KubeletControlPlane(snap snap.Snap, hostname string, nodeIP net.IP, clusterDNS string, clusterDomain string, cloudProvider string) error {
 	return kubelet(snap, hostname, nodeIP, clusterDNS, clusterDomain, cloudProvider, nil, append(kubeletControlPlaneLabels, kubeletWorkerLabels...))
@@ -67,7 +52,7 @@ func kubelet(snap snap.Snap, hostname string, nodeIP net.IP, clusterDNS string, 
 		"--fail-swap-on":                 "false",
 		"--hostname-override":            hostname,
 		"--kubeconfig":                   path.Join(snap.KubernetesConfigDir(), "kubelet.conf"),
-		"--node-labels":                  strings.Join(append(kubeletControlPlaneLabels, kubeletWorkerLabels...), ","),
+		"--node-labels":                  strings.Join(labels, ","),
 		"--read-only-port":               "0",
 		"--register-with-taints":         strings.Join(taints, ","),
 		"--root-dir":                     snap.KubeletRootDir(),
@@ -89,6 +74,5 @@ func kubelet(snap snap.Snap, hostname string, nodeIP net.IP, clusterDNS string, 
 	if _, err := snaputil.UpdateServiceArguments(snap, "kubelet", args, nil); err != nil {
 		return fmt.Errorf("failed to render arguments file: %w", err)
 	}
-
 	return nil
 }
