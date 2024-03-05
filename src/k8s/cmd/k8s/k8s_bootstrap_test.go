a896623abde9351856b01a8959fd56f5f26c6eba
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-03-05 09:20:55 +0100

    table driven test

--- src/k8s/cmd/k8s/k8s_bootstrap_test.go
+++ src/k8s/cmd/k8s/k8s_bootstrap_test.go
@@ -9,7 +9,17 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var yamlConfigComplete = `
+type testCase struct {
+	name           string
+	yamlConfig     string
+	expectedConfig apiv1.BootstrapConfig
+	expectedError  string
+}
+
+var testCases = []testCase{
+	{
+		name: "CompleteConfig",
+		yamlConfig: `
 components:
   - network
   - dns
@@ -19,12 +29,33 @@ components:
   - metrics-server
 cluster-cidr: "10.244.0.0/16"
 enable-rbac: true
-k8s-dqlite-port: 12379`
-
-var yamlConfigIncomplete = `
+k8s-dqlite-port: 12379`,
+		expectedConfig: apiv1.BootstrapConfig{
+			Components:    []string{"network", "dns", "gateway", "ingress", "storage", "metrics-server"},
+			ClusterCIDR:   "10.244.0.0/16",
+			EnableRBAC:    &[]bool{true}[0],
+			K8sDqlitePort: 12379,
+		},
+	},
+	{
+		name: "IncompleteConfig",
+		yamlConfig: `
 cluster-cidr: "10.244.0.0/16"
 enable-rbac: true
-bananas: 5`
+bananas: 5`,
+		expectedConfig: apiv1.BootstrapConfig{
+			Components:    []string{"dns", "metrics-server", "network"},
+			ClusterCIDR:   "10.244.0.0/16",
+			EnableRBAC:    &[]bool{true}[0],
+			K8sDqlitePort: 9000,
+		},
+	},
+	{
+		name:          "InvalidYaml",
+		yamlConfig:    "this is not valid yaml",
+		expectedError: "failed to parse YAML config file",
+	},
+}
 
 func mustAddConfigToTestDir(t *testing.T, configPath string, data string) {
 	t.Helper()
@@ -36,70 +67,26 @@ func mustAddConfigToTestDir(t *testing.T, configPath string, data string) {
 }
 
 func TestGetConfigYaml(t *testing.T) {
-	t.Run("CompleteConfig", func(t *testing.T) {
-		g := NewWithT(t)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
 
-		tempDir := t.TempDir()
-		configPath := filepath.Join(tempDir, "init.yaml")
+			tempDir := t.TempDir()
+			configPath := filepath.Join(tempDir, "init.yaml")
 
-		// Add the complete config to the test directory
-		mustAddConfigToTestDir(t, configPath, yamlConfigComplete)
+			// Add the test case config to the test directory
+			mustAddConfigToTestDir(t, configPath, tc.yamlConfig)
 
-		// Get the config from the test directory
-		bootstrapConfig, err := getConfigFromYaml(configPath)
-		if err != nil {
-			t.Fatalf("failed to load bootstrap configuration file: %v", err)
-		}
+			// Get the config from the test directory
+			bootstrapConfig, err := getConfigFromYaml(configPath)
 
-		// Check the config
-		expectedConfig := apiv1.BootstrapConfig{
-			Components:    []string{"network", "dns", "gateway", "ingress", "storage", "metrics-server"},
-			ClusterCIDR:   "10.244.0.0/16",
-			EnableRBAC:    &[]bool{true}[0],
-			K8sDqlitePort: 12379,
-		}
-		g.Expect(bootstrapConfig).To(Equal(expectedConfig))
-
-	})
-	t.Run("IncompleteConfig", func(t *testing.T) {
-		// test an incomplete config file, set defaults for unspecified fields
-		g := NewWithT(t)
-
-		tempDir := t.TempDir()
-		configPath := filepath.Join(tempDir, "init.yaml")
-
-		// Add the complete config to the test directory
-		mustAddConfigToTestDir(t, configPath, yamlConfigIncomplete)
-
-		// Get the config from the test directory
-		bootstrapConfig, err := getConfigFromYaml(configPath)
-		if err != nil {
-			t.Fatalf("failed to load bootstrap configuration file: %v", err)
-		}
-
-		// Check the config
-		expectedConfig := apiv1.BootstrapConfig{
-			Components:    []string{"dns", "metrics-server", "network"},
-			ClusterCIDR:   "10.244.0.0/16",
-			EnableRBAC:    &[]bool{true}[0],
-			K8sDqlitePort: 9000,
-		}
-		g.Expect(bootstrapConfig).To(Equal(expectedConfig))
-	})
-
-	t.Run("InvalidYaml", func(t *testing.T) {
-		// test an invalid yaml file
-		g := NewWithT(t)
-
-		tempDir := t.TempDir()
-		configPath := filepath.Join(tempDir, "init.yaml")
-
-		// Add the invalid yaml to the test directory
-		mustAddConfigToTestDir(t, configPath, "this is not valid yaml")
-
-		// Get the config from the test directory
-		_, err := getConfigFromYaml(configPath)
-		g.Expect(err.Error()).To(ContainSubstring("failed to parse YAML config file"))
-	})
-
+			if tc.expectedError != "" {
+				g.Expect(err).To(HaveOccurred())
+				g.Expect(err.Error()).To(ContainSubstring(tc.expectedError))
+			} else {
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(bootstrapConfig).To(Equal(tc.expectedConfig))
+			}
+		})
+	}
 }
