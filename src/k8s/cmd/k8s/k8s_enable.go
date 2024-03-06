f21fbe88c8b2824564a6b460887d47a98b5da572
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-03-06 11:22:58 +0100

    Enable/disable multiple funcs in one CLI call

--- src/k8s/cmd/k8s/k8s_enable.go
+++ src/k8s/cmd/k8s/k8s_enable.go
@@ -17,62 +17,62 @@ var (
 )
 
 type EnableResult struct {
-	Functionality string `json:"functionality" yaml:"functionality"`
+	Functionalities []string `json:"functionalities" yaml:"functionalities"`
 }
 
 func (e EnableResult) String() string {
-	return fmt.Sprintf("%s enabled.\n", e.Functionality)
+	return fmt.Sprintf("%s enabled.\n", strings.Join(e.Functionalities, ", "))
 }
 
 func newEnableCmd() *cobra.Command {
 	enableCmd := &cobra.Command{
-		Use:     "enable <functionality>",
+		Use:     "enable <functionality1> ...",
 		Short:   "Enable core cluster functionalities",
 		Long:    fmt.Sprintf("Enable one of %s.", strings.Join(componentList, ", ")),
 		PreRunE: chainPreRunHooks(hookSetupClient),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			defer errors.Transform(&err, nil)
 
-			if len(args) > 1 {
-				return fmt.Errorf("too many arguments: provide only the name of the functionality that should be enabled")
-			}
 			if len(args) < 1 {
-				return fmt.Errorf("missing argument: provide the name of the functionality that should be enabled")
-			}
-			if !slices.Contains(componentList, args[0]) {
-				return fmt.Errorf("unknown functionality %q; needs to be one of: %s", args[0], strings.Join(componentList, ", "))
+				return fmt.Errorf("missing argument: provide the names of the functionalites that should be enabled")
 			}
 
 			config := api.UserFacingClusterConfig{}
-			functionality := args[0]
-			switch functionality {
-			case "network":
-				config.Network = &api.NetworkConfig{
-					Enabled: vals.Pointer(true),
+			functionalities := args
+			for _, functionality := range functionalities {
+				if !slices.Contains(componentList, functionality) {
+					return fmt.Errorf("unknown functionality %q; needs to be one of: %s", args[0], strings.Join(componentList, ", "))
 				}
-			case "dns":
-				config.DNS = &api.DNSConfig{
-					Enabled: vals.Pointer(true),
-				}
-			case "gateway":
-				config.Gateway = &api.GatewayConfig{
-					Enabled: vals.Pointer(true),
-				}
-			case "ingress":
-				config.Ingress = &api.IngressConfig{
-					Enabled: vals.Pointer(true),
-				}
-			case "local-storage":
-				config.LocalStorage = &api.LocalStorageConfig{
-					Enabled: vals.Pointer(true),
-				}
-			case "load-balancer":
-				config.LoadBalancer = &api.LoadBalancerConfig{
-					Enabled: vals.Pointer(true),
-				}
-			case "metrics-server":
-				config.MetricsServer = &api.MetricsServerConfig{
-					Enabled: vals.Pointer(true),
+
+				switch functionality {
+				case "network":
+					config.Network = &api.NetworkConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "dns":
+					config.DNS = &api.DNSConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "gateway":
+					config.Gateway = &api.GatewayConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "ingress":
+					config.Ingress = &api.IngressConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "local-storage":
+					config.LocalStorage = &api.LocalStorageConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "load-balancer":
+					config.LoadBalancer = &api.LoadBalancerConfig{
+						Enabled: vals.Pointer(true),
+					}
+				case "metrics-server":
+					config.MetricsServer = &api.MetricsServerConfig{
+						Enabled: vals.Pointer(true),
+					}
 				}
 			}
 
@@ -80,7 +80,7 @@ func newEnableCmd() *cobra.Command {
 				Config: config,
 			}
 
-			fmt.Fprintf(cmd.ErrOrStderr(), "Enabling %s. This may take some time, please wait.\n", functionality)
+			fmt.Fprintf(cmd.ErrOrStderr(), "Enabling %s. This may take some time, please wait.\n", strings.Join(functionalities, ", "))
 			if err := k8sdClient.UpdateClusterConfig(cmd.Context(), request); err != nil {
 				return fmt.Errorf("failed to update cluster configuration: %w", err)
 			}
@@ -90,7 +90,7 @@ func newEnableCmd() *cobra.Command {
 				return fmt.Errorf("failed to create formatter: %w", err)
 			}
 			return f.Print(EnableResult{
-				Functionality: functionality,
+				Functionalities: functionalities,
 			})
 
 		},
