2f564b7150c433d3bbee78e8ba166f27052a4809
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-03-12 08:53:55 +0100

    Remove timeout contexts
    The timeout should only be set via CLI. Introducing hard-coded timeouts
    in the code may randomly break.

--- src/k8s/pkg/k8s/client/cluster_config.go
+++ src/k8s/pkg/k8s/client/cluster_config.go
@@ -3,18 +3,14 @@ package client
 import (
 	"context"
 	"fmt"
-	"time"
 
 	api "github.com/canonical/k8s/api/v1"
 	lxdApi "github.com/canonical/lxd/shared/api"
 )
 
 func (c *k8sdClient) UpdateClusterConfig(ctx context.Context, request api.UpdateClusterConfigRequest) error {
-	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
 	var response api.UpdateClusterConfigResponse
-	err := c.Query(queryCtx, "PUT", lxdApi.NewURL().Path("k8sd", "cluster", "config"), request, &response)
+	err := c.Query(ctx, "PUT", lxdApi.NewURL().Path("k8sd", "cluster", "config"), request, &response)
 	if err != nil {
 		return fmt.Errorf("failed to update cluster configuration: %w", err)
 	}
@@ -22,12 +18,9 @@ func (c *k8sdClient) UpdateClusterConfig(ctx context.Context, request api.Update
 }
 
 func (c *k8sdClient) GetClusterConfig(ctx context.Context, request api.GetClusterConfigRequest) (api.UserFacingClusterConfig, error) {
-	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
 	var response api.GetClusterConfigResponse
 
-	err := c.Query(queryCtx, "GET", lxdApi.NewURL().Path("k8sd", "cluster", "config"), nil, &response)
+	err := c.Query(ctx, "GET", lxdApi.NewURL().Path("k8sd", "cluster", "config"), nil, &response)
 	if err != nil {
 		return api.UserFacingClusterConfig{}, err
 	}
--- src/k8s/pkg/k8s/client/kubernetes_auth_tokens.go
+++ src/k8s/pkg/k8s/client/kubernetes_auth_tokens.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	"fmt"
-	"time"
 
 	apiv1 "github.com/canonical/k8s/api/v1"
 	"github.com/canonical/lxd/shared/api"
@@ -11,13 +10,10 @@ import (
 
 // GenerateAuthToken calls "POST 1.0/kubernetes/auth/tokens".
 func (c *k8sdClient) GenerateAuthToken(ctx context.Context, username string, groups []string) (string, error) {
-	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
 	request := apiv1.CreateKubernetesAuthTokenRequest{Username: username, Groups: groups}
 	response := apiv1.CreateKubernetesAuthTokenResponse{}
 
-	err := c.Query(queryCtx, "POST", api.NewURL().Path("kubernetes", "auth", "tokens"), request, &response)
+	err := c.Query(ctx, "POST", api.NewURL().Path("kubernetes", "auth", "tokens"), request, &response)
 	if err != nil {
 		clientURL := c.mc.URL()
 		return "", fmt.Errorf("failed to query endpoint POST /kubernetes/auth/tokens on %q: %w", clientURL.String(), err)
@@ -28,12 +24,9 @@ func (c *k8sdClient) GenerateAuthToken(ctx context.Context, username string, gro
 
 // RevokeAuthToken calls "DELETE 1.0/kubernetes/auth/tokens".
 func (c *k8sdClient) RevokeAuthToken(ctx context.Context, token string) error {
-	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-	defer cancel()
-
 	request := apiv1.RevokeKubernetesAuthTokenRequest{Token: token}
 
-	err := c.Query(queryCtx, "DELETE", api.NewURL().Path("kubernetes", "auth", "tokens"), request, nil)
+	err := c.Query(ctx, "DELETE", api.NewURL().Path("kubernetes", "auth", "tokens"), request, nil)
 	if err != nil {
 		clientURL := c.mc.URL()
 		return fmt.Errorf("failed to query endpoint DELETE /kubernetes/auth/tokens on %q: %w", clientURL.String(), err)
