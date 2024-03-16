f7894820bee82f7269d64cd054ea26f218fc3221
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-03-17 01:08:10 +0200

    update API optional fields

--- src/k8s/api/v1/cluster_config.go
+++ src/k8s/api/v1/cluster_config.go
@@ -20,45 +20,45 @@ type UpdateClusterConfigResponse struct {
 }
 
 type UserFacingClusterConfig struct {
-	Network       *NetworkConfig       `json:"network,omitempty" yaml:"network,omitempty"`
-	DNS           *DNSConfig           `json:"dns,omitempty" yaml:"dns,omitempty"`
-	Ingress       *IngressConfig       `json:"ingress,omitempty" yaml:"ingress,omitempty"`
-	LoadBalancer  *LoadBalancerConfig  `json:"load-balancer,omitempty" yaml:"load-balancer,omitempty"`
-	LocalStorage  *LocalStorageConfig  `json:"local-storage,omitempty" yaml:"local-storage,omitempty"`
-	Gateway       *GatewayConfig       `json:"gateway,omitempty" yaml:"gateway,omitempty"`
-	MetricsServer *MetricsServerConfig `json:"metrics-server,omitempty" yaml:"metrics-server,omitempty"`
+	Network       NetworkConfig       `json:"network,omitempty" yaml:"network,omitempty"`
+	DNS           DNSConfig           `json:"dns,omitempty" yaml:"dns,omitempty"`
+	Ingress       IngressConfig       `json:"ingress,omitempty" yaml:"ingress,omitempty"`
+	LoadBalancer  LoadBalancerConfig  `json:"load-balancer,omitempty" yaml:"load-balancer,omitempty"`
+	LocalStorage  LocalStorageConfig  `json:"local-storage,omitempty" yaml:"local-storage,omitempty"`
+	Gateway       GatewayConfig       `json:"gateway,omitempty" yaml:"gateway,omitempty"`
+	MetricsServer MetricsServerConfig `json:"metrics-server,omitempty" yaml:"metrics-server,omitempty"`
 }
 
 type DNSConfig struct {
-	Enabled             *bool    `json:"enabled,omitempty" yaml:"enabled"`
-	ClusterDomain       string   `json:"cluster-domain,omitempty" yaml:"cluster-domain"`
-	ServiceIP           string   `json:"service-ip,omitempty" yaml:"service-ip"`
-	UpstreamNameservers []string `json:"upstream-nameservers,omitempty" yaml:"upstream-nameservers"`
+	Enabled             *bool     `json:"enabled,omitempty" yaml:"enabled"`
+	ClusterDomain       *string   `json:"cluster-domain,omitempty" yaml:"cluster-domain"`
+	ServiceIP           *string   `json:"service-ip,omitempty" yaml:"service-ip"`
+	UpstreamNameservers *[]string `json:"upstream-nameservers,omitempty" yaml:"upstream-nameservers"`
 }
 
 type IngressConfig struct {
-	Enabled             *bool  `json:"enabled,omitempty" yaml:"enabled"`
-	DefaultTLSSecret    string `json:"default-tls-secret,omitempty" yaml:"default-tls-secret"`
-	EnableProxyProtocol *bool  `json:"enable-proxy-protocol,omitempty" yaml:"enable-proxy-protocol"`
+	Enabled             *bool   `json:"enabled,omitempty" yaml:"enabled"`
+	DefaultTLSSecret    *string `json:"default-tls-secret,omitempty" yaml:"default-tls-secret"`
+	EnableProxyProtocol *bool   `json:"enable-proxy-protocol,omitempty" yaml:"enable-proxy-protocol"`
 }
 
 type LoadBalancerConfig struct {
-	Enabled        *bool    `json:"enabled,omitempty" yaml:"enabled"`
-	CIDRs          []string `json:"cidrs,omitempty" yaml:"cidrs"`
-	L2Enabled      *bool    `json:"l2-mode,omitempty" yaml:"l2-mode"`
-	L2Interfaces   []string `json:"l2-interfaces,omitempty" yaml:"l2-interfaces"`
-	BGPEnabled     *bool    `json:"bgp-mode,omitempty" yaml:"bgp-mode"`
-	BGPLocalASN    int      `json:"bgp-local-asn,omitempty" yaml:"bgp-local-asn"`
-	BGPPeerAddress string   `json:"bgp-peer-address,omitempty" yaml:"bgp-peer-address"`
-	BGPPeerASN     int      `json:"bgp-peer-asn,omitempty" yaml:"bgp-peer-asn"`
-	BGPPeerPort    int      `json:"bgp-peer-port,omitempty" yaml:"bgp-peer-port"`
+	Enabled        *bool     `json:"enabled,omitempty" yaml:"enabled"`
+	CIDRs          *[]string `json:"cidrs,omitempty" yaml:"cidrs"`
+	L2Mode         *bool     `json:"l2-mode,omitempty" yaml:"l2-mode"`
+	L2Interfaces   *[]string `json:"l2-interfaces,omitempty" yaml:"l2-interfaces"`
+	BGPMode        *bool     `json:"bgp-mode,omitempty" yaml:"bgp-mode"`
+	BGPLocalASN    *int      `json:"bgp-local-asn,omitempty" yaml:"bgp-local-asn"`
+	BGPPeerAddress *string   `json:"bgp-peer-address,omitempty" yaml:"bgp-peer-address"`
+	BGPPeerASN     *int      `json:"bgp-peer-asn,omitempty" yaml:"bgp-peer-asn"`
+	BGPPeerPort    *int      `json:"bgp-peer-port,omitempty" yaml:"bgp-peer-port"`
 }
 
 type LocalStorageConfig struct {
-	Enabled       *bool  `json:"enabled,omitempty" yaml:"enabled"`
-	LocalPath     string `json:"local-path,omitempty" yaml:"local-path"`
-	ReclaimPolicy string `json:"reclaim-policy,omitempty" yaml:"reclaim-policy"`
-	SetDefault    *bool  `json:"set-default,omitempty" yaml:"set-default"`
+	Enabled       *bool   `json:"enabled,omitempty" yaml:"enabled"`
+	LocalPath     *string `json:"local-path,omitempty" yaml:"local-path"`
+	ReclaimPolicy *string `json:"reclaim-policy,omitempty" yaml:"reclaim-policy"`
+	SetDefault    *bool   `json:"set-default,omitempty" yaml:"set-default"`
 }
 
 type NetworkConfig struct {
