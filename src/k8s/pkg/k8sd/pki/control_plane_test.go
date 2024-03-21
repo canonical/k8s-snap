859a33f74c3a40a671a0c4554939ca17c6c6590c
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-03-21 19:58:10 +0000

    Add tests and move long string into file

--- src/k8s/pkg/k8sd/pki/control_plane_test.go
+++ src/k8s/pkg/k8sd/pki/control_plane_test.go
@@ -1,9 +1,13 @@
 package pki_test
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"net"
 	"testing"
 
 	"github.com/canonical/k8s/pkg/k8sd/pki"
+	data "github.com/canonical/k8s/pkg/k8sd/pki/data"
 	. "github.com/onsi/gomega"
 )
 
@@ -24,31 +28,52 @@ func TestControlPlaneCertificates(t *testing.T) {
 			Hostname: "h1",
 			Years:    10,
 		})
-		c.CACert = `
------BEGIN CERTIFICATE-----
-MIIDtTCCAp2gAwIBAgIQOPOTOjxvIVlC5ev8EzrnITANBgkqhkiG9w0BAQsFADAY
-MRYwFAYDVQQDEw1rdWJlcm5ldGVzLWNhMB4XDTI0MDIwODAyNDYyOVoXDTM0MDIw
-ODAyNDYyOVowGTEXMBUGA1UEAxMOa3ViZS1hcGlzZXJ2ZXIwggEiMA0GCSqGSIb3
-DQEBAQUAA4IBDwAwggEKAoIBAQCum3KkohfK+E4KCpauilnlxm0e6y+jzyOaRCHx
-P/3iLqN5zN+s2SV+GJNNcT3vSVZ1YhcJKWNrs7QxK2qcq9OhHncmp9Vqu5BV9O+e
-ys4bBlf08lHH0//wrAwXy71ueWXN2uWyFg4i2VSirbRxpXGIR751i4qVtutbSOPy
-3Jjf07upq3zAMyvTx1YTZcwduwW2vrU1f48IZOTueS1eOz0YjCkWLueD2uhLLgRA
-mcxq33pwTM9P0MaZGrrM2GeA+1Hyss5WtoEMkR6TPUWQmYcKFEZui9/JpLfbM8yu
-6h6Ta7GeSccjtclHSGp9fge0IXErhYSmLNoQ7JP8fQeg0DpTAgMBAAGjgfkwgfYw
-DgYDVR0PAQH/BAQDAgSwMB0GA1UdJQQWMBQGCCsGAQUFBwMCBggrBgEFBQcDATAM
-BgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFJjD6HMwGRJQMOzNm919/ZaqdcUwMIGV
-BgNVHREEgY0wgYqCCmt1YmVybmV0ZXOCEmt1YmVybmV0ZXMuZGVmYXVsdIIWa3Vi
-ZXJuZXRlcy5kZWZhdWx0LnN2Y4Iea3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVz
-dGVygiRrdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyHBAqYtwGH
-BH8AAAEwDQYJKoZIhvcNAQELBQADggEBADPWn//rPb0SmZ49WhIa6wc39Ryl7eGo
-Q2H+RY9BMah/xge6fLgeTvFe+H6Vol9BVqm5XgD0BuD5yzKYI2aDq8Ikm4EMOxPl
-7Gs9cqWMMF7Iiw+rYJY4vwzm+5kSCg6oxBx8GLYYkDpbFe8UAWKf/9QTghtoBEEw
-JVBDECnQwJU4tb9ANmPbgxmCYLZjx2vmXQRlXpe6QS9nPmMSS9KkJMyLEEpgzIIA
-aSprnA8WIeSaO/5wLMYS1lUWWzegz2LnKuJ5C5Q+XYkwIY/vFH7OSTnmvt+rHwhh
-4Oj+ScJ0RKnGGcXQnctSvMogDoucw7Y2RjxKcJV8fEKV5ZIeTz0U+nE=
------END CERTIFICATE-----`
+
+		c.CACert = data.DUMMY_CA_CERT
 
 		g := NewWithT(t)
 		g.Expect(c.CompleteCertificates()).ToNot(BeNil())
+	})
+
+	t.Run("ExtraSANs", func(t *testing.T) {
+		c := pki.NewControlPlanePKI(pki.ControlPlanePKIOpts{
+			Hostname:          "h1",
+			Years:             10,
+			AllowSelfSignedCA: true,
+			ExtraSANs:         "192.168.2.123,cluster.local",
+		})
+
+		g := NewWithT(t)
+		g.Expect(c.CompleteCertificates()).To(BeNil())
+
+		block, _ := pem.Decode([]byte(c.APIServerCert))
+		g.Expect(block).ToNot(BeNil())
+
+		cert, _ := x509.ParseCertificate(block.Bytes)
+		g.Expect(cert).ToNot(BeNil())
+
+		t.Run("IPAddresses", func(t *testing.T) {
+			g := NewWithT(t)
+			found := false
+			for _, ip := range cert.IPAddresses {
+				if ip.Equal(net.ParseIP("192.168.2.123")) {
+					found = true
+					break
+				}
+			}
+			g.Expect(found).To(BeTrue())
+		})
+
+		t.Run("DNSNames", func(t *testing.T) {
+			g := NewWithT(t)
+			found := false
+			for _, dns := range cert.DNSNames {
+				if dns == "cluster.local" {
+					found = true
+					break
+				}
+			}
+			g.Expect(found).To(BeTrue())
+		})
 	})
 }
