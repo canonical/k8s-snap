1b4f940085ed9c06b1bf6ce15c9ee5feae449bff
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-03-22 16:07:49 +0000

    Update test

--- src/k8s/api/v1/types_test.go
+++ src/k8s/api/v1/types_test.go
@@ -21,6 +21,7 @@ func TestSetDefaults(t *testing.T) {
 		EnableRBAC:    vals.Pointer(true),
 		K8sDqlitePort: 9000,
 		Datastore:     "k8s-dqlite",
+		ExtraSANs:     []string{},
 	}
 
 	g.Expect(b).To(Equal(expected))
