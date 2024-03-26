72364d653ba5e3e427f5bb3e066d7762e2b1abbe
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-03-26 08:32:18 +0100

    fixup! Move kubeconfig test into smoke test

--- tests/e2e/tests/test_smoke.py
+++ tests/e2e/tests/test_smoke.py
@@ -15,7 +15,7 @@ def test_smoke(instances: List[harness.Instance]):
     util.wait_until_k8s_ready(instance, instances)
 
     # Verify the functionality of the k8s config command during the smoke test.
-    # It would be excessive to deploy a cluster solely for this purpose.    
+    # It would be excessive to deploy a cluster solely for this purpose.
     result = instance.exec(
         "k8s config --server 192.168.210.41".split(), capture_output=True
     )
