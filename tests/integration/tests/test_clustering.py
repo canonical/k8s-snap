66922da7b4342098790611af2f2b4fee6a289268
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-03-28 08:44:11 +0100

    Add integration test covering for scaling down 3 node cluster

--- tests/integration/tests/test_clustering.py
+++ tests/integration/tests/test_clustering.py
@@ -28,24 +28,29 @@ def join_cluster(instance, join_token):
     instance.exec(["k8s", "join-cluster", join_token])
 
 
-@pytest.mark.node_count(2)
-def test_clustering(instances: List[harness.Instance]):
+@pytest.mark.node_count(3)
+def test_control_plane_nodes(instances: List[harness.Instance]):
     cluster_node = instances[0]
-    joining_node = instances[1]
+    joining_nodes = instances[1:]
 
-    join_token = get_join_token(cluster_node, joining_node)
-    join_cluster(joining_node, join_token)
+    for joining_node in joining_nodes:
+        join_token = get_join_token(cluster_node, joining_node)
+        join_cluster(joining_node, join_token)
 
     util.wait_until_k8s_ready(cluster_node, instances)
     nodes = util.ready_nodes(cluster_node)
-    assert len(nodes) == 2, "node should have joined cluster"
+    assert len(nodes) == 3, "nodes should have joined cluster"
 
     assert "control-plane" in util.get_local_node_status(cluster_node)
-    assert "control-plane" in util.get_local_node_status(joining_node)
+    for idx, joining_node in enumerate(joining_nodes):
+        assert "control-plane" in util.get_local_node_status(joining_node)
 
-    cluster_node.exec(["k8s", "remove-node", joining_node.id])
-    nodes = util.ready_nodes(cluster_node)
-    assert len(nodes) == 1, "node should have been removed from cluster"
+        cluster_node.exec(["k8s", "remove-node", joining_node.id])
+        nodes = util.ready_nodes(cluster_node)
+        assert (
+            len(nodes) == len(instances) - 1 - idx
+        ), "node should have been removed from cluster"
+
     assert (
         nodes[0]["metadata"]["name"] == cluster_node.id
     ), f"only {cluster_node.id} should be left in cluster"
