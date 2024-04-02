7a9ed49964a26dac8bb105c7d37b278da5cca48a
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2024-04-02 11:02:08 +0300

    Initial work for etcd integration tests

---
+++ tests/integration/tests/test_etcd.py
@@ -0,0 +1,206 @@
+#
+# Copyright 2024 Canonical, Ltd.
+#
+import logging
+import textwrap
+from typing import List
+
+import pytest
+from test_util import harness, util
+
+LOG = logging.getLogger(__name__)
+
+ETCD_URL = "https://github.com/etcd-io/etcd/releases/download"
+ETCD_VERSION = "v3.3.8"
+
+
+@pytest.mark.node_count(2)
+def test_etcd(instances: List[harness.Instance]):
+    instance = instances[0]
+    instance_default_ip = util.get_default_ip(instance)
+
+    openssl_conf = f"""
+    [req]
+    default_bits  = 4096
+    distinguished_name = req_distinguished_name
+    req_extensions = v3_req
+    prompt = no
+
+    [req_distinguished_name]
+    countryName = US
+    stateOrProvinceName = CA
+    localityName = San Francisco
+    organizationName = etcd
+    commonName = etcd-host
+
+    [v3_req]
+    keyUsage = digitalSignature, keyEncipherment, dataEncipherment
+    extendedKeyUsage = serverAuth, clientAuth
+    subjectAltName = @alt_names
+
+    [alt_names]
+    IP.1 = 127.0.0.1
+    IP.2 = {instance_default_ip}
+    DNS.1 = localhost
+    DNS.2 = {instance.id}
+    """
+    instance.exec(
+        ["dd", "of=/tmp/etcd_tls.conf"],
+        input=str.encode(openssl_conf),
+    )
+
+    instance.exec(
+        [
+            "openssl",
+            "req",
+            "-x509",
+            "-noenc",
+            "-newkey",
+            "rsa:4096",
+            "-subj",
+            "/CN=etcdRootCA",
+            "-keyout",
+            "/tmp/ca_key.pem",
+            "-out",
+            "/tmp/ca_cert.pem",
+            "-days",
+            "36500",
+        ]
+    )
+
+    instance.exec(
+        [
+            "openssl",
+            "req",
+            "-noenc",
+            "-newkey",
+            "rsa:4096",
+            "-keyout",
+            "/tmp/etcd_key.pem",
+            "-out",
+            "/tmp/etcd_cert.csr",
+            "-config",
+            "/tmp/etcd_tls.conf",
+        ]
+    )
+
+    instance.exec(
+        [
+            "openssl",
+            "x509",
+            "-req",
+            "-days",
+            "36500",
+            "-in",
+            "/tmp/etcd_cert.csr",
+            "-CA",
+            "/tmp/ca_cert.pem",
+            "-CAkey",
+            "/tmp/ca_key.pem",
+            "-out",
+            "/tmp/etcd_cert.pem",
+            "-copy_extensions",
+            "copy",
+        ]
+    )
+
+    instance.exec(["mkdir", "-p", "/tmp/test-etcd"])
+
+    instance.exec(
+        [
+            "curl",
+            "-L",
+            f"{ETCD_URL}/{ETCD_VERSION}/etcd-{ETCD_VERSION}-linux-amd64.tar.gz",
+            "-o",
+            f"/tmp/etcd-{ETCD_VERSION}-linux-amd64.tar.gz",
+        ]
+    )
+
+    instance.exec(
+        [
+            "tar",
+            "xzvf",
+            f"/tmp/etcd-{ETCD_VERSION}-linux-amd64.tar.gz",
+            "-C",
+            "/tmp/test-etcd",
+            "--strip-components=1",
+        ]
+    )
+
+    etcd_service = f"""
+    [Unit]
+    Description=etcd
+    Documentation=https://github.com/etcd-io/etcd
+    Conflicts=etcd.service
+    Conflicts=etcd2.service
+
+    [Service]
+    Type=notify
+    Restart=always
+    RestartSec=5s
+    LimitNOFILE=40000
+    TimeoutStartSec=0
+
+    ExecStart=/tmp/test-etcd/etcd --name s1 \
+    --data-dir /tmp/etcd/s1 \
+    --listen-client-urls https://{instance_default_ip}:2379 \
+    --advertise-client-urls https://{instance_default_ip}:2379 \
+    --listen-peer-urls https://{instance_default_ip}:2380 \
+    --initial-advertise-peer-urls https://{instance_default_ip}:2380 \
+    --initial-cluster s1=https://{instance_default_ip}:2380 \
+    --initial-cluster-token tkn \
+    --initial-cluster-state new \
+    --client-cert-auth \
+    --trusted-ca-file /tmp/ca_cert.pem \
+    --cert-file /tmp/etcd_cert.pem \
+    --key-file /tmp/etcd_key.pem \
+    --peer-client-cert-auth \
+    --peer-trusted-ca-file /tmp/ca_cert.pem \
+    --peer-cert-file /tmp/etcd_cert.pem \
+    --peer-key-file /tmp/etcd_key.pem
+
+    [Install]
+    WantedBy=multi-user.target
+    """
+    instance.exec(
+        ["dd", "of=/etc/systemd/system/etcd-s1.service"],
+        input=str.encode(etcd_service),
+    )
+
+    instance.exec(["systemctl", "daemon-reload"])
+    instance.exec(["systemctl", "enable", "etcd-s1.service"])
+    instance.exec(["systemctl", "start", "etcd-s1.service"])
+
+    p = instance.exec(["cat", "/tmp/ca_cert.pem"], capture_output=True)
+    ca_cert = p.stdout.decode()
+    p = instance.exec(["cat", "/tmp/etcd_cert.pem"], capture_output=True)
+    etcd_cert = p.stdout.decode()
+    p = instance.exec(["cat", "/tmp/etcd_key.pem"], capture_output=True)
+    etcd_key = p.stdout.decode()
+
+    k8s_instance = instances[1]
+
+    bootstrap_conf = f"""
+datastore: external
+datastore-url: https://{instance_default_ip}:2379
+datastore-ca-crt: |
+{textwrap.indent(ca_cert, "  ")}
+datastore-client-crt: |
+{textwrap.indent(etcd_cert, "  ")}
+datastore-client-key: |
+{textwrap.indent(etcd_key, "  ")}
+    """
+
+    k8s_instance.exec(
+        ["dd", "of=/root/config.yaml"],
+        input=str.encode(bootstrap_conf),
+    )
+
+    k8s_instance.exec(["k8s", "bootstrap", "--config", "/root/config.yaml"])
+    util.wait_for_dns(k8s_instance)
+    util.wait_for_network(k8s_instance)
+
+    p = k8s_instance.exec(
+        ["systemctl", "is-active", "--quiet", "snap.k8s.k8s-dqlite"], check=False
+    )
+    assert p.returncode != 0
--- tests/integration/tests/test_util/util.py
+++ tests/integration/tests/test_util/util.py
@@ -278,3 +278,25 @@ def get_join_token(
 # Join an existing cluster.
 def join_cluster(instance: harness.Instance, join_token: str):
     instance.exec(["k8s", "join-cluster", join_token])
+
+
+def get_default_cidr(instance: harness.Instance, instance_default_ip: str):
+    # ----
+    # 1:  lo    inet 127.0.0.1/8 scope host lo .....
+    # 28: eth0  inet 10.42.254.197/24 metric 100 brd 10.42.254.255 scope global dynamic eth0 ....
+    # ----
+    # Fetching the cidr for the default interface by matching with instance ip from the output
+    p = instance.exec(["ip", "-o", "-f", "inet", "addr", "show"], capture_output=True)
+    out = p.stdout.decode().split(" ")
+    return [i for i in out if instance_default_ip in i][0]
+
+
+def get_default_ip(instance: harness.Instance):
+    # ---
+    # default via 10.42.254.1 dev eth0 proto dhcp src 10.42.254.197 metric 100
+    # ---
+    # Fetching the default IP address from the output, e.g. 10.42.254.197
+    p = instance.exec(
+        ["ip", "-o", "-4", "route", "show", "to", "default"], capture_output=True
+    )
+    return p.stdout.decode().split(" ")[8]
