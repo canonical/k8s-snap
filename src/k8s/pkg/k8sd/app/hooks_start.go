ad5ad44ef81fea1d53364c661a2d9425948dfd7a
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-04-08 13:49:37 +0300

    Control Plane Configuration Controller (#298)
    * implement control plane configuration controller
    
    * use the control plane controller on the microcluster app
    
    * retry on failed worker check

--- src/k8s/pkg/k8sd/app/hooks_start.go
+++ src/k8s/pkg/k8sd/app/hooks_start.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"context"
+
+	"github.com/canonical/k8s/pkg/k8sd/types"
+	"github.com/canonical/k8s/pkg/utils"
 	"github.com/canonical/microcluster/state"
 )
 
@@ -11,6 +15,13 @@ func (a *App) onStart(s *state.State) error {
 	// start node config controller
 	if a.nodeConfigController != nil {
 		go a.nodeConfigController.Run(s.Context)
+	}
+
+	// start control plane config controller
+	if a.controlPlaneConfigController != nil {
+		go a.controlPlaneConfigController.Run(s.Context, func(ctx context.Context) (types.ClusterConfig, error) {
+			return utils.GetClusterConfig(ctx, s)
+		})
 	}
 
 	return nil
