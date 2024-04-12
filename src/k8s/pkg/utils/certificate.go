e91db63a249ea9435fb42e03d11188cd38895750
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2024-04-12 15:10:00 +0200

    Add fingerprint check when worker joins cluster (#319)
    * add fingerprint check worker join cluster
    
    * add comment
    
    * move get remote cert
    
    * create http client from cert
    
    * add fingerprint to token
    
    * get the right cert
    
    * cleanup
    
    * cleanup code
    
    * cleanup again
    
    * update address
    
    * looks like we need a url here
    
    * lets parse it
    
    * try with better tls config
    
    * review comments
    
    * review comments
    
    * rm comment
    
    * deal with RootCA pool
    
    * add a little test
    
    * add edge cases
    
    * more tests

--- src/k8s/pkg/utils/certificate.go
+++ src/k8s/pkg/utils/certificate.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
 )
 
 // SplitIPAndDNSSANs splits a list of SANs into IP and DNS SANs
@@ -25,4 +30,73 @@ func SplitIPAndDNSSANs(extraSANs []string) ([]net.IP, []string) {
 	}
 
 	return ipSANs, dnsSANs
+}
+
+// TLSClientConfig returns a TLS configuration that trusts a remote server
+// The remoteCert is the public key of the server we are connecting to.
+// The rootCAs is the list of trusted CAs, allowing you to pass the clients existing trusted CAs.
+func TLSClientConfigWithTrustedCertificate(remoteCert *x509.Certificate, rootCAs *x509.CertPool) (*tls.Config, error) {
+	config := &tls.Config{}
+	if remoteCert == nil {
+		return nil, fmt.Errorf("invalid remote public key")
+	}
+
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	config.RootCAs = rootCAs
+	remoteCert.IsCA = true
+	config.RootCAs.AddCert(remoteCert)
+
+	// Always use public key DNS name rather than server cert, so that it matches.
+	if len(remoteCert.DNSNames) > 0 {
+		config.ServerName = remoteCert.DNSNames[0]
+	}
+
+	return config, nil
+}
+
+// GetRemoteCertificate retrieves the remote certificate from a given address
+// The address should be in the format of "hostname:port"
+// Returns the remote certificate or an error
+func GetRemoteCertificate(address string) (*x509.Certificate, error) {
+	// validate address
+	_, _, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate the cluster member address: %w", err)
+	}
+
+	url := fmt.Sprintf("https://%s", address)
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	// Connect
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Retrieve the certificate
+	if resp.TLS == nil || len(resp.TLS.PeerCertificates) == 0 {
+		return nil, fmt.Errorf("unable to read remote TLS certificate")
+	}
+
+	return resp.TLS.PeerCertificates[0], nil
+}
+
+// CertFingerprint returns the SHA256 fingerprint of a certificate
+func CertFingerprint(cert *x509.Certificate) string {
+	return fmt.Sprintf("%x", sha256.Sum256(cert.Raw))
 }
--- src/k8s/pkg/utils/certificate_test.go
+++ src/k8s/pkg/utils/certificate_test.go
@@ -1,6 +1,12 @@
 package utils_test
 
 import (
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/canonical/k8s/pkg/utils"
@@ -23,4 +29,77 @@ func TestSplitIPAndDNSSANs(t *testing.T) {
 	g.Expect(ips).To(ConsistOf("192.168.0.1", "::1", "2001:db8:0:1:1:1:1:1"))
 
 	g.Expect(gotDNSs).To(ConsistOf("cluster.local", "kubernetes.svc.local"))
+}
+
+func TestTLSClientConfigWithTrustedCertificate(t *testing.T) {
+	g := NewWithT(t)
+
+	// Mock certificate and certificate pool for testing
+	remoteCert := &x509.Certificate{
+		DNSNames: []string{"bubblegum.com"},
+	}
+	rootCAs := x509.NewCertPool()
+
+	tlsConfig, err := utils.TLSClientConfigWithTrustedCertificate(remoteCert, rootCAs)
+
+	g.Expect(err).To(BeNil())
+	g.Expect(tlsConfig.ServerName).To(Equal("bubblegum.com"))
+	g.Expect(tlsConfig.RootCAs.Subjects()).To(ContainElement(remoteCert.RawSubject))
+
+	// Test with invalid remote certificate
+	tlsConfig, err = utils.TLSClientConfigWithTrustedCertificate(nil, rootCAs)
+	g.Expect(err).ToNot(BeNil())
+	g.Expect(tlsConfig).To(BeNil())
+
+	// Test with nil root CAs
+	_, err = utils.TLSClientConfigWithTrustedCertificate(remoteCert, nil)
+	g.Expect(err).To(BeNil())
+}
+
+func TestCertFingerprint(t *testing.T) {
+	g := NewWithT(t)
+	// Create a mock certificate for testing
+	mockCert := &x509.Certificate{
+		Raw: []byte("ChocolateChipCookieDough"),
+	}
+
+	// Calculate the expected SHA256 fingerprint of the mock certificate
+	expectedFingerprint := sha256.Sum256(mockCert.Raw)
+	expectedFingerprintStr := hex.EncodeToString(expectedFingerprint[:])
+
+	// Call the CertFingerprint function
+	actualFingerprint := utils.CertFingerprint(mockCert)
+
+	// Check if the returned fingerprint matches the expected fingerprint
+	g.Expect(actualFingerprint).To(Equal(expectedFingerprintStr))
+}
+
+func TestGetRemoteCertificate(t *testing.T) {
+	g := NewWithT(t)
+	// Create a mock HTTP server that returns a mock certificate
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	// Test with a valid address
+	remoteCert, err := utils.GetRemoteCertificate(server.Listener.Addr().String())
+	g.Expect(err).To(BeNil())
+	g.Expect(remoteCert).To(Equal(server.Certificate()))
+
+	// Test with an invalid address (missing port)
+	invalidAddr := "candy.canes"
+	expectedErr := fmt.Sprintf("failed to validate the cluster member address: address %s: missing port in address", invalidAddr)
+
+	remoteCert, err = utils.GetRemoteCertificate(invalidAddr)
+	g.Expect(err.Error()).To(ContainSubstring(expectedErr))
+	g.Expect(remoteCert).To(BeNil())
+
+	// Test with a non-existent address
+	nonExistentAddr := "jellybeans:9999"
+	expectedErr = fmt.Sprintf("Get \"https://%s\": dial tcp: lookup jellybeans on", nonExistentAddr)
+
+	remoteCert, err = utils.GetRemoteCertificate(nonExistentAddr)
+	g.Expect(err.Error()).To(ContainSubstring(expectedErr))
+	g.Expect(remoteCert).To(BeNil())
 }
