9438a01062a193fdca6d6a06bab3b46a1a67517a
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-04-15 10:35:31 +0200

    fmt

--- src/k8s/cmd/k8s/k8s.go
+++ src/k8s/cmd/k8s/k8s.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	componentList = []string{"network", "dns", "gateway", "ingress", "local-storage", "load-balancer"}
+	componentList   = []string{"network", "dns", "gateway", "ingress", "local-storage", "load-balancer"}
 	outputFormatter cmdutil.Formatter
 )
 
