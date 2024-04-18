c683cf477caf7c61a2cbf6155daba2400f421fbb
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-04-19 00:14:28 +0300

    Pinc runc to build with Go 1.21

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -5,6 +5,13 @@ VERSION="${2}"
 export INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
+# Pin runc to Go 1.21: https://github.com/opencontainers/runc/issues/4233
+if ! which go_121; then
+  snap download go --channel 1.21 --basename go
+  snap install ./go.snap --classic --dangerous --name go_121
+fi
+export GO=go_121
+
 # Ensure `runc --version` prints the right commit hash from upstream
 export COMMIT="$(git describe --long --always "${VERSION}")"
 
