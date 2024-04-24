0cdc6801e46c7604867d55c366e7129b60313339
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-04-24 14:57:12 -0400

    Collect argument files for each service

--- scripts/inspect.sh
+++ scripts/inspect.sh
@@ -3,15 +3,20 @@
 JOURNALCTL_LIMIT=100000
 RETURN_CODE=0
 
-source utils.sh
+#INSPECT_DUMP=${SNAP_DATA}/inspection-report
+INSPECT_DUMP=/home/user/Repos/k8s-inspect-script/inspection-report
 
-INSPECT_DUMP=${SNAP_DATA}/inspection-report
+SVC_ARGS_DIR=/var/snap/k8s/common/args
 
 function collect_args {
+	local service=$1
 	mkdir -p $INSPECT_DUMP/$service
 
-	# Strip k8s prefix if present because args directories _are not_ created with k8s prefix
-	cp -r $SNAP_COMMON/args/${service#k8s.} $INSPECT_DUMP/$service/args
+	if [ -f $SVC_ARGS_DIR/${service#k8s.} ]; then
+		# Strip k8s. prefix if present because args directories _are not_ created with k8s. prefix
+		cat $SVC_ARGS_DIR/${service#k8s.} &> $INSPECT_DUMP/$service/args
+		printf -- ' Arguments for %s collected\n' "$service"
+	fi
 }
 
 function check_service {
@@ -22,13 +27,11 @@ function check_service {
 	
 	journalctl -n $JOURNALCTL_LIMIT -u snap.$service &> $INSPECT_DUMP/$service/journal.log
 	systemctl status snap.$service &> $INSPECT_DUMP/$service/systemctl.log
-	if systemctl status snap.$service &> /dev/null
-	then
+	if systemctl status snap.$service &> /dev/null; then
 		status="active"
 	fi
 
-	if [ "$status" == "active" ]
-	then
+	if [ "$status" == "active" ]; then
 	  printf -- '  Service %s is running\n' "$service"
 	else
 	  printf -- '\033[31m FAIL: \033[0m Service %s is not running\n' "$service"
@@ -51,12 +54,24 @@ svc_kube_proxy='k8s.kube-proxy'
 svc_kube_scheduler='k8s.kube-scheduler'
 svc_kubelet='k8s.kubelet'
 
-check_service $svc_containerd
-check_service $svc_api_server_proxy
-check_service $svc_k8s_dqlite
-check_service $svc_k8sd
-check_service $svc_kube_apiserver
-check_service $svc_kube_controller_manager
-check_service $svc_kube_proxy
-check_service $svc_kube_scheduler
-check_service $svc_kubelet
+#check_service $svc_containerd
+#check_service $svc_api_server_proxy
+#check_service $svc_k8s_dqlite
+#check_service $svc_k8sd
+#check_service $svc_kube_apiserver
+#check_service $svc_kube_controller_manager
+#check_service $svc_kube_proxy
+#check_service $svc_kube_scheduler
+#check_service $svc_kubelet
+
+printf -- 'Collecting arguments\n'
+
+collect_args $svc_containerd
+collect_args $svc_api_server_proxy
+collect_args $svc_k8s_dqlite
+collect_args $svc_k8sd
+collect_args $svc_kube_apiserver
+collect_args $svc_kube_controller_manager
+collect_args $svc_kube_proxy
+collect_args $svc_kube_scheduler
+collect_args $svc_kubelet
