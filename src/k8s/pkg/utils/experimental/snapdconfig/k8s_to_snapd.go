70b8ebf885a7560d8b8359a6fda9f7a6b8558e46
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-05-03 02:48:59 +0300

    fix snapd from k8s

--- src/k8s/pkg/utils/experimental/snapdconfig/k8s_to_snapd.go
+++ src/k8s/pkg/utils/experimental/snapdconfig/k8s_to_snapd.go
@@ -13,7 +13,7 @@ func SetSnapdFromK8sd(ctx context.Context, config apiv1.UserFacingClusterConfig,
 
 	var sets []string
 	for key, cfg := range map[string]any{
-		"meta":          Meta{Orb: "k8sd", APIVersion: "1.30"},
+		"meta":          Meta{Orb: "snapd", APIVersion: "1.30"},
 		"dns":           config.DNS,
 		"network":       config.Network,
 		"local-storage": config.LocalStorage,
