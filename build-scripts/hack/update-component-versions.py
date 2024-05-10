d039b763bea5b963a3060e87181a75c8a497a0dd
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-05-10 18:56:54 +0300

    introduce build-scripts/hack/update-component-versions.py

---
+++ build-scripts/hack/update-component-versions.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+USAGE = "Update component versions for Canonical Kubernetes"
+
+DESCRIPTION = """
+Update the component versions that we use to build Canonical Kubernetes. This
+script updates the individual `build-scripts/components/<component>/version`
+files. The logic for each component is different, and is managed by configuration
+options found in this script.
+"""
+
+
+import argparse
+import logging
+import sys
+import yaml
+from pathlib import Path
+import util
+
+logging.basicConfig(level=logging.INFO)
+
+LOG = logging.getLogger(__name__)
+
+DIR = Path(__file__).absolute().parent
+COMPONENTS = DIR.parent / "components"
+
+# Version marker for latest Kubernetes version. Expected to be one of:
+#
+# - "https://dl.k8s.io/release/stable.txt"
+# - "https://dl.k8s.io/release/stable-1.xx.txt"
+KUBERNETES_VERSION_MARKER = "https://dl.k8s.io/release/stable.txt"
+
+# Containerd release branch to track. The most recent tag in the branch will be used.
+CONTAINERD_RELEASE_BRANCH = "release/1.6"
+
+# Helm release branch to track. The most recent tag in the branch will be used.
+HELM_RELEASE_BRANCH = "release-3.14"
+
+
+def get_kubernetes_version(marker: str) -> str:
+    """Update Kubernetes version based on the specified marker file"""
+    LOG.info("Checking latest Kubernetes version from %s", marker)
+    return util.read_url(KUBERNETES_VERSION_MARKER)
+
+
+def get_cni_version() -> str:
+    """Update CNI version to match the CNI version used in $kubernetes/build/dependencies.yaml"""
+    kube_repo = util.read_file(COMPONENTS / "kubernetes/repository")
+    kube_version = util.read_file(COMPONENTS / "kubernetes/version")
+
+    with util.git_repo(kube_repo, kube_version) as dir:
+        deps_file = dir / "build/dependencies.yaml"
+        deps = yaml.safe_load(util.read_file(deps_file))
+
+        for dep in deps["dependencies"]:
+            if dep["name"] == "cni":
+                return dep["version"]
+
+        raise Exception(f"Failed to find cni dependency in {deps_file}")
+
+
+def get_containerd_version() -> str:
+    """Update containerd version using latest tag of specified branch"""
+    containerd_repo = util.read_file(COMPONENTS / "containerd/repository")
+
+    with util.git_repo(
+        containerd_repo, CONTAINERD_RELEASE_BRANCH, shallow=False
+    ) as dir:
+        # Get the latest tagged release from the current branch
+        return util.parse_output(["git", "describe", "--tags", "--abbrev=0"], cwd=dir)
+
+
+def get_runc_version() -> str:
+    """Update runc version based on containerd"""
+    containerd_repo = util.read_file(COMPONENTS / "containerd/repository")
+    containerd_version = util.read_file(COMPONENTS / "containerd/version")
+
+    with util.git_repo(containerd_repo, containerd_version) as dir:
+        # See https://github.com/containerd/containerd/blob/main/docs/RUNC.md
+        return util.read_file(dir / "script/setup/runc-version")
+
+
+def get_helm_version() -> str:
+    """Get latest version of helm"""
+    helm_repo = util.read_file(COMPONENTS / "helm/repository")
+    with util.git_repo(helm_repo, HELM_RELEASE_BRANCH, shallow=False) as dir:
+        return util.parse_output(["git", "describe", "--tags", "--abbrev=0"], cwd=dir)
+
+
+def update_component_versions(dry_run: bool):
+    # get_kubernetes_version(KUBERNETES_VERSION_MARKER, dry_run)
+    # update_cni_version(dry_run)
+    for component, get_version in [
+        ("kubernetes", get_kubernetes_version),
+        ("cni", get_cni_version),
+        ("containerd", get_containerd_version),
+        ("runc", get_runc_version),
+        ("helm", get_helm_version),
+    ]:
+        LOG.info("Updating version for %s", component)
+        version = get_version()
+        path = COMPONENTS / component / "version"
+        LOG.info("Update %s version to %s in %s", component, version, path)
+        if not dry_run:
+            Path(path).write_text(version)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        "update-component-versions.py", usage=USAGE, description=DESCRIPTION
+    )
+    parser.add_argument("--dry-run", default=False, action="store_true")
+    args = parser.parse_args(sys.argv[1:])
+
+    return update_component_versions(args.dry_run)
+
+
+if __name__ == "__main__":
+    main()
