8cf35801dbc2c1fc3661edbf5de0c1dd4dbeafc4
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-05-18 17:14:51 -0400

    Review comments

---
+++ build-scripts/hack/sync-registry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=$(realpath $(dirname "${0}"))
+
+docker run -v "${DIR}/registry-k8s-io.yaml":/config.yaml quay.io/skopeo/stable:v1.15 sync \
+  --src yaml \
+  --dest docker \
+  /config.yaml ghcr.io/canonical \
+  --format oci \
+  --dest-creds "${ACTOR}":"${TOKEN}"
