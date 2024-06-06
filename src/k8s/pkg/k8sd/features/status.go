e90c0cd30dde74aefb229e5239cfb9ab13d490f7
Mateo Florido <mateo.florido@canonical.com>
2024-06-05 19:56:06 -0500

    Make network tests more generic

--- src/k8s/pkg/k8sd/features/status.go
+++ src/k8s/pkg/k8sd/features/status.go
@@ -6,11 +6,15 @@ import (
 	"github.com/canonical/k8s/pkg/snap"
 )
 
+// StatusInterface defines the interface for checking the status of the built-in features.
 type StatusInterface interface {
+	// CheckDNS checks the status of the DNS feature.
 	CheckDNS(context.Context, snap.Snap) error
+	// CheckNetwork checks the status of the Network feature.
 	CheckNetwork(context.Context, snap.Snap) error
 }
 
+// statusChecks implements the StatusInterface.
 type statusChecks struct {
 	checkDNS     func(context.Context, snap.Snap) error
 	checkNetwork func(context.Context, snap.Snap) error
