e7e693106ebd7cc36776e06b6ad37b0399e9849a
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-06-10 11:07:38 +0200

    Cleanup Status check code
    * Refactor IsPodReady to `CheckForReadyPods` and add go-docs
    * Add unittest for `CheckForReadyPods`
    * Remove `ListPods`
    * Refactor `CheckNetwork` and `CheckDNS` to only return error (nil->success)

--- src/k8s/pkg/client/kubernetes/pods.go
+++ src/k8s/pkg/client/kubernetes/pods.go
@@ -3,41 +3,52 @@ package kubernetes
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// IsPodReady checks if a pod is ready.
-func (c *Client) IsPodReady(ctx context.Context, name, namespace string, listOptions metav1.ListOptions) (bool, error) {
+// CheckForReadyPods checks if all pods in the specified namespace are ready.
+// It returns an error if any of the pods are not ready.
+// The listOptions specify additional options for listing the pods, e.g. labels.
+// It returns an error if it fails to list the pods or if there are no pods in the namespace.
+// If any of the pods are not ready, it returns an error with the names of the not ready pods.
+// If all pods are ready, it returns nil.
+func (c *Client) CheckForReadyPods(ctx context.Context, namespace string, listOptions metav1.ListOptions) error {
 	pods, err := c.CoreV1().Pods(namespace).List(ctx, listOptions)
 	if err != nil {
-		return false, fmt.Errorf("failed to list pods: %w", err)
+		return fmt.Errorf("failed to list pods: %w", err)
+	}
+	if len(pods.Items) == 0 {
+		return fmt.Errorf("no pods in %v namespace on the cluster", namespace)
 	}
 
+	notReadyPods := []string{}
 	for _, pod := range pods.Items {
-		if strings.Contains(pod.Name, name) {
-			if pod.Status.Phase != corev1.PodRunning {
-				return false, nil
-			}
-
-			for _, condition := range pod.Status.Conditions {
-				if condition.Type == corev1.PodReady && condition.Status == corev1.ConditionTrue {
-					return true, nil
-				}
-			}
+		if !podIsReady(pod) {
+			notReadyPods = append(notReadyPods, pod.Name)
 		}
 	}
 
-	return false, nil
+	if len(notReadyPods) > 0 {
+		return fmt.Errorf("pods %v not ready", notReadyPods)
+	}
+	return nil
 }
 
-// ListPods lists all pods in a namespace.
-func (c *Client) ListPods(ctx context.Context, namespace string, listOptions metav1.ListOptions) ([]corev1.Pod, error) {
-	pods, err := c.CoreV1().Pods(namespace).List(ctx, listOptions)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list pods: %w", err)
+// podIsReady checks if a pod is in the ready state.
+// It returns true if the pod is running (Condition "Ready" = true).
+// Otherwise, it returns false.
+func podIsReady(pod corev1.Pod) bool {
+	if pod.Status.Phase != corev1.PodRunning {
+		return false
 	}
-	return pods.Items, nil
+
+	for _, condition := range pod.Status.Conditions {
+		if condition.Type == corev1.PodReady && condition.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+
+	return false
 }
--- src/k8s/pkg/k8sd/features/coredns/status.go
+++ src/k8s/pkg/k8sd/features/coredns/status.go
@@ -10,19 +10,25 @@ import (
 )
 
 // CheckDNS checks the CoreDNS deployment in the cluster.
-func CheckDNS(ctx context.Context, snap snap.Snap) (bool, error) {
+func CheckDNS(ctx context.Context, snap snap.Snap) error {
 	client, err := snap.KubernetesClient("kube-system")
 	if err != nil {
-		return false, fmt.Errorf("failed to create kubernetes client: %w", err)
+		return fmt.Errorf("failed to create kubernetes client: %w", err)
 	}
 
-	isReady, err := client.IsPodReady(ctx, "coredns", "kube-system", metav1.ListOptions{LabelSelector: "app.kubernetes.io/name=coredns"})
-	if err != nil {
-		return false, fmt.Errorf("failed to wait for CoreDNS pod to be ready: %w", err)
-	}
-	if !isReady {
-		return false, fmt.Errorf("coredns pod not ready yet")
+	for _, check := range []struct {
+		name      string
+		namespace string
+		labels    map[string]string
+	}{
+		{name: "coredns", namespace: "kube-system", labels: map[string]string{"app.kubernetes.io/name": "coredns"}},
+	} {
+		if err := client.CheckForReadyPods(ctx, check.namespace, metav1.ListOptions{
+			LabelSelector: metav1.FormatLabelSelector(&metav1.LabelSelector{MatchLabels: check.labels}),
+		}); err != nil {
+			return fmt.Errorf("check %v failed: %w", check.name, err)
+		}
 	}
 
-	return isReady, nil
+	return nil
 }
