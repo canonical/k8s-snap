e87f8fe108d19cc15a2aba8e00f8df3a103b9b0d
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-06-16 13:28:51 +0300

    add k8s x-list-images command

---
+++ src/k8s/cmd/k8s/k8s_x_list_images.go
@@ -0,0 +1,21 @@
+package k8s
+
+import (
+	"strings"
+
+	cmdutil "github.com/canonical/k8s/cmd/util"
+	"github.com/canonical/k8s/pkg/k8sd/images"
+	"github.com/spf13/cobra"
+)
+
+func newXListImagesCmd(env cmdutil.ExecutionEnvironment) *cobra.Command {
+	cmd := &cobra.Command{
+		Hidden: true,
+		Use:    "x-list-images",
+		Short:  "List all images used by the current version of k8s-snap",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.PrintErrln(strings.Join(images.Images(), "\n"))
+		},
+	}
+	return cmd
+}
---
+++ src/k8s/pkg/k8sd/features/coredns/register.go
@@ -0,0 +1,13 @@
+package coredns
+
+import (
+	"fmt"
+
+	"github.com/canonical/k8s/pkg/k8sd/images"
+)
+
+func init() {
+	images.Register(
+		fmt.Sprintf("%s:%s", imageRepo, imageTag),
+	)
+}
