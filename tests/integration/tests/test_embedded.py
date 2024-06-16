8e7f7beebae367379c53ce4ca5d635b6c2c59abe
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-06-16 14:01:27 +0300

    add e2e test for embedded datastore

---
+++ tests/integration/tests/test_embedded.py
@@ -0,0 +1,84 @@
+#
+# Copyright 2024 Canonical, Ltd.
+#
+import logging
+from typing import List
+
+import pytest
+import yaml
+from test_util import harness, util
+
+LOG = logging.getLogger(__name__)
+
+
+@pytest.mark.node_count(2)
+@pytest.mark.disable_k8s_bootstrapping()
+def test_embedded(instances: List[harness.Instance]):
+    cluster_node = instances[0]
+    joining_node = instances[1]
+
+    bootstrap_conf = yaml.safe_dump(
+        {
+            "cluster-config": {"network": {"enabled": True}, "dns": {"enabled": True}},
+            "datastore-type": "embedded",
+        }
+    )
+
+    cluster_node.exec(
+        ["dd", "of=/root/config.yaml"],
+        input=str.encode(bootstrap_conf),
+    )
+
+    cluster_node.exec(["k8s", "bootstrap", "--file", "/root/config.yaml"])
+    util.wait_for_dns(cluster_node)
+    util.wait_for_network(cluster_node)
+
+    join_token = util.get_join_token(cluster_node, joining_node)
+    util.join_cluster(joining_node, join_token)
+
+    util.wait_until_k8s_ready(cluster_node, instances)
+    nodes = util.ready_nodes(cluster_node)
+    assert len(nodes) == 2, "node should have joined cluster"
+
+    assert "control-plane" in util.get_local_node_status(cluster_node)
+    assert "control-plane" in util.get_local_node_status(joining_node)
+
+    cluster_node.exec(["k8s", "remove-node", joining_node.id])
+    nodes = util.ready_nodes(cluster_node)
+    assert len(nodes) == 1, "node should have been removed from cluster"
+    assert (
+        nodes[0]["metadata"]["name"] == cluster_node.id
+    ), f"only {cluster_node.id} should be left in cluster"
+
+
+@pytest.mark.node_count(3)
+def test_worker_nodes(instances: List[harness.Instance]):
+    cluster_node = instances[0]
+    joining_node = instances[1]
+    other_joining_node = instances[2]
+
+    join_token = util.get_join_token(cluster_node, joining_node, "--worker")
+    join_token_2 = util.get_join_token(cluster_node, other_joining_node, "--worker")
+
+    assert join_token != join_token_2
+
+    util.join_cluster(joining_node, join_token)
+
+    util.join_cluster(other_joining_node, join_token_2)
+
+    util.wait_until_k8s_ready(cluster_node, instances)
+    nodes = util.ready_nodes(cluster_node)
+    assert len(nodes) == 3, "workers should have joined cluster"
+
+    assert "control-plane" in util.get_local_node_status(cluster_node)
+    assert "worker" in util.get_local_node_status(joining_node)
+    assert "worker" in util.get_local_node_status(other_joining_node)
+
+    cluster_node.exec(["k8s", "remove-node", joining_node.id])
+    nodes = util.ready_nodes(cluster_node)
+    assert len(nodes) == 2, "worker should have been removed from cluster"
+    assert cluster_node.id in [
+        node["metadata"]["name"] for node in nodes
+    ] and other_joining_node.id in [
+        node["metadata"]["name"] for node in nodes
+    ], f"only {cluster_node.id} should be left in cluster"
