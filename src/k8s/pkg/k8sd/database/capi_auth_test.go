b3c333692881b54b5ba937400c5ee9b1997a5069
Mateo Florido <mateo.florido@canonical.com>
2024-06-18 15:32:35 -0500

    Remove ClusterConfig seed from tests

--- src/k8s/pkg/k8sd/database/capi_auth_test.go
+++ src/k8s/pkg/k8sd/database/capi_auth_test.go
@@ -6,24 +6,12 @@ import (
 	"testing"
 
 	"github.com/canonical/k8s/pkg/k8sd/database"
-	"github.com/canonical/k8s/pkg/k8sd/types"
 	. "github.com/onsi/gomega"
 )
 
 func TestClusterAPIAuthTokens(t *testing.T) {
 	WithDB(t, func(ctx context.Context, db DB) {
 		var token string = "test-token"
-
-		// Seed the initial cluster config.
-		err := db.Transaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
-			clusterConfig := types.ClusterConfig{}
-			clusterConfig.SetDefaults()
-			_, err := database.SetClusterConfig(ctx, tx, clusterConfig)
-			return err
-		})
-		if err != nil {
-			t.Fatalf("failed to set cluster config: %v", err)
-		}
 
 		t.Run("SetAuthToken", func(t *testing.T) {
 			g := NewWithT(t)
