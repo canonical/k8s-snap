88bbb6401b8a4da528b6d2923cf56f604b4d28a3
Mateo Florido <mateo.florido@canonical.com>
2024-06-19 12:46:03 -0500

    Changed Repository for Calico images

--- src/k8s/pkg/k8sd/features/calico/register.go
+++ src/k8s/pkg/k8sd/features/calico/register.go
@@ -15,23 +15,23 @@ func init() {
 	//
 	// Hardcoded list based on "k8s kubectl get node -o template='{{ range .items }}{{ .metadata.name }}{{":"}}{{ range .status.images }}{{ "\n- " }}{{ index .names 1 }}{{ end }}{{"\n"}}{{ end }}' | grep calico":
 	//
-	// - docker.io/calico/node:v3.28.0
-	// - docker.io/calico/cni:v3.28.0
-	// - docker.io/calico/apiserver:v3.28.0
-	// - docker.io/calico/kube-controllers:v3.28.0
-	// - docker.io/calico/typha:v3.28.0
-	// - docker.io/calico/node-driver-registrar:v3.28.0
-	// - docker.io/calico/csi:v3.28.0
-	// - docker.io/calico/pod2daemon-flexvol:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/node:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/cni:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/apiserver:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/kube-controllers:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/typha:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/node-driver-registrar:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/csi:v3.28.0
+	// - ghcr.io/canonical/k8s-snap/calico/pod2daemon-flexvol:v3.28.0
 
 	images.Register(
-		"docker.io/calico/node:v3.28.0",
-		"docker.io/calico/cni:v3.28.0",
-		"docker.io/calico/apiserver:v3.28.0",
-		"docker.io/calico/kube-controllers:v3.28.0",
-		"docker.io/calico/typha:v3.28.0",
-		"docker.io/calico/node-driver-registrar:v3.28.0",
-		"docker.io/calico/csi:v3.28.0",
-		"docker.io/calico/pod2daemon-flexvol:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/node:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/cni:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/apiserver:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/kube-controllers:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/typha:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/node-driver-registrar:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/csi:v3.28.0",
+		"ghcr.io/canonical/k8s-snap/calico/pod2daemon-flexvol:v3.28.0",
 	)
 }
