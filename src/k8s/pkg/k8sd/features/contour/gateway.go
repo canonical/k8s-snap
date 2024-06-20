58de94f542a271e857c70e2fa99faf7bc7df4795
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-06-20 10:50:16 +0200

    correct api-resource name

--- src/k8s/pkg/k8sd/features/contour/gateway.go
+++ src/k8s/pkg/k8sd/features/contour/gateway.go
@@ -62,11 +62,11 @@ func waitForRequiredContourCommonCRDs(ctx context.Context, snap snap.Snap) error
 		combinedAPIResources := append(resourcesV1Alpha.APIResources, resourcesV1.APIResources...)
 
 		requiredCRDs := map[string]bool{
-			"projectcontour.io/v1alpha1:contourconfigurations.projectcontour.io": true,
-			"projectcontour.io/v1alpha1:contourdeployments.projectcontour.io":    true,
-			"projectcontour.io/v1alpha1:extensionservices.projectcontour.io":     true,
-			"projectcontour.io/v1:tlscertificatedelegations.projectcontour.io":   true,
-			"projectcontour.io/v1:httpproxies.projectcontour.io":                 true,
+			"projectcontour.io/v1alpha1:contourconfigurations": true,
+			"projectcontour.io/v1alpha1:contourdeployments":    true,
+			"projectcontour.io/v1alpha1:extensionservices":     true,
+			"projectcontour.io/v1:tlscertificatedelegations":   true,
+			"projectcontour.io/v1:httpproxies":                 true,
 		}
 
 		requiredCount := len(requiredCRDs)
