f8275770b6e4835d1a0e61c5f621b4279bd1b904
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-06-20 21:06:10 -0400

    refactor cis-yaml-to-md.py and cis-template.jinja2
    refactor the file to use clearer function, variable names
    
    extract logic from generate_markdown that is better suited to be in a new function
    
    add a setup_directories function
    
    extract the logic to find the kube bench control files into a function
    
    extract argument parsing to a function
    
    reflect these changes in the jinja template

--- k8s/scripts/cis-yaml-to-md.py
+++ k8s/scripts/cis-yaml-to-md.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
+from typing import Any
+
 USAGE = """
 python3 cis-yaml-to-md.py --input-directory=INPUT_DIR --output-directory=OUTPUT_DIR
 """
@@ -26,7 +28,8 @@ LOG = logging.getLogger(__name__)
 DIR = Path(__file__).absolute().parent
 
 JINJA_TEMPLATE = "cis-template.jinja2"
-CONFIG_FILE = "config.yaml"
+KUBE_BENCH_CONFIG_FILE = "config.yaml"
+KUBE_BENCH_CONTROL_OUTPUTS = "expected-outputs.yaml"
 
 
 def get_variable_substitutions(data: Any) -> dict[str, str]:
@@ -52,7 +55,7 @@ def get_variable_substitutions(data: Any) -> dict[str, str]:
     }
 
 
-def make_template(data: Any) -> str:
+def render_jinja_template(**kwargs) -> str:
     def to_yaml_filter(value):
         return yaml.dump(value, default_flow_style=False).strip()
 
@@ -63,35 +66,55 @@ def make_template(data: Any) -> str:
     )
     env.filters["to_yaml"] = to_yaml_filter
 
-    return env.get_template(JINJA_TEMPLATE).render(
-        title=data["text"],
-        groups=data["groups"],
+    return env.get_template(JINJA_TEMPLATE).render(**kwargs)
+
+
+def generate_markdown_from_yaml_file(
+    original_yaml_file: Path,
+    substitutions: dict[str, str] | None = None,
+    custom_outputs_by_control_id: dict[str, str] | None = None,
+):
+    markdown_content = render_jinja_template(
+        kube_bench_control_file=yaml.safe_load(original_yaml_file.read_text()),
+        custom_outputs_by_control_id=custom_outputs_by_control_id,
     )
 
+    if substitutions:
+        for old, new in substitutions.items():
+            markdown_content = markdown_content.replace(old, new)
 
-def generate_markdown(
-    input_dir: Path, output_dir: Path, substitutions: dict[str, str] = None
-):
-    input_files = [
+    return markdown_content
+
+
+def setup_directories(input_dir: str, output_dir: str) -> tuple[Path, Path]:
+    input_path = Path(input_dir).expanduser()
+    output_path = Path(output_dir).expanduser()
+    output_path.mkdir(exist_ok=True)
+    return input_path, output_path
+
+
+def get_kube_bench_input_files(input_dir: Path) -> list[Path]:
+    return [
         file
         for file in input_dir.iterdir()
-        if file.is_file() and file.suffix == ".yaml" and file.name != "config.yaml"
+        if file.is_file()
+        and file.suffix == ".yaml"
+        and file.name != KUBE_BENCH_CONFIG_FILE
     ]
 
-    for file in input_files:
-        markdown_content = make_template(yaml.safe_load(file.read_text()))
 
-        if substitutions:
-            for old, new in substitutions.items():
-                markdown_content = markdown_content.replace(old, new)
-
+def process_files(input_dir, output_dir, substitutions, custom_outputs_by_control_id):
+    for file in get_kube_bench_input_files(input_dir):
         output_file = output_dir / f"{file.stem}.md"
+        markdown_content = generate_markdown_from_yaml_file(
+            file, substitutions, custom_outputs_by_control_id
+        )
         output_file.write_text(markdown_content)
 
         LOG.info(f"Rendered {file} to {output_file}.")
 
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION)
     parser.add_argument(
         "--input-dir", type=str, required=True, help="Input directory path"
@@ -99,18 +122,23 @@ def main():
     parser.add_argument(
         "--output-dir", type=str, required=True, help="Output directory path"
     )
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    input_dir = Path(args.input_dir).expanduser()
-    output_dir = Path(args.output_dir).expanduser()
 
-    output_dir.mkdir(exist_ok=True)
+def main():
+    args = parse_arguments()
+
+    input_dir, output_dir = setup_directories(args.input_dir, args.output_dir)
 
     substitutions = get_variable_substitutions(
-        yaml.safe_load((input_dir / CONFIG_FILE).read_text())
+        yaml.safe_load((input_dir / KUBE_BENCH_CONFIG_FILE).read_text())
     )
 
-    generate_markdown(input_dir, output_dir, substitutions)
+    custom_outputs_by_control_id: dict[str, str] = yaml.safe_load(
+        (DIR / KUBE_BENCH_CONTROL_OUTPUTS).read_text()
+    )
+
+    process_files(input_dir, output_dir, substitutions, custom_outputs_by_control_id)
 
 
 if __name__ == "__main__":
