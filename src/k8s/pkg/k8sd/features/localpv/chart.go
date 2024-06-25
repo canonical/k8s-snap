332ce3032b1cc811a671a1a4f5c647dcdd3d4175
Mateo Florido <32885896+mateoflorido@users.noreply.github.com>
2024-06-25 14:50:46 -0500

    Sync CSI images to `ghcr.io` (#507)
    Sync the CSI images to ghcr.io and use them in the snap workloads.

--- src/k8s/pkg/k8sd/features/localpv/chart.go
+++ src/k8s/pkg/k8sd/features/localpv/chart.go
@@ -11,12 +11,20 @@ var (
 	chart = helm.InstallableChart{
 		Name:         "ck-storage",
 		Namespace:    "kube-system",
-		ManifestPath: path.Join("charts", "rawfile-csi-0.8.0.tgz"),
+		ManifestPath: path.Join("charts", "rawfile-csi-0.9.0.tgz"),
 	}
 
-	// imageRepo is the image to use for Rawfile LocalPV CSI.
+	// imageRepo is the repository to use for Rawfile LocalPV CSI.
 	imageRepo = "ghcr.io/canonical/rawfile-localpv"
-
-	// imageTag is the image tag to use.
+	// imageTag is the image tag to use for Rawfile LocalPV CSI.
 	imageTag = "0.8.0-ck5"
+
+	// csiNodeDriverImage is the image to use for the CSI node driver.
+	csiNodeDriverImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-node-driver-registrar:v2.10.1"
+	// csiProvisionerImage is the image to use for the CSI provisioner.
+	csiProvisionerImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-provisioner:v5.0.1"
+	// csiResizerImage is the image to use for the CSI resizer.
+	csiResizerImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-resizer:v1.11.1"
+	// csiSnapshotterImage is the image to use for the CSI snapshotter.
+	csiSnapshotterImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-snapshotter:v8.0.1"
 )
