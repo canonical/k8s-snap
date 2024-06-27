2958f9427980bae015e22271385e3eb8100faf30
eaudetcobello <155978570+eaudetcobello@users.noreply.github.com>
2024-06-27 07:11:14 -0400

    Sync MetalLB images to ghcr.io (#513)
    * sync images from quay to ghcr, point chart.go to new ghcr repos 
    
    * remove load-balancer test todo as a drive-by

--- src/k8s/pkg/k8sd/features/metallb/chart.go
+++ src/k8s/pkg/k8sd/features/metallb/chart.go
@@ -22,19 +22,19 @@ var (
 	}
 
 	// controllerImageRepo is the image to use for metallb-controller.
-	controllerImageRepo = "quay.io/metallb/controller"
+	controllerImageRepo = "ghcr.io/canonical/k8s-snap/metallb/controller"
 
 	// controllerImageTag is the tag to use for metallb-controller.
 	controllerImageTag = "v0.14.5"
 
 	// speakerImageRepo is the image to use for metallb-speaker.
-	speakerImageRepo = "quay.io/metallb/speaker"
+	speakerImageRepo = "ghcr.io/canonical/k8s-snap/metallb/speaker"
 
 	// speakerImageTag is the tag to use for metallb-speaker.
 	speakerImageTag = "v0.14.5"
 
 	// frrImageRepo is the image to use for frrouting.
-	frrImageRepo = "quay.io/frrouting/frr"
+	frrImageRepo = "ghcr.io/canonical/k8s-snap/frrouting/frr"
 
 	// frrImageTag is the tag to use for frrouting.
 	frrImageTag = "9.0.2"
