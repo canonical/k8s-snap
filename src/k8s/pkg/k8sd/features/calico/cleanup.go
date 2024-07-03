2bb842f003a9dd726620b78994ddd667f03ab1a9
Berkay Tekin Öz <berkay.tekinoz@canonical.com>
2024-07-03 12:12:47 +0300

    Switch default calico encapsulation to VXLAN, add annotations for autodetection (#514)

--- src/k8s/pkg/k8sd/features/calico/cleanup.go
+++ src/k8s/pkg/k8sd/features/calico/cleanup.go
@@ -14,6 +14,8 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+var reDefaultCalicoInterface = regexp.MustCompile("^vxlan[-v6]*.calico|cali[a-f0-9]*|tunl[0-9]*$")
+
 func CleanupNetwork(ctx context.Context, snap snap.Snap) error {
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -25,10 +27,7 @@ func CleanupNetwork(ctx context.Context, snap snap.Snap) error {
 		// Check if the interface name matches the regex pattern
 		// Adapted from MicroK8s' link removal hook:
 		// https://github.com/canonical/microk8s/blob/dff3627959d4774198000795a0a0afcaa003324b/microk8s-resources/default-hooks/remove.d/10-cni-link#L15
-		match, err := regexp.MatchString("^vxlan[-v6]*.calico|cali[a-f0-9]*|tunl[0-9]*$", iface.Name)
-		if err != nil {
-			return fmt.Errorf("failed to match regex pattern: %w", err)
-		}
+		match := reDefaultCalicoInterface.MatchString(iface.Name)
 		if match {
 			// Perform cleanup for Calico interface
 			if err := exec.CommandContext(ctx, "ip", "link", "delete", iface.Name).Run(); err != nil {
--- src/k8s/pkg/k8sd/features/calico/network.go
+++ src/k8s/pkg/k8sd/features/calico/network.go
@@ -13,7 +13,7 @@ import (
 // ApplyNetwork will deploy Calico when cfg.Enabled is true.
 // ApplyNetwork will remove Calico when cfg.Enabled is false.
 // ApplyNetwork returns an error if anything fails.
-func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, _ types.Annotations) error {
+func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, annotations types.Annotations) error {
 	m := snap.HelmClient()
 
 	if !cfg.GetEnabled() {
@@ -23,6 +23,11 @@ func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, _ type
 		return nil
 	}
 
+	config, err := internalConfig(annotations)
+	if err != nil {
+		return fmt.Errorf("failed to parse annotations: %w", err)
+	}
+
 	podIpPools := []map[string]any{}
 	ipv4PodCIDR, ipv6PodCIDR, err := utils.ParseCIDRs(cfg.GetPodCIDR())
 	if err != nil {
@@ -30,14 +35,16 @@ func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, _ type
 	}
 	if ipv4PodCIDR != "" {
 		podIpPools = append(podIpPools, map[string]any{
-			"name": "ipv4-ippool",
-			"cidr": ipv4PodCIDR,
+			"name":          "ipv4-ippool",
+			"cidr":          ipv4PodCIDR,
+			"encapsulation": config.encapsulationV4,
 		})
 	}
 	if ipv6PodCIDR != "" {
 		podIpPools = append(podIpPools, map[string]any{
-			"name": "ipv6-ippool",
-			"cidr": ipv6PodCIDR,
+			"name":          "ipv6-ippool",
+			"cidr":          ipv6PodCIDR,
+			"encapsulation": config.encapsulationV6,
 		})
 	}
 
@@ -53,6 +60,18 @@ func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, _ type
 		serviceCIDRs = append(serviceCIDRs, ipv6ServiceCIDR)
 	}
 
+	calicoNetworkValues := map[string]any{
+		"ipPools": podIpPools,
+	}
+
+	if config.autodetectionV4 != nil {
+		calicoNetworkValues["nodeAddressAutodetectionV4"] = config.autodetectionV4
+	}
+
+	if config.autodetectionV6 != nil {
+		calicoNetworkValues["nodeAddressAutodetectionV6"] = config.autodetectionV6
+	}
+
 	values := map[string]any{
 		"tigeraOperator": map[string]any{
 			"registry": imageRepo,
@@ -64,10 +83,11 @@ func ApplyNetwork(ctx context.Context, snap snap.Snap, cfg types.Network, _ type
 			"tag":   calicoCtlTag,
 		},
 		"installation": map[string]any{
-			"calicoNetwork": map[string]any{
-				"ipPools": podIpPools,
-			},
-			"registry": imageRepo,
+			"calicoNetwork": calicoNetworkValues,
+			"registry":      imageRepo,
+		},
+		"apiServer": map[string]any{
+			"enabled": config.apiServerEnabled,
 		},
 		"serviceCIDRs": serviceCIDRs,
 	}
