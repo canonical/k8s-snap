a8eed664f23e9404c7e3fd0351bd854abff1a618
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-07-19 17:38:36 +0200

    linting

--- tests/integration/tests/test_clustering_race.py
+++ tests/integration/tests/test_clustering_race.py
@@ -1,17 +1,10 @@
 #
 # Copyright 2024 Canonical, Ltd.
 #
-import logging
 from typing import List
-import time
-import base64
-import json
 
 import pytest
 from test_util import harness, util
-
-LOG = logging.getLogger(__name__)
-
 
 
 @pytest.mark.node_count(3)
@@ -27,6 +20,7 @@ def test_wrong_token_race(instances: List[harness.Instance]):
 
     another_join_token = util.get_join_token(cluster_node, instances[2])
 
-    # The join token should have changed after the node was removed as it contains the ip addresses of all cluster nodes.
+    # The join token should have changed after the node was removed as
+    # it contains the ip addresses of all cluster nodes.
     assert new_join_token != another_join_token
     util.join_cluster(instances[2], new_join_token)
