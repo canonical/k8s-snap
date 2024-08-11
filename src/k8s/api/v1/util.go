5a58e2ea05549eccd04d0e0b1fb87bddeb2ed0e1
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-08-11 03:17:56 +0300

    remove API from repo

--- src/k8s/api/v1/util.go
+++
@@ -1,9 +0,0 @@
-package apiv1
-
-func getField[T any](val *T) T {
-	if val != nil {
-		return *val
-	}
-	var zero T
-	return zero
-}
--- src/k8s/api/v1/version.go
+++
@@ -1,8 +0,0 @@
-package apiv1
-
-import "github.com/canonical/microcluster/v2/rest/types"
-
-const (
-	// K8sdAPIVersion is the path prefix that will be used for the k8sd endpoints for this api version.
-	K8sdAPIVersion types.EndpointPrefix = "1.0"
-)
