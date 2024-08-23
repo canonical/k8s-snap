801c7aa1119f6b7ec53c9948bc560b3ce41d2106
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-08-23 09:39:56 +0200

    inline default expiry value

--- src/k8s/pkg/k8sd/database/sql/migrations/worker-tokens/001-add-expiry.sql
+++ src/k8s/pkg/k8sd/database/sql/migrations/worker-tokens/001-add-expiry.sql
@@ -1,7 +1,2 @@
 ALTER TABLE worker_tokens
-ADD COLUMN expiry DATETIME;
-
--- set default value for existing values.
-UPDATE worker_tokens
-SET expiry = '2100-01-01 23:59:59'
-WHERE expiry IS NULL;
+ADD COLUMN expiry DATETIME DEFAULT '2100-01-01 23:59:59';
