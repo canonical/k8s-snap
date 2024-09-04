1b51ba74e57089eb8362d4f99452103381849028
Homayoon Alimohammadi <homayoon.alimohammadi@canonical.com>
2024-09-04 13:44:07 +0400

    Add unit tests for Calico network k8sd feature (#647)
    * Add unit tests for calico k8sd network feature
    
    * Remove t.Helper from cilium tests

--- src/k8s/pkg/k8sd/features/calico/register.go
+++ src/k8s/pkg/k8sd/features/calico/register.go
@@ -11,14 +11,14 @@ func init() {
 		// Tigera images
 		fmt.Sprintf("%s/%s:%s", imageRepo, tigeraOperatorImage, tigeraOperatorVersion),
 		// Calico images
-		fmt.Sprintf("%s/apiserver:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/cni:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/csi:%s", calicoImageRepo, calicoTag),
+		fmt.Sprintf("%s/apiserver:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/cni:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/csi:%s", calicoImageRepo, CalicoTag),
 		fmt.Sprintf("%s/ctl:%s", calicoImageRepo, calicoCtlTag),
-		fmt.Sprintf("%s/kube-controllers:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/node:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/node-driver-registrar:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/pod2daemon-flexvol:%s", calicoImageRepo, calicoTag),
-		fmt.Sprintf("%s/typha:%s", calicoImageRepo, calicoTag),
+		fmt.Sprintf("%s/kube-controllers:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/node:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/node-driver-registrar:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/pod2daemon-flexvol:%s", calicoImageRepo, CalicoTag),
+		fmt.Sprintf("%s/typha:%s", calicoImageRepo, CalicoTag),
 	)
 }
