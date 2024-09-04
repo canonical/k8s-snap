3fc78adfa64a68ec669f29826061fe47fd034ae7
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-04 18:38:20 +0200

    add ipv6 only test

--- tests/integration/tests/test_networking.py
+++ tests/integration/tests/test_networking.py
@@ -56,3 +56,50 @@ def test_dualstack(instances: List[harness.Instance]):
         util.stubbornly(retries=3, delay_s=1).on(main).exec(
             ["curl", address], shell=True
         )
+
+
+@pytest.mark.node_count(1)
+@pytest.mark.bootstrap_config(
+    (config.MANIFESTS_DIR / "bootstrap-ipv6-only.yaml").read_text()
+)
+@pytest.mark.dualstack()
+def test_dualstack(instances: List[harness.Instance]):
+    main = instances[0]
+    dualstack_config = (config.MANIFESTS_DIR / "nginx-dualstack.yaml").read_text()
+
+    # Deploy nginx with dualstack service
+    main.exec(
+        ["k8s", "kubectl", "apply", "-f", "-"], input=str.encode(dualstack_config)
+    )
+    addresses = (
+        util.stubbornly(retries=5, delay_s=3)
+        .on(main)
+        .exec(
+            [
+                "k8s",
+                "kubectl",
+                "get",
+                "svc",
+                "nginx-dualstack",
+                "-o",
+                "jsonpath='{.spec.clusterIPs[*]}'",
+            ],
+            text=True,
+            capture_output=True,
+        )
+        .stdout
+    )
+
+    for ip in addresses.split():
+        addr = ip_address(ip.strip("'"))
+        if isinstance(addr, IPv6Address):
+            address = f"http://[{str(addr)}]"
+        elif isinstance(addr, IPv4Address):
+            address = f"http://{str(addr)}"
+        else:
+            pytest.fail(f"Unknown IP address type: {addr}")
+
+        # need to shell out otherwise this runs into permission errors
+        util.stubbornly(retries=3, delay_s=1).on(main).exec(
+            ["curl", address], shell=True
+        )
