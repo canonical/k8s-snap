a9f060f6558c4ca5025d6147d2ecf49d9157bc7f
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2024-09-04 14:01:16 +0000

    Add "DEBUG_BUILD" flag
    A recent PR [1] added debug symbols by default, which wasn't intended.
    This increases the k8s binary size from ~74M to ~99M.
    
    We're introducing an env variable flag for this:
    
      export DEBUG_BUILD=y
    
    [1] https://github.com/canonical/k8s-snap/pull/618

--- src/k8s/hack/dynamic-go-build.sh
+++ src/k8s/hack/dynamic-go-build.sh
@@ -4,7 +4,15 @@ DIR="$(realpath `dirname "${0}"`)"
 
 . "${DIR}/dynamic-dqlite.sh"
 
-go build \
+DEBUG_BUILD=${DEBUG_BUILD:-"n"}
+if [[ "$DEBUG_BUILD" == "y" ]]; then
+  go build \
+  -gcflags=all="-N -l" \
+  -tags dqlite,libsqlite3 \
+  "${@}"
+else
+  go build \
   -tags dqlite,libsqlite3 \
   -ldflags '-s -w' \
   "${@}"
+fi
--- src/k8s/hack/static-go-build.sh
+++ src/k8s/hack/static-go-build.sh
@@ -4,8 +4,16 @@ DIR="$(realpath `dirname "${0}"`)"
 
 . "${DIR}/static-dqlite.sh"
 
-go build \
-  -gcflags=all="-N -l" \
-  -tags dqlite,libsqlite3 \
-  -ldflags '--linkmode "external" -extldflags "-static"' \
-  "${@}"
+DEBUG_BUILD=${DEBUG_BUILD:-"n"}
+if [[ "$DEBUG_BUILD" == "y" ]]; then
+  go build \
+    -gcflags=all="-N -l" \
+    -tags dqlite,libsqlite3 \
+    -ldflags '--linkmode "external" -extldflags "-static"' \
+    "${@}"
+else
+  go build \
+    -tags dqlite,libsqlite3 \
+    -ldflags '-s -w --linkmode "external" -extldflags "-static"' \
+    "${@}"
+fi
