027c6f57ef95f0e11a001979a54116d33dc31cec
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-23 12:10:23 +0200

    fix arch command

--- tests/integration/tests/test_version_upgrades.py
+++ tests/integration/tests/test_version_upgrades.py
@@ -26,7 +26,7 @@ def test_version_upgrades(instances: List[harness.Instance]):
                 "'recent' requires the number of releases as second argument and the flavour as third argument"
             )
         _, num_channels, flavour = channels
-        arch = cp.exec(["dpkg", "--print-architecture"]).strip()
+        arch = cp.exec(["dpkg", "--print-architecture"], text=True).stdout.strip()
         channels = snap.get_latest_channels(num_channels, flavour, arch)
 
     LOG.info(
