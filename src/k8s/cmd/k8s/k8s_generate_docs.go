938e4320ef7cf8ba73a0c6efceb669932e1a8216
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2024-10-02 11:39:44 +0000

    Automatically generate the bootstrap config documentation
    The bootstrap config documentation may get out of sync as a result
    of apiv1.BootstrapConfig changes.
    
    To avoid this, we're going to automatically generate the documentation
    based on the apiv1.BootstrapConfig structure definition and docstrings.
    
    This change adds the necessary tooling as part of the ``docgen``
    package, which may be reused for other structures or projects.

--- src/k8s/cmd/k8s/k8s_generate_docs.go
+++ src/k8s/cmd/k8s/k8s_generate_docs.go
@@ -1,7 +1,11 @@
 package k8s
 
 import (
+	"os"
+
+	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
 	cmdutil "github.com/canonical/k8s/cmd/util"
+	"github.com/canonical/k8s/pkg/docgen"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
@@ -15,11 +19,29 @@ func newGenerateDocsCmd(env cmdutil.ExecutionEnvironment) *cobra.Command {
 		Hidden: true,
 		Short:  "Generate markdown documentation",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := doc.GenMarkdownTree(cmd.Parent(), opts.outputDir); err != nil {
+			if err := doc.GenMarkdownTree(cmd.Parent(), opts.outputDir+"/commands"); err != nil {
 				cmd.PrintErrf("Error: Failed to generate markdown documentation for k8s command.\n\nThe error was: %v\n", err)
 				env.Exit(1)
 				return
 			}
+
+			bootstrap_doc, err := docgen.MarkdownFromJsonStruct(apiv1.BootstrapConfig{})
+			if err != nil {
+				cmd.PrintErrf("Error: Failed to generate markdown documentation for bootstrap configuration\n\n")
+				cmd.PrintErrf("Error: %v", err)
+				env.Exit(1)
+				return
+			}
+
+			bootstrap_doc_path := opts.outputDir + "/bootstrap_config.md"
+			err = os.WriteFile(bootstrap_doc_path, []byte(bootstrap_doc), 0644)
+			if err != nil {
+				cmd.PrintErrf("Error: Failed to write markdown documentation for bootstrap configuration\n\n")
+				cmd.PrintErrf("Error: %v")
+				env.Exit(1)
+				return
+			}
+
 			cmd.Printf("Generated documentation in %s\n", opts.outputDir)
 		},
 	}
---
+++ src/k8s/pkg/docgen/gomod.go
@@ -0,0 +1,125 @@
+package docgen
+
+import (
+	"fmt"
+	"golang.org/x/mod/modfile"
+	"golang.org/x/mod/module"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+func getGoDepModulePath(name string, version string) (string, error) {
+	cachePath := os.Getenv("GOMODCACHE")
+	if cachePath == "" {
+		goPath := os.Getenv("GOPATH")
+		if goPath == "" {
+			goPath = path.Join(os.Getenv("HOME"), "/go")
+		}
+		cachePath = path.Join(goPath, "pkg", "mod")
+	}
+
+	escapedPath, err := module.EscapePath(name)
+	if err != nil {
+		return "", fmt.Errorf(
+			"couldn't escape module path: %s %v", name, err)
+	}
+
+	escapedVersion, err := module.EscapeVersion(version)
+	if err != nil {
+		return "", fmt.Errorf(
+			"couldn't escape module version: %s %v", version, err)
+	}
+
+	path := path.Join(cachePath, escapedPath+"@"+escapedVersion)
+
+	// Validate the path.
+	if _, err := os.Stat(path); err != nil {
+		return "", fmt.Errorf(
+			"Go module path not accessible: %s %s %s. Error: %v.",
+			name, version, path, err)
+	}
+
+	return path, nil
+}
+
+func getDependencyVersionFromGoMod(goModPath string, packageName string, directOnly bool) (string, string, error) {
+	goModContents, err := os.ReadFile(goModPath)
+	if err != nil {
+		return "", "", fmt.Errorf("could not read go.mod file %s. Error: ", goModPath, err)
+	}
+	goModFile, err := modfile.ParseLax(goModPath, goModContents, nil)
+	if err != nil {
+		return "", "", fmt.Errorf("could not parse go.mod file %s. Error: ", goModPath, err)
+	}
+
+	for _, dep := range goModFile.Require {
+		if directOnly && dep.Indirect {
+			continue
+		}
+		if strings.HasPrefix(packageName, dep.Mod.Path) {
+			return dep.Mod.Path, dep.Mod.Version, nil
+		}
+	}
+
+	return "", "", fmt.Errorf("could not find dependency %s in %s", packageName, goModPath)
+}
+
+// getProjectDir retrieves the full path of k8s-snap/src/k8s.
+// For simplicity, we assume that the executable is placed in
+// k8s-snap/src/k8s/bin/(static|dynamic).
+// This will mostly be used to generate the project documentation using
+// "make go.doc".
+func getProjectDir() (string, error) {
+	exec, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("couldn't retrieve executable path, error: %v", err)
+	}
+
+	projDir := path.Join(filepath.Dir(exec), "..", "..")
+	return filepath.Abs(projDir)
+}
+
+func getGoModPath() (string, error) {
+	projDir, err := getProjectDir()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(projDir, "go.mod"), nil
+}
+
+func getGoPackageDir(packageName string) (string, error) {
+	if packageName == "" {
+		return "", fmt.Errorf("could not retrieve package dir, no package name specified.")
+	}
+
+	if strings.HasPrefix(packageName, "github.com/canonical/k8s/") {
+		projDir, err := getProjectDir()
+		if err != nil {
+			return "", err
+		}
+
+		return strings.Replace(packageName, "github.com/canonical/k8s", projDir, 1), nil
+	}
+
+	// Dependency, need to retrieve its version from go.mod
+	goModPath, err := getGoModPath()
+	if err != nil {
+		return "", err
+	}
+
+	basePackageName, version, err := getDependencyVersionFromGoMod(goModPath, packageName, false)
+	if err != nil {
+		return "", err
+	}
+
+	basePath, err := getGoDepModulePath(basePackageName, version)
+	if err != nil {
+		return "", err
+	}
+
+	subPath := strings.TrimPrefix(packageName, basePackageName)
+	return path.Join(basePath, subPath), nil
+}
