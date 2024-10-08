7911a75d48ec94dc483abe6ca25cb4d6a13114ff
Maciej Golaszewski <maciej.golaszewski@canonical.com>
2024-10-08 13:03:26 +0200

    test15

--- src/k8s/hack/static-go-lint.sh
+++ src/k8s/hack/static-go-lint.sh
@@ -4,6 +4,9 @@ DIR="$(realpath `dirname "${0}"`)"
 
 . "${DIR}/static-dqlite.sh"
 #	golangci-lint run -c ./../../golangci.yml -v
+
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
+
 golangci-lint run \
   --build-tags dqlite,libsqlite3 \
   "${@}"
