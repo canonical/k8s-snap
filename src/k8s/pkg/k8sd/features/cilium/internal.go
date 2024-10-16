d75ac08ac1f8eb046a4c5e0a04fe481a840046b6
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2024-10-16 11:35:17 +0000

    Move feature annotations to k8s-snapa-api

--- src/k8s/pkg/k8sd/features/cilium/internal.go
+++ src/k8s/pkg/k8sd/features/cilium/internal.go
@@ -1,17 +1,8 @@
 package cilium
 
 import (
+	apiv1_annotations "github.com/canonical/k8s-snap-api/api/v1/annotations/cilium"
 	"github.com/canonical/k8s/pkg/k8sd/types"
-)
-
-const (
-	// List of devices facing cluster/external network (used for BPF NodePort, BPF masquerading and host firewall); supports '+' as wildcard in device name, e.g. 'eth+'
-	// e.g. k8sd/v1alpha1/cilium/devices="eth+ lxdbr+"
-	annotationDevices = "k8sd/v1alpha1/cilium/devices"
-
-	// Device name used to connect nodes in direct routing mode (used by BPF NodePort, BPF host routing; if empty, automatically set to a device with k8s InternalIP/ExternalIP or with a default route
-	// bridge type devices are ingored in automatic selection
-	annotationDirectRoutingDevice = "k8sd/v1alpha1/cilium/direct-routing-device"
 )
 
 type config struct {
@@ -22,11 +13,11 @@ type config struct {
 func internalConfig(annotations types.Annotations) (config, error) {
 	c := config{}
 
-	if v, ok := annotations.Get(annotationDevices); ok {
+	if v, ok := annotations.Get(apiv1_annotations.AnnotationDevices); ok {
 		c.devices = v
 	}
 
-	if v, ok := annotations.Get(annotationDirectRoutingDevice); ok {
+	if v, ok := annotations.Get(apiv1_annotations.AnnotationDirectRoutingDevice); ok {
 		c.directRoutingDevice = v
 	}
 
