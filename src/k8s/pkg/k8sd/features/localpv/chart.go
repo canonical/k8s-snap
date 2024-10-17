ed603b9a800bd66b7afea4ce6037c95930592c7d
Adam Dyess <adam.dyess@canonical.com>
2024-10-17 16:49:39 -0500

    Update to arm64 versions of the same images

--- src/k8s/pkg/k8sd/features/localpv/chart.go
+++ src/k8s/pkg/k8sd/features/localpv/chart.go
@@ -17,7 +17,7 @@ var (
 	// imageRepo is the repository to use for Rawfile LocalPV CSI.
 	imageRepo = "ghcr.io/canonical/rawfile-localpv"
 	// imageTag is the image tag to use for Rawfile LocalPV CSI.
-	imageTag = "0.8.0-ck5"
+	imageTag = "0.8.0-ck4"
 
 	// csiNodeDriverImage is the image to use for the CSI node driver.
 	csiNodeDriverImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-node-driver-registrar:v2.10.1"
