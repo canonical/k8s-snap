76611e2f4c852da1547ea921232b6d1b3e70ee26
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-10-20 18:15:09 +0200

    use metallb loadbalancer instead of cilium

--- src/k8s/pkg/k8sd/features/implementation_default.go
+++ src/k8s/pkg/k8sd/features/implementation_default.go
@@ -4,18 +4,20 @@ import (
 	"github.com/canonical/k8s/pkg/k8sd/features/cilium"
 	"github.com/canonical/k8s/pkg/k8sd/features/coredns"
 	"github.com/canonical/k8s/pkg/k8sd/features/localpv"
+	"github.com/canonical/k8s/pkg/k8sd/features/metallb"
 	metrics_server "github.com/canonical/k8s/pkg/k8sd/features/metrics-server"
 )
 
 // Default implements the Canonical Kubernetes built-in features.
-// Cilium is used for networking (network + load-balancer + ingress + gateway).
+// Cilium is used for networking (network + ingress + gateway).
+// MetalLB is used for LoadBalancer.
 // CoreDNS is used for DNS.
 // MetricsServer is used for metrics-server.
 // LocalPV Rawfile CSI is used for local-storage.
 var Implementation Interface = &implementation{
 	applyDNS:           coredns.ApplyDNS,
 	applyNetwork:       cilium.ApplyNetwork,
-	applyLoadBalancer:  cilium.ApplyLoadBalancer,
+	applyLoadBalancer:  metallb.ApplyLoadBalancer,
 	applyIngress:       cilium.ApplyIngress,
 	applyGateway:       cilium.ApplyGateway,
 	applyMetricsServer: metrics_server.ApplyMetricsServer,
