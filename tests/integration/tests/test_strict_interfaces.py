739c43b1d1ed7584c7dbed0fd1c4711bef83f9dc
louiseschmidtgen <louise.schmidtgen@canonical.com>
2024-10-21 19:05:16 +0200

    refactoring grab channels

--- tests/integration/tests/test_strict_interfaces.py
+++ tests/integration/tests/test_strict_interfaces.py
@@ -12,7 +12,6 @@ LOG = logging.getLogger(__name__)
 @pytest.mark.skipif(
     not config.STRICT_INTERFACE_CHANNELS, reason="No strict channels configured"
 )
-
 def test_strict_interfaces(instances: List[harness.Instance], tmp_path):
     channels = config.STRICT_INTERFACE_CHANNELS
     cp = instances[0]
@@ -24,8 +23,7 @@ def test_strict_interfaces(instances: List[harness.Instance], tmp_path):
                 "'recent' requires the number of releases as second argument and the flavour as third argument"
             )
         _, num_channels, flavour = channels
-        channels = snap.get_latest_channels(int(num_channels), flavour, cp.arch, minimum_risk=True)
-        current_channel = channels[0]
+        channels = snap.get_channels(int(num_channels), flavour, cp.arch, "edge", True)
 
     for channel in channels:
         util.setup_k8s_snap(cp, tmp_path, channel, connect_interfaces=False)
@@ -38,33 +36,32 @@ def test_strict_interfaces(instances: List[harness.Instance], tmp_path):
 
         cp.exec(
             ["snap", "remove", config.SNAP_NAME, "--purge"]
-        )     
-
+        )
        
 def check_snap_interfaces(cp, snap_name):
     """Check the strict snap interfaces."""
     interfaces = [
-    "docker-privileged",
-    "kubernetes-support",
-    "network",
-    "network-bind",
-    "network-control",
-    "network-observe",
-    "firewall-control",
-    "process-control",
-    "kernel-module-observe",
-    "cilium-module-load",
-    "mount-observe",
-    "hardware-observe",
-    "system-observe",
-    "home",
-    "opengl",
-    "home-read-all",
-    "login-session-observe",
-    "log-observe",
+        "docker-privileged",
+        "kubernetes-support",
+        "network",
+        "network-bind",
+        "network-control",
+        "network-observe",
+        "firewall-control",
+        "process-control",
+        "kernel-module-observe",
+        "cilium-module-load",
+        "mount-observe",
+        "hardware-observe",
+        "system-observe",
+        "home",
+        "opengl",
+        "home-read-all",
+        "login-session-observe",
+        "log-observe",
     ]
     for interface in interfaces:
         cp.exec(
-                ["snap", "run", "--shell", snap_name, "-c", f"snapctl is-connected {interface}"],
-                check=True
-            )
+            ["snap", "run", "--shell", snap_name, "-c", f"snapctl is-connected {interface}"],
+            check=True
+        )
