08064e9b249e18595038d9042ae7e7a6b51ac4f3
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-11-11 11:29:00 -0500

    add integration test

---
+++ tests/integration/tests/test_util/test_bootstrap.py
@@ -0,0 +1,12 @@
+from typing import List
+
+import pytest
+from test_util import harness
+
+@pytest.mark.node_count(1)
+@pytest.mark.disable_k8s_bootstrapping()
+def test_microk8s_installed(instances: List[harness.Instance]):
+    instance = instances[0]
+    instance.exec("snap install microk8s --classic".split())
+    result = instance.exec("k8s bootstrap".split(), capture_output=True, check=False)
+    assert "Error: microk8s snap is installed" in result.stderr.decode()
