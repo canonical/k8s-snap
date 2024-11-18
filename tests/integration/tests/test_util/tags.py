1fc014bebce00b84dedc686ed813e0cc8403ab22
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-11-18 15:23:01 +0100

    add testing level tags

---
+++ tests/integration/tests/test_util/tags.py
@@ -0,0 +1,12 @@
+#
+# Copyright 2024 Canonical, Ltd.
+#
+from pytest_tagging import combine_tags
+
+PULL_REQUEST = "pull_request"
+NIGHTLY = "nightly"
+WEEKLY = "weekly"
+
+# Those tags can be used for a convenient way to run multiple test levels.
+combine_tags("up_to_nightly", PULL_REQUEST, NIGHTLY)
+combine_tags("up_to_weekly", PULL_REQUEST, NIGHTLY, WEEKLY)
