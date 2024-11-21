f866fa72c1b282b71e5a743dc8c2fcbaba873ef4
Claudiu Belu <claudiu.belu@canonical.com>
2024-11-21 14:42:25 +0000

    Cleans up all containerd-related resources on snap removal
    Currently, when removing the snap, the /var/run/containerd folder is not
    properly removed, as it is a folder. This fixes this issue.
    
    Additionally removes other containerd-related folders: /etc/containerd
    and /var/lib/containerd.
    
    We're also removing /opt/cni/bin on snap removal, which is created when
    bootstrapping the node. As we're removing the k8s snap, we no longer
    need this folder either.

--- tests/integration/tests/test_cleanup.py
+++ tests/integration/tests/test_cleanup.py
@@ -9,6 +9,13 @@ from test_util import harness, util
 
 LOG = logging.getLogger(__name__)
 
+CONTAINERD_PATHS = [
+    "/etc/containerd",
+    "/opt/cni/bin",
+    "/run/containerd",
+    "/var/lib/containerd",
+]
+
 
 @pytest.mark.node_count(1)
 def test_node_cleanup(instances: List[harness.Instance]):
@@ -17,3 +24,10 @@ def test_node_cleanup(instances: List[harness.Instance]):
     util.wait_for_network(instance)
 
     util.remove_k8s_snap(instance)
+
+    # Check that the containerd-related folders are removed on snap removal.
+    process = instance.exec(
+        ["ls", *CONTAINERD_PATHS], capture_output=True, text=True, check=False
+    )
+    for path in CONTAINERD_PATHS:
+        assert f"cannot access '{path}': No such file or directory" in process.stderr
