b78e045e02c01d8f916171b33823519fa6df6ca4
Claudiu Belu <claudiu.belu@canonical.com>
2024-11-22 06:55:49 +0000

    Adds node cleanup integration test for new containerd path

--- tests/integration/tests/test_cleanup.py
+++ tests/integration/tests/test_cleanup.py
@@ -2,19 +2,21 @@
 # Copyright 2024 Canonical, Ltd.
 #
 import logging
+import os
 from typing import List
 
 import pytest
-from test_util import harness, tags, util
+import yaml
+from test_util import config, harness, tags, util
 
 LOG = logging.getLogger(__name__)
 
 CONTAINERD_PATHS = [
     "/etc/containerd",
-    "/opt/cni/bin",
     "/run/containerd",
     "/var/lib/containerd",
 ]
+CNI_PATH = "/opt/cni/bin"
 
 
 @pytest.mark.node_count(1)
@@ -27,11 +29,65 @@ def test_node_cleanup(instances: List[harness.Instance], tmp_path):
     util.remove_k8s_snap(instance)
 
     # Check that the containerd-related folders are removed on snap removal.
+    all_paths = CONTAINERD_PATHS + [CNI_PATH]
     process = instance.exec(
-        ["ls", *CONTAINERD_PATHS], capture_output=True, text=True, check=False
+        ["ls", *all_paths], capture_output=True, text=True, check=False
     )
-    for path in CONTAINERD_PATHS:
+    for path in all_paths:
         assert f"cannot access '{path}': No such file or directory" in process.stderr
 
     util.setup_k8s_snap(instance, tmp_path)
     instance.exec(["k8s", "bootstrap"])
+
+
+@pytest.mark.node_count(2)
+@pytest.mark.disable_k8s_bootstrapping()
+@pytest.mark.tags(tags.NIGHTLY)
+def test_node_cleanup_new_containerd_path(instances: List[harness.Instance]):
+    main = instances[0]
+    joiner = instances[1]
+
+    containerd_path_bootstrap_config = (
+        config.MANIFESTS_DIR / "bootstrap-containerd-path.yaml"
+    ).read_text()
+
+    main.exec(
+        ["k8s", "bootstrap", "--file", "-"],
+        input=str.encode(containerd_path_bootstrap_config),
+    )
+
+    join_token = util.get_join_token(main, joiner)
+    joiner.exec(
+        ["k8s", "join-cluster", join_token, "--file", "-"],
+        input=str.encode(containerd_path_bootstrap_config),
+    )
+
+    boostrap_config = yaml.safe_load(containerd_path_bootstrap_config)
+    new_containerd_paths = [
+        os.path.join(boostrap_config["containerd-base-dir"], "k8s-containerd", p)
+        for p in CONTAINERD_PATHS
+    ]
+    for instance in instances:
+        # Check that the containerd-related folders are not in the default locations.
+        process = instance.exec(
+            ["ls", *CONTAINERD_PATHS], capture_output=True, text=True, check=False
+        )
+        for path in CONTAINERD_PATHS:
+            assert (
+                f"cannot access '{path}': No such file or directory" in process.stderr
+            )
+
+        # Check that the containerd-related folders are in the new locations.
+        # If one of them is missing, this should have a non-zero exit code.
+        instance.exec(["ls", *new_containerd_paths], check=True)
+
+    for instance in instances:
+        # Check that the containerd-related folders are not in the new locations after snap removal.
+        util.remove_k8s_snap(instance)
+        process = instance.exec(
+            ["ls", *new_containerd_paths], capture_output=True, text=True, check=False
+        )
+        for path in new_containerd_paths:
+            assert (
+                f"cannot access '{path}': No such file or directory" in process.stderr
+            )
