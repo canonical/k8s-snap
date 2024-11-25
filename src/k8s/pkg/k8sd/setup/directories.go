ec2081ebd87113bd4fa045546beb848b7b04323d
Maciek Gołaszewski <maciej.golaszewski@canonical.com>
2024-11-25 15:22:49 +0100

    Test file creation in CNI dir (#832)

--- src/k8s/pkg/k8sd/setup/directories.go
+++ src/k8s/pkg/k8sd/setup/directories.go
@@ -82,5 +82,16 @@ func ensureCniBinDir(cniBinDir string) error {
 		}
 	}
 
+	f, err := os.CreateTemp(cniBinDir, "test*.txt")
+	if err != nil {
+		return fmt.Errorf("failed create file in %q: %w", cniBinDir, err)
+	}
+	if err = f.Close(); err != nil {
+		return fmt.Errorf("failed close file in %q: %w", cniBinDir, err)
+	}
+	if err = os.Remove(f.Name()); err != nil {
+		return fmt.Errorf("failed delete file in %q: %w", cniBinDir, err)
+	}
+
 	return nil
 }
