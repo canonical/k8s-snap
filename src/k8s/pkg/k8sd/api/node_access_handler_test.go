6d469e27aad5ee47f7dff3b53a43daf378beb575
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-12-09 12:29:17 -0500

    provider mock, accessnodehandler test

---
+++ src/k8s/pkg/k8sd/api/node_access_handler_test.go
@@ -0,0 +1,90 @@
+package api
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/canonical/k8s/pkg/snap"
+	"github.com/canonical/k8s/pkg/snap/mock"
+	. "github.com/onsi/gomega"
+)
+
+func TestValidateNodeTokenAccessHandler(t *testing.T) {
+	g := NewWithT(t)
+
+	const tokenFileName = "tmp-token-file"
+	const tokenHeaderName = "X-Node-Token"
+
+	tempDir := os.TempDir()
+	defer os.RemoveAll(tempDir)
+
+	for _, tc := range []struct {
+		tokenHeaderContent string
+		tokenFileContent   string
+		expectErr          bool
+		createFile         bool
+	}{
+		{
+			tokenHeaderContent: "node-token",
+			tokenFileContent:   "node-token",
+			createFile:         true,
+		},
+		{
+			tokenHeaderContent: "node-token",
+			tokenFileContent:   "different-node-token",
+			expectErr:          true,
+			createFile:         true,
+		},
+		{
+			tokenHeaderContent: "",
+			tokenFileContent:   "node-token",
+			expectErr:          true,
+			createFile:         true,
+		},
+		{
+			tokenHeaderContent: "node-token",
+			tokenFileContent:   "node-token",
+			expectErr:          true,
+		},
+	} {
+		var err error
+		if tc.createFile {
+			err = os.WriteFile(path.Join(tempDir, tokenFileName), []byte(tc.tokenFileContent), 0o644)
+			g.Expect(err).To(BeNil())
+		}
+
+		e := &Endpoints{
+			context: context.Background(),
+			provider: &mock.Provider{
+				SnapFn: func() snap.Snap {
+					return &mock.Snap{
+						Mock: mock.Mock{
+							NodeTokenFile: path.Join(tempDir, tokenFileName),
+						},
+					}
+				},
+			},
+		}
+
+		req := &http.Request{
+			Header: make(http.Header),
+		}
+		req.Header.Set(tokenHeaderName, tc.tokenHeaderContent)
+
+		handler := e.ValidateNodeTokenAccessHandler(tokenHeaderName)
+		valid, resp := handler(nil, req)
+
+		os.Remove(path.Join(tempDir, tokenFileName))
+
+		if tc.expectErr {
+			g.Expect(valid).To(BeFalse())
+			g.Expect(resp).NotTo(BeNil())
+		} else {
+			g.Expect(valid).To(BeTrue())
+			g.Expect(resp).To(BeNil())
+		}
+	}
+}
