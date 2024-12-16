d32126d0b8931493efece079f0ef7b926dc6fd29
Nashwan Azhari <nashwan.azhari@canonical.com>
2024-12-16 09:04:21 +0200

    fix: ensure containerd-related directories removed on failed `bootstrap/join-cluster`. (#863)
    * fix: ensure containerd-related directories removed on failed `bootstrap/join-cluster`
    
    `k8sd` automatically sets up some directories with the appropriate
    ownership/permissions to be used by containerd in the early stages
    of the `bootstrap` and `join-cluster` commands.
    
    In the classic (non-strict) version of the k8s-snap, these
    containerd directories are system-wide (e.g. `/etc/containerd`,
    `/run/containerd`, etc).
    
    Should any of the other setup steps fail after the containerd
    directories were set up, the directories would still remain on
    disk and thus lead to a 'partial installation' of on the host system.
    
    This patch ensures that `k8s` will automatically remove any
    containerd-related directories which were created in the event of
    the  `bootstrap` / `join-cluster` commands failing.
    
    Signed-off-by: Nashwan Azhari <nashwan.azhari@canonical.com>
    
    * fix: ensure containerd Base Dir lockfile is never accidentally deleted.
    
    The containerd Base Dir is the special path all other containerd-related
    paths on the snap are derived from.
    
    Under classic confinement and default settings, this path defaults
    to the host's root (`/`), and thus extreme care must be taken to
    not accidentally include it in k8sd's cleanup routine or the
    k8s-snap's remove hook.
    
    Signed-off-by: Nashwan Azhari <nashwan.azhari@canonical.com>
    
    ---------
    
    Signed-off-by: Nashwan Azhari <nashwan.azhari@canonical.com>

--- tests/integration/tests/test_cleanup.py
+++ tests/integration/tests/test_cleanup.py
@@ -11,6 +11,8 @@ from test_util import config, harness, tags, util
 
 LOG = logging.getLogger(__name__)
 
+KUBE_CONTROLLER_MANAGER_SNAP_PORT = 10257
+
 CONTAINERD_PATHS = [
     "/etc/containerd",
     "/run/containerd",
@@ -19,9 +21,22 @@ CONTAINERD_PATHS = [
 CNI_PATH = "/opt/cni/bin"
 
 
+def _assert_paths_not_exist(instance: harness.Instance, paths: List[str]):
+    paths_which_exist = [
+        p
+        for p, exists in util.check_file_paths_exist(instance, paths).items()
+        if exists
+    ]
+    if paths_which_exist:
+        raise AssertionError(
+            f"Expected the following path(s) to not exist: {paths_which_exist}"
+        )
+
+
 @pytest.mark.node_count(1)
 @pytest.mark.tags(tags.NIGHTLY)
 def test_node_cleanup(instances: List[harness.Instance], tmp_path):
+    """Verifies that a `snap remove k8s` will perform proper cleanup."""
     instance = instances[0]
     util.wait_for_dns(instance)
     util.wait_for_network(instance)
@@ -30,11 +45,7 @@ def test_node_cleanup(instances: List[harness.Instance], tmp_path):
 
     # Check that the containerd-related folders are removed on snap removal.
     all_paths = CONTAINERD_PATHS + [CNI_PATH]
-    process = instance.exec(
-        ["ls", *all_paths], capture_output=True, text=True, check=False
-    )
-    for path in all_paths:
-        assert f"cannot access '{path}': No such file or directory" in process.stderr
+    _assert_paths_not_exist(instance, all_paths)
 
     util.setup_k8s_snap(instance, tmp_path)
     instance.exec(["k8s", "bootstrap"])
@@ -88,18 +99,60 @@ def test_node_cleanup_new_containerd_path(instances: List[harness.Instance]):
             assert (
                 f"cannot access '{path}': No such file or directory" in process.stderr
             )
+        _assert_paths_not_exist(instance, exp_missing_paths)
 
         # Check that the containerd-related folders are in the new locations.
         # If one of them is missing, this should have a non-zero exit code.
         instance.exec(["ls", *new_containerd_paths], check=True)
 
     for instance in instances:
-        # Check that the containerd-related folders are not in the new locations after snap removal.
         util.remove_k8s_snap(instance)
-        process = instance.exec(
-            ["ls", *new_containerd_paths], capture_output=True, text=True, check=False
+        # Check that the containerd-related folders are not in the new locations after snap removal.
+        _assert_paths_not_exist(instance, new_containerd_paths)
+
+
+@pytest.mark.node_count(1)
+@pytest.mark.no_setup()
+@pytest.mark.tags(tags.NIGHTLY)
+def test_containerd_path_cleanup_on_failed_init(
+    instances: List[harness.Instance], tmp_path
+):
+    """Tests that a failed `bootstrap` properly cleans up any
+    containerd-related paths it may have created as part of the
+    failed `bootstrap`.
+
+    It induces a bootstrap failure by pre-binding a required k8s service
+    port (10257 for the kube-controller-manager) before running `k8s bootstrap`.
+
+    NOTE: a failed `join-cluster` will trigger the exact same cleanup
+    hook, so the test implicitly applies to it as well.
+    """
+    instance = instances[0]
+    expected_code = 1
+    expected_message = (
+        "Encountered error(s) while verifying port availability for Kubernetes "
+        "services: Port 10257 (needed by: kube-controller-manager) is already in use."
+    )
+
+    with util.open_port(KUBE_CONTROLLER_MANAGER_SNAP_PORT) as _:
+        util.setup_k8s_snap(instance, tmp_path, config.SNAP, connect_interfaces=False)
+
+        proc = instance.exec(
+            ["k8s", "bootstrap"], capture_output=True, text=True, check=False
         )
-        for path in new_containerd_paths:
-            assert (
-                f"cannot access '{path}': No such file or directory" in process.stderr
+
+        if proc.returncode != expected_code:
+            raise AssertionError(
+                f"Expected `k8s bootstrap` to exit with code {expected_code}, "
+                f"but it exited with {proc.returncode}.\n"
+                f"Stdout was: \n{proc.stdout}.\nStderr was: \n{proc.stderr}"
             )
+
+        if expected_message not in proc.stderr:
+            raise AssertionError(
+                f"Expected to find port-related warning '{expected_message}' in "
+                "stderr of the `k8s bootstrap` command.\n"
+                f"Stdout was: \n{proc.stdout}.\nStderr was: \n{proc.stderr}"
+            )
+
+        _assert_paths_not_exist(instance, CONTAINERD_PATHS)
