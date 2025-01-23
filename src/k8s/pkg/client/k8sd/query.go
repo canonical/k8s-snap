256aa4e1d70e7aed15a43e9ef0ac6860b286690d
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-01-23 09:28:22 +0100

    address comments

--- src/k8s/pkg/client/k8sd/query.go
+++ src/k8s/pkg/client/k8sd/query.go
@@ -9,12 +9,16 @@ import (
 	"github.com/canonical/k8s/pkg/log"
 	"github.com/canonical/k8s/pkg/utils/control"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/microcluster/v2/rest/types"
 )
 
 // query is a helper method for sending requests to the k8sd client with common error checking and automatic retries.
 // It retries on temporary microcluster errors and returns the deserialized response.
 func query[T any](ctx context.Context, c *k8sd, method, path string, in any, out *T) (T, error) {
 	var result T
+	if out == nil {
+		return result, fmt.Errorf("out must be a non-nil pointer")
+	}
 
 	retryErr := control.WaitUntilReady(ctx, func() (bool, error) {
 		err := c.client.Query(ctx, method, apiv1.K8sdAPIVersion, api.NewURL().Path(strings.Split(path, "/")...), in, out)
@@ -36,7 +40,12 @@ func query[T any](ctx context.Context, c *k8sd, method, path string, in any, out
 }
 
 // isTemporary checks if an error is temporary and should be retried.
-// This function is tighly coupled with the error messages returned by microcluster and should not contain any genery error checks.
+// This function is tightly coupled with the error messages returned by microcluster and
+// should not contain any generic error checks.
 func isTemporary(err error) bool {
-	return strings.Contains(err.Error(), "Database is still starting")
+	if strings.Contains(err.Error(), string(types.DatabaseStarting)) ||
+		strings.Contains(err.Error(), "Database is not yet ready") {
+		return true
+	}
+	return false
 }
