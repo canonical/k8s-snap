578e1f8e3948daced888ffd76e2ca8fabcbd6199
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-01-28 13:26:50 +0000

    Clean up the log messages

--- src/k8s/pkg/client/k8sd/query.go
+++ src/k8s/pkg/client/k8sd/query.go
@@ -28,10 +28,11 @@ func query[T any](ctx context.Context, c *k8sd, method, path string, in any, out
 				return false, nil
 			}
 			if bootstrapPending(err) {
-				log.FromContext(ctx).Info(
-					"The cluster hasn't been initialized yet. " +
-					"Please use 'sudo k8s bootstrap' to create a new cluster " +
-					"or 'sudo k8s join-cluster' to join an existing one.")
+				return false, fmt.Errorf(
+					"the cluster hasn't been initialized yet. "+
+						"Please use 'sudo k8s bootstrap' to create a new cluster "+
+						"or 'sudo k8s join-cluster' to join an existing one."+
+						" Attempted k8sd query: %s /%s: %w", method, path, err)
 			}
 			return false, fmt.Errorf("failed to %s /%s: %w", method, path, err)
 		}
