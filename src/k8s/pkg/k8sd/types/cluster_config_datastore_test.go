42d19f0084cc9f25d6738421ff2763355e1edc67
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-02-05 10:44:26 +0000

    dbg: enable WatchList
    This feature should work with the latest k8s-dqlite version, let's
    try enabling it to see if this fixes our kubelet issue, which
    seems to miss pod events.

--- src/k8s/pkg/k8sd/types/cluster_config_datastore_test.go
+++ src/k8s/pkg/k8sd/types/cluster_config_datastore_test.go
@@ -34,7 +34,6 @@ func TestDatastoreToKubeAPIServerArguments(t *testing.T) {
 			},
 			expectUpdateArgs: map[string]string{
 				"--etcd-servers":  "unix:///k8s-dqlite/k8s-dqlite.sock",
-				"--feature-gates": "WatchList=false",
 			},
 			expectDeleteArgs: []string{"--etcd-cafile", "--etcd-certfile", "--etcd-keyfile"},
 		},
