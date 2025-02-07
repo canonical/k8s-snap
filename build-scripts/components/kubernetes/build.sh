ce1c2db8390512f60d61f9f2a844d166bfebaa4a
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2025-02-06 22:03:27 -0500

    fips dqlite/k8s-dqlite

--- build-scripts/components/kubernetes/build.sh
+++ build-scripts/components/kubernetes/build.sh
@@ -6,7 +6,11 @@ mkdir -p "${INSTALL}"
 export KUBE_GIT_VERSION_FILE="${PWD}/.version.sh"
 
 for app in kubernetes; do
-  make GOEXPERIMENT=opensslcrypto WHAT="cmd/${app}" KUBE_CGO_OVERRIDES="${app}" GOFLAGS="-tags=providerless,goexperiment.systemcrypto,linux,cgo"
+  # We are setting allowcryptofallback here. Normally this is not recommended because the user could
+  # be led to think their system is compliant when the binaries have actually fallen back to non-FIPS compliant crypto.
+  # We do this because we want to have a single branch/track for FIPS and non-FIPS deployments. In this scenario,
+  # we don't have a choice to allow fallback.
+  make GOEXPERIMENT=opensslcrypto WHAT="cmd/${app}" KUBE_CGO_OVERRIDES="${app}" GOFLAGS="-tags=providerless,goexperiment.systemcrypto,linux,cgo,allowcryptofallback"
   cp _output/bin/"${app}" "${INSTALL}/${app}"
 done
 
