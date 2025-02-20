301cddd558cfeb9fe666a079050233c4cf6eca8d
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2025-02-20 15:04:04 -0500

    bump chart, image

--- src/k8s/pkg/k8sd/features/coredns/chart.go
+++ src/k8s/pkg/k8sd/features/coredns/chart.go
@@ -11,12 +11,12 @@ var (
 	Chart = helm.InstallableChart{
 		Name:         "ck-dns",
 		Namespace:    "kube-system",
-		ManifestPath: filepath.Join("charts", "coredns-1.36.0.tgz"),
+		ManifestPath: filepath.Join("charts", "coredns-1.36.2.tgz"),
 	}
 
 	// imageRepo is the image to use for CoreDNS.
 	imageRepo = "ghcr.io/canonical/coredns"
 
 	// ImageTag is the tag to use for the CoreDNS image.
-	ImageTag = "1.11.3-ck0"
+	ImageTag = "1.11.4-ck2"
 )
