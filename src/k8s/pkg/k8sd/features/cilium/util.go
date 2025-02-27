d20d50678e490349743bd7cdf0579c4aad772942
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2025-02-27 15:00:22 +0300

    feat(k8sd): introduce feature packages and update feature apply functions
    - Added the FeatureManifest struct that contains the required charts and images for the feature.
    - Features are updated to use feature manifests when reconciling through the feature controller.
    - Implementation modules under `features` are updated to contain sub modules per feature.
    - Values handling is separated from the `Apply` functions and is performed via helper functions instead.
    - Introduced helper `Values` types and related functions for merging values together using mergo.

---
+++ src/k8s/pkg/k8sd/features/cilium/util.go
@@ -0,0 +1,36 @@
+package cilium
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/canonical/k8s/pkg/snap"
+	"github.com/canonical/k8s/pkg/utils/control"
+)
+
+func RolloutRestartCilium(ctx context.Context, snap snap.Snap, attempts int) error {
+	client, err := snap.KubernetesClient("")
+	if err != nil {
+		return fmt.Errorf("failed to create kubernetes client: %w", err)
+	}
+
+	if err := control.RetryFor(ctx, attempts, 0, func() error {
+		if err := client.RestartDeployment(ctx, "cilium-operator", "kube-system"); err != nil {
+			return fmt.Errorf("failed to restart cilium-operator deployment: %w", err)
+		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("failed to restart cilium-operator deployment after %d attempts: %w", attempts, err)
+	}
+
+	if err := control.RetryFor(ctx, attempts, 0, func() error {
+		if err := client.RestartDaemonset(ctx, "cilium", "kube-system"); err != nil {
+			return fmt.Errorf("failed to restart cilium daemonset: %w", err)
+		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("failed to restart cilium daemonset after %d attempts: %w", attempts, err)
+	}
+
+	return nil
+}
---
+++ src/k8s/pkg/k8sd/features/localpv/local-storage/register.go
@@ -0,0 +1,25 @@
+package local_storage
+
+import (
+	"fmt"
+
+	"github.com/canonical/k8s/pkg/k8sd/images"
+)
+
+func init() {
+	rawFileImage := FeatureLocalStorage.GetImage(RawFileImageName)
+	csiNodeDriverImage := FeatureLocalStorage.GetImage(CSINodeDriverImageName)
+	csiProvisionerImage := FeatureLocalStorage.GetImage(CSIProvisionerImageName)
+	csiResizerImage := FeatureLocalStorage.GetImage(CSIResizerImageName)
+	csiSnapshotterImage := FeatureLocalStorage.GetImage(CSISnapshotterImageName)
+
+	images.Register(
+		// Rawfile LocalPV CSI driver images
+		fmt.Sprintf("%s:%s", rawFileImage.GetURI(), rawFileImage.Tag),
+		// CSI images
+		fmt.Sprintf("%s:%s", csiNodeDriverImage.GetURI(), csiNodeDriverImage.Tag),
+		fmt.Sprintf("%s:%s", csiProvisionerImage.GetURI(), csiProvisionerImage.Tag),
+		fmt.Sprintf("%s:%s", csiResizerImage.GetURI(), csiResizerImage.Tag),
+		fmt.Sprintf("%s:%s", csiSnapshotterImage.GetURI(), csiSnapshotterImage.Tag),
+	)
+}
