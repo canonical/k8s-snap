a4eb46b396b533d52f85c5e0e386f576c1619e06
Claudiu Belu <claudiu.belu@canonical.com>
2025-03-04 08:46:05 +0000

    Enable cilium session affinity

--- src/k8s/pkg/k8sd/features/cilium/network.go
+++ src/k8s/pkg/k8sd/features/cilium/network.go
@@ -186,6 +186,7 @@ func ApplyNetwork(ctx context.Context, snap snap.Snap, m helm.Client, s state.St
 		"k8sServicePort": apiserver.GetSecurePort(),
 		// This flag enables the runtime device detection which is set to true by default in Cilium 1.16+
 		"enableRuntimeDeviceDetection": true,
+		"sessionAffinity":              true,
 	}
 
 	// If we are deploying with IPv6 only, we need to set the routing mode to native
