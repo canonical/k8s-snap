136e0364f4f04b0ea559727bcbf3361605436584
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2025-03-10 15:13:01 +0200

    ci: fix update components (#1167)

--- build-scripts/hack/update-cilium-chart.sh
+++ build-scripts/hack/update-cilium-chart.sh
@@ -3,7 +3,7 @@
 VERSION="v1.17.1"
 DIR=$(realpath $(dirname "${0}"))
 
-CHARTS_PATH="$DIR/../../k8s/manifests/charts"
+CHARTS_PATH="$DIR/../../src/k8s/pkg/k8sd/features/cilium/charts"
 
 cd "$CHARTS_PATH" || exit
 
