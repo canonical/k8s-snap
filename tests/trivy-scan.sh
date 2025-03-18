6e9c14fbafc02aa20c6e9100a21998d424e656f7
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-03-18 09:28:05 +0000

    ci: compare Trivy CVEs with CISA KEV list
    This change will compare the list of CVEs outputed by Trivy scans
    with the list of Known Exploited Vulnerabilities from CISA.
    
    Inspired by https://github.com/canonical/ams/pull/873/files
    
    While at it, we'll deduplicate the security scan job, using the
    trivy-scan.sh script.

--- tests/trivy-scan.sh
+++ tests/trivy-scan.sh
@@ -12,28 +12,75 @@ if [[ ! -f $SNAP_PATH ]]; then
 fi
 
 # Setup Trivy vulnerability scanner
-mkdir -p manual-trivy/sarifs
-pushd manual-trivy
+mkdir -p .trivy/sarifs
+pushd .trivy
 VER=$(curl --silent -qI https://github.com/aquasecurity/trivy/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');
 wget https://github.com/aquasecurity/trivy/releases/download/${VER}/trivy_${VER#v}_Linux-64bit.tar.gz
 tar -zxvf ./trivy_${VER#v}_Linux-64bit.tar.gz
 popd
 
-# Run Trivy vulnerability scanner in repo mode
-./manual-trivy/trivy fs . \
+# Run Trivy vulnerability scanner in repo mode.
+#
+# We'll have two runs:
+# * one with SARIF output, used by GitHub
+# * one with "json" output
+#   * SARIF is also a json but not as well structured
+#   * the list of vulnerabilities is easier to parse and compare with the CISA list
+#   * the second run will not filter the records based on severity
+./.trivy/trivy fs . \
   --format sarif \
   --db-repository public.ecr.aws/aquasecurity/trivy-db \
   --severity "MEDIUM,HIGH,CRITICAL" \
   --ignore-unfixed \
-  > ./manual-trivy/sarifs/trivy-k8s-repo-scan--results.sarif
+  > ./.trivy/sarifs/trivy-k8s-repo-scan--results.sarif
 
+./.trivy/trivy fs . \
+  --format json \
+  --db-repository public.ecr.aws/aquasecurity/trivy-db \
+  --ignore-unfixed \
+  > ./.trivy/sarifs/trivy-k8s-repo-scan--results.json
+
+
+# Run Trivy vulnerability scanner in rootfs mode, scanning the snap
 for var in $(env | grep -o '^TRIVY_[^=]*'); do
   unset "$var"
 done
-cp "${SNAP_PATH}" ./k8s-test.snap
-rm -rf ./squashfs-root
-unsquashfs k8s-test.snap
-./manual-trivy/trivy rootfs ./squashfs-root/ \
+cp "${SNAP_PATH}" ./.trivy/k8s-test.snap
+rm -rf ./.trivy/squashfs-root
+pushd ./.trivy
+unsquashfs ./k8s-test.snap
+popd
+./.trivy/trivy rootfs ./.trivy/squashfs-root/ \
   --format sarif \
   --db-repository public.ecr.aws/aquasecurity/trivy-db \
-  > ./manual-trivy/sarifs/snap.sarif
+  > ./.trivy/sarifs/snap.sarif
+
+./.trivy/trivy rootfs ./.trivy/squashfs-root/ \
+  --format json \
+  --db-repository public.ecr.aws/aquasecurity/trivy-db \
+  > ./.trivy/sarifs/snap.json
+
+# Obtain CISA Known Exploited Vulnerabilities list.
+curl -s -o ./.trivy/kev.json \
+  https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json
+
+function get_cisa_kev_cves() {
+  local kevJson=$1
+  local trivyJsonReport=$2
+
+  set +x
+  local kev_cves=$(jq -r '.vulnerabilities[].cveID' $kevJson | sort -u)
+  local found_cves=$(jq -r '.Results[] | select(.Vulnerabilities != null) | .Vulnerabilities[].VulnerabilityID' $trivyJsonReport | sort -u)
+  local matches="$(echo "$found_cves" | grep -F -f <(echo "$kev_cves") || true)"
+  set -x
+
+  if [ -n "$matches" ]; then
+    echo "KEV listed vulnerabilities found in $2:"
+    echo "$matches"
+    exit 1
+  fi
+}
+
+# Compare the trivy reports with the CISA KEV list
+get_cisa_kev_cves ./.trivy/kev.json ./.trivy/sarifs/trivy-k8s-repo-scan--results.json
+get_cisa_kev_cves ./.trivy/kev.json ./.trivy/sarifs/snap.json
