7edb70bec05b6754ceda0bf131f3a1eb50b68369
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2025-03-19 14:57:43 -0400

    use new rawfile-localpv

--- src/k8s/pkg/k8sd/features/localpv/chart.go
+++ src/k8s/pkg/k8sd/features/localpv/chart.go
@@ -17,7 +17,7 @@ var (
 	// imageRepo is the repository to use for Rawfile LocalPV CSI.
 	imageRepo = "ghcr.io/canonical/rawfile-localpv"
 	// ImageTag is the image tag to use for Rawfile LocalPV CSI.
-	ImageTag = "0.8.1"
+	ImageTag = "4f27006ed63281a5aea6fa2f83437c267aa30e203a2cdf351f9c20fef624a5e4-amd64"
 
 	// csiNodeDriverImage is the image to use for the CSI node driver.
 	csiNodeDriverImage = "ghcr.io/canonical/k8s-snap/sig-storage/csi-node-driver-registrar:v2.10.1"
