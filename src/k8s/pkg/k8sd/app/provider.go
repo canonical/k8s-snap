54c6e01a4dab4d34f864562f47e2ac1e34f02b93
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2025-03-21 16:55:35 +0300

    feat: Add leadership to feature reconcilation
    - A helper util is added to check if this current node is the
      microcluster leader.
    - Feature controller is upgraded to check for leadership and skip
      reconcilation on non-leader nodes.
    - A k8sd API endpoint is introduced to enable triggering feature
      reconcilation from other nodes.
    - Cluster config endpoint is updated to notify the controller or forward
      the reconcilation request based on leadership status.

--- src/k8s/pkg/k8sd/app/provider.go
+++ src/k8s/pkg/k8sd/app/provider.go
@@ -1,10 +1,18 @@
 package app
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
+	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
 	"github.com/canonical/k8s/pkg/k8sd/api"
 	"github.com/canonical/k8s/pkg/snap"
 	"github.com/canonical/k8s/pkg/utils"
+	microclusterutil "github.com/canonical/k8s/pkg/utils/microcluster"
+	microclusterapi "github.com/canonical/lxd/shared/api"
 	"github.com/canonical/microcluster/v2/microcluster"
+	"github.com/canonical/microcluster/v2/state"
 )
 
 func (a *App) MicroCluster() *microcluster.MicroCluster {
@@ -41,6 +49,42 @@ func (a *App) NotifyFeatureController(network, gateway, ingress, loadBalancer, l
 	if dns {
 		utils.MaybeNotify(a.triggerFeatureControllerDNSCh)
 	}
+}
+
+func (a *App) NotifyOrForwardFeatureReconcilation(ctx context.Context, s state.State, network, gateway, ingress, loadBalancer, localStorage, metricsServer, dns bool) error {
+	isLeader, err := microclusterutil.IsLeader(s)
+	if err != nil {
+		return fmt.Errorf("failed to check if node is leader: %w", err)
+	}
+
+	// If the node is not the leader, we need to forward the reconcilation request to the leader.
+	if !isLeader {
+		leaderClient, err := s.Leader()
+		if err != nil {
+			return fmt.Errorf("failed to get leader client: %w", err)
+		}
+
+		in := &apiv1.ReconcileFeaturesRequest{
+			Network:       network,
+			Gateway:       gateway,
+			Ingress:       ingress,
+			LoadBalancer:  loadBalancer,
+			LocalStorage:  localStorage,
+			MetricsServer: metricsServer,
+			DNS:           dns,
+		}
+
+		result := &apiv1.ReconcileFeaturesResponse{}
+
+		if err := leaderClient.Query(ctx, "POST", apiv1.K8sdAPIVersion, microclusterapi.NewURL().Path(strings.Split(apiv1.ReconcileFeaturesRPC, "/")...), in, result); err != nil {
+			return fmt.Errorf("failed to trigger feature reconcilation: %w", err)
+		}
+
+		return nil
+	}
+
+	a.NotifyFeatureController(network, gateway, ingress, loadBalancer, localStorage, metricsServer, dns)
+	return nil
 }
 
 // Ensure App implements api.Provider.
--- src/k8s/pkg/k8sd/features/cilium/network_test.go
+++ src/k8s/pkg/k8sd/features/cilium/network_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/canonical/k8s/pkg/snap"
 	snapmock "github.com/canonical/k8s/pkg/snap/mock"
 	"github.com/canonical/k8s/pkg/utils"
-	testenv "github.com/canonical/k8s/pkg/utils/microcluster"
+	testenv "github.com/canonical/k8s/pkg/utils/microcluster/testenv"
 	"github.com/canonical/microcluster/v2/state"
 	. "github.com/onsi/gomega"
 	"k8s.io/klog/v2"
