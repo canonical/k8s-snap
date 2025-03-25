915029a7d9cba9e71241e37b41e6aeccb33359d4
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-03-25 10:43:29 +0100

    fix: Execute post-refresh hook after node is ready
    Initially, the custom post-refresh hook was executed in the microcluster
    `onStart` hook. This hook is executed right after the microcluster
    daemon started which could lead to "Database not initialized" error if
    the node was not bootstrapped yet.
    
    By introducing a nodeReadyHook and execute the refresh from there, we
    ensure that the refresh is only executed after a `snap refresh` but not
    after a `snap install`

---
+++ src/k8s/pkg/k8sd/app/hooks_node_ready.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/canonical/k8s/pkg/log"
+	"github.com/canonical/k8s/pkg/utils"
+	"github.com/canonical/microcluster/v2/state"
+)
+
+// onNodeReady is called when the node is ready, right after the wait group is released.
+// The node is ready if:
+// - the microcluster database is accessible
+// - the kubernetes endpoint is reachable.
+// Note that this is not a microcluster hook, but a custom k8sd hook.
+func (a *App) onNodeReady(ctx context.Context, s state.State) error {
+	log := log.FromContext(ctx).WithValues("hook", "onNodeReady")
+
+	// Check if a refresh was performed and if so, run the custom post-refresh hook
+	isPostRefresh, err := utils.FileExists(a.snap.PostRefreshLockPath())
+	if err != nil {
+		return fmt.Errorf("failed to check if snap is post-refresh: %w", err)
+	}
+	if isPostRefresh {
+		if err := a.postRefreshHook(ctx, s); err != nil {
+			return fmt.Errorf("failed to run post-refresh hook: %w", err)
+		}
+
+		log.Info("Post-refresh hook completed successfully - removing lock file.")
+		if err := os.Remove(a.snap.PostRefreshLockPath()); err != nil {
+			return fmt.Errorf("failed to remove post-refresh lock file: %w", err)
+		}
+	}
+
+	return nil
+}
