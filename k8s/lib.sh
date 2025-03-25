d37328968d5e69d1ab3b68c045763cadd9dd38b0
Reza Abbasalipour <reza.abbasalipour@canonical.com>
2025-03-26 00:01:36 +0400

    Revert "chore: extract argument parsing from execute_service in lib script"
    This reverts commit cee03a4282b5a4b21685f41453c2a1cca3271d7e.

--- k8s/lib.sh
+++ k8s/lib.sh
@@ -179,13 +179,6 @@ k8s::util::wait_kube_apiserver() {
   done
 }
 
-# Source arguments and substitute environment variables. Will fail if we cannot read the file.
-k8s::util::parse_arguments_file() {
-  arguments_file_path="$1"
-
-  declare -ag parsed_args="($(cat $arguments_file_path))"  
-}
-
 # Execute a "$SNAP/bin/$service" with arguments from "$SNAP_DATA/args/$service"
 # Example: 'k8s::common::execute_service kubelet'
 k8s::common::execute_service() {
@@ -193,16 +186,17 @@ k8s::common::execute_service() {
 
   k8s::common::setup_env
 
-  k8s::util::parse_arguments_file ${SNAP_COMMON}/args/${service_name}
+  # Source arguments and substitute environment variables. Will fail if we cannot read the file.
+  declare -a args="($(cat "${SNAP_COMMON}/args/${service_name}"))"
 
   set -xe
   ulimit -c unlimited
   export GOTRACEBACK="crash"
   if [[ -f "${SNAP_COMMON}/args/${service_name}-env" ]]; then
     mapfile -t env_vars < "${SNAP_COMMON}/args/${service_name}-env"
-    exec env -S "${env_vars[@]}" "${SNAP}/bin/${service_name}" "${parsed_args[@]}"
+    exec env -S "${env_vars[@]}" "${SNAP}/bin/${service_name}" "${args[@]}"
   else
-    exec "${SNAP}/bin/${service_name}" "${parsed_args[@]}"
+    exec "${SNAP}/bin/${service_name}" "${args[@]}"
   fi
 }
 
