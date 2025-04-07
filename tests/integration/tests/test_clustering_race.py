549e18a1f3abb9885e3382716e069e4b6a388a54
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2025-04-07 14:08:47 +0000

    Avoid spawning harness instances for xfail tests
    We have a few tests that are currently skipped using pytest.xfail.
    
    The problem is that the harness instances are initialized through
    fixtures before pytest.xfail gets called, so we waste time and
    resources by spawning harness instances for tests that are skipped
    anyway.
    
    This can be avoided by using the pytest.mark.xfail decorator, which
    gets handled before the "instances" fixture.

--- tests/integration/tests/test_clustering_race.py
+++ tests/integration/tests/test_clustering_race.py
@@ -7,15 +7,17 @@ import pytest
 from test_util import harness, tags, util
 
 
-# Note(ben): Commented out as otherwise the setup would still happen for xfail tests.
-# @pytest.mark.node_count(3)
+@pytest.mark.xfail(
+    run=False,
+    reason="This test is currently flaky because of a k8s-dqlite shutdown issue.",
+)
+@pytest.mark.node_count(3)
 @pytest.mark.tags(tags.NIGHTLY)
 def test_wrong_token_race(instances: List[harness.Instance]):
     # Note(ben): k8s-dqlite sometimes takes very long to shutdown (to be investigated) and
     # since microcluster has a 30s fixed timeout for the remove hooks this test sometimes fails.
     # The timeout will be configurable in https://github.com/canonical/microcluster/pull/365)
     # The k8s-dqlite issue will be investigated separately.
-    pytest.xfail("This test is currently flaky because of a k8s-dqlite shutdown issue.")
     cluster_node = instances[0]
 
     join_token = util.get_join_token(cluster_node, instances[1])
