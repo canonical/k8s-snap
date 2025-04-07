dbf294c36c8e98f08b5b278e388583dca19fe704
Mateo Florido <mateo.florido@canonical.com>
2025-04-07 18:53:43 -0500

    fix: mark CSR as failed if the CA key is missing

--- src/k8s/pkg/k8sd/controllers/csrsigning/const.go
+++ src/k8s/pkg/k8sd/controllers/csrsigning/const.go
@@ -8,4 +8,12 @@ const (
 
 	// requeueAfterWaitingForApproved is the amount of time to requeue requests if waiting for CSR to be approved.
 	requeueAfterWaitingForApproved = 10 * time.Second
+
+	// missingKeyFailedReason indicates the failure reason used when the CA
+	// private key is not available to the controller.
+	missingKeyFailedReason = "MissingCAKey"
+
+	// missingKeyFailedMessage provides the failure message used when the
+	// controller is unable to sign the CSR due to a missing CA private key.
+	missingKeyFailedMessage = "The CSR could not be signed because the controller is missing the CA private key."
 )
