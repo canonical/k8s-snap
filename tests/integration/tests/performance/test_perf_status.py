3bb4653b51e773e2c3d5729d8ed9386d3d34b296
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-04-30 12:04:23 +0200

    test: Add performance tests
    This commit introduces machinery to create performance tests and
    measure them agains a historic baseline.
    
    * Create test structures including tags and `pytest-benchmark` to create
      performance tests
    * Introduce structure to store and compare performance against a
      historic baseline that runs on specified hardware (GH runners)
    * Create a workflow that creates new performance baselines when requirements
      or performance tests change
    * Extend the nightly test with performance tests that validates current
      latests edge with the current baseline and fails if performance
    significantly deteriorated

---
+++ tests/integration/tests/performance/test_perf_status.py
@@ -0,0 +1,18 @@
+#
+# Copyright 2025 Canonical, Ltd.
+#
+from typing import List
+
+import pytest
+from test_util import harness, tags
+
+
+@pytest.mark.node_count(1)
+@pytest.mark.tags(tags.PULL_REQUEST, tags.PERFORMANCE)
+def test_perf_status_single_node_cli(instances: List[harness.Instance], benchmark):
+    node = instances[0]
+
+    def run():
+        node.exec(["k8s", "status"])
+
+    benchmark.pedantic(run, rounds=40)
