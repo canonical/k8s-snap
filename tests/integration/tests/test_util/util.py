2d8ac3ef6c73ac2900a5dfb9d86fd3a6f0cb47d6
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-05-21 08:56:20 +0200

    linter

--- tests/integration/tests/test_util/util.py
+++ tests/integration/tests/test_util/util.py
@@ -204,7 +204,9 @@ def preload_snaps(instance: harness.Instance):
         instance.send_file(source=ack.as_posix(), destination=remote.as_posix())
 
         LOG.info("Running snap ack for %s", remote.as_posix())
-        stubbornly(retries=3, delay_s=2).on(instance).exec(["snap", "ack", remote.as_posix()])
+        stubbornly(retries=3, delay_s=2).on(instance).exec(
+            ["snap", "ack", remote.as_posix()]
+        )
 
         LOG.info("Wait for snap changes to finish...")
         stubbornly(retries=20, delay_s=5).on(instance).until(
