0942666ad73c5a9a39b8c05f17a8e7010ae51937
Homayoon Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-05-23 17:15:03 +0400

    refactor: Add coordinator to use a single manager for managed controllers (#1453)
    - This PR enables us to use a single `manager.Manager` for both the "upgrade" and "csrSigning" controllers (+ any future controllers).
    - A `Coordinator` is added which is responsible for creating a single `manager` instance, and registering all the controllers with it.
    - The `csrSigning` controller logic is refactored to remove an abstraction layer. Instead of having a `Controller` and `csrsigningReconciler`, there's now only a single `Controller` struct. This change simplifies the controller logic and brings it closer to the `upgrade` controller implementation (more consistent implementations).

--- src/k8s/pkg/client/kubernetes/scheme.go
+++ src/k8s/pkg/client/kubernetes/scheme.go
@@ -5,11 +5,16 @@ import (
 
 	upgradesv1alpha "github.com/canonical/k8s/pkg/k8sd/crds/upgrades/v1alpha"
 	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
 // NewScheme creates a new runtime.Scheme and adds the types defined in this package to it.
+// It also adds the client-go scheme to the scheme.
 func NewScheme() (*runtime.Scheme, error) {
 	scheme := runtime.NewScheme()
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		return nil, fmt.Errorf("failed to add client-go types to scheme: %w", err)
+	}
 	if err := upgradesv1alpha.AddToScheme(scheme); err != nil {
 		return nil, fmt.Errorf("failed to add types to scheme: %w", err)
 	}
