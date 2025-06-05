abad6fd10cbd6a2fdf1f09e5a558c884095c1c69
nhennigan <niamh.hennigan@canonical.com>
2025-06-05 11:41:11 -0700

    updte sitemap config

--- docs/canonicalk8s/conf.py
+++ docs/canonicalk8s/conf.py
@@ -184,14 +184,14 @@ slug = 'canonical-kubernetes'
 html_baseurl = 'https://documentation.ubuntu.com/canonical-kubernetes/'
 
 # URL scheme. Add language and version scheme elements manually e.g. '{0}/{1}/{{link}}'.format(os.environ['READTHEDOCS_LANGUAGE'], os.environ['READTHEDOCS_VERSION'])
-sitemap_url_scheme = "{link}"
+
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
 
-# if 'READTHEDOCS_VERSION' in os.environ:
-    # version = os.environ["READTHEDOCS_VERSION"]
-    # sitemap_url_scheme = '{version}{link}'
-# else:
-#     sitemap_url_scheme = 'MANUAL/{link}'
+if 'READTHEDOCS_VERSION' in os.environ:
+    version = os.environ["READTHEDOCS_VERSION"]
+    sitemap_url_scheme = '{version}{link}'
+else:
+    sitemap_url_scheme = 'MANUAL/{link}'
 
 # Template and asset locations
 
