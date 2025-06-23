b5004ddef94dd51804447e2e53474629debd40d0
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-06-23 13:52:58 +0200

    don't fail if check=False

--- tests/integration/tests/test_util/util.py
+++ tests/integration/tests/test_util/util.py
@@ -88,7 +88,7 @@ class Retriable:
             LOG.warning(f"  stdout={stdout}")
             LOG.warning(f"  stderr={stderr}")
             raise
-        if self._condition:
+        if self._condition and command_kwds.get('check', True):
             assert self._condition(resp), "Failed to meet condition"
         return resp
 
