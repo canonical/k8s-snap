362e8164aea856b3cdfcf5ad513244c26e67aa2f
Niamh Hennigan <niamh.hennigan@canonical.com>
2025-06-24 21:51:43 -0700

    docs: Update custom worker join how to guide (#1600)
    * Update custom worker join how to
    
    the previous example where the --name should only be used when the hostname cannot be resolved and is not the default or recommended behaviour so replacing that example and reformatting the page
    
    * Editing the linkcheck ignore
    
    There is a huge amount of links here for the same website that often flake and give a false positive in our tests. By only checking 3 of the links (ones starting with V-25*) we should still be altered to broken links but not have the same stability issues

--- docs/canonicalk8s/conf.py
+++ docs/canonicalk8s/conf.py
@@ -217,6 +217,7 @@ linkcheck_ignore = [
     r'https://www\.stigviewer\.com/.*',
     'https://www.squid-cache.org/',
     'https://www.esd.whs.mil/portals/54/documents/dd/issuances/dodi/855101p.pdf',
+    r'https://stigviewer.com/stigs/kubernetes/2024-06-10/finding/V-24',
     ]
 
 
