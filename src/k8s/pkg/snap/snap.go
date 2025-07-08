3cc3dfc85710f3416f7e7fa32b64b46e333ea884
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-07-08 11:54:13 +0400

    fix: Pass parent context to helm operations
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- src/k8s/pkg/snap/snap.go
+++ src/k8s/pkg/snap/snap.go
@@ -10,7 +10,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/canonical/k8s/pkg/client/dqlite"
 	"github.com/canonical/k8s/pkg/client/helm"
@@ -373,7 +372,6 @@ func (s *snap) HelmClient() helm.Client {
 		func(namespace string) genericclioptions.RESTClientGetter {
 			return s.restClientGetter(filepath.Join(s.KubernetesConfigDir(), "admin.conf"), namespace)
 		},
-		5*time.Minute,
 		10,
 	)
 }
