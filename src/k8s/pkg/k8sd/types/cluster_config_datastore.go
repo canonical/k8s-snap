20f1017fd4d9a3fca2a3e46263022795e7adcc54
louiseschmidtgen <louise.schmidtgen@canonical.com>
2025-07-17 10:19:22 +0200

    enable WatchList feature

--- src/k8s/pkg/k8sd/types/cluster_config_datastore.go
+++ src/k8s/pkg/k8sd/types/cluster_config_datastore.go
@@ -68,8 +68,6 @@ func (c Datastore) ToKubeAPIServerArguments(p DatastorePathsProvider) (map[strin
 	case "k8s-dqlite":
 		updateArgs["--etcd-healthcheck-timeout"] = "4s"
 		updateArgs["--etcd-servers"] = fmt.Sprintf("unix://%s", filepath.Join(p.K8sDqliteStateDir(), "k8s-dqlite.sock"))
-		// The watch list feature is enable by default on >1.32, but k8s-dqlite currently doesn't support it.
-		updateArgs["--feature-gates"] = "WatchList=false"
 		deleteArgs = []string{"--etcd-cafile", "--etcd-certfile", "--etcd-keyfile"}
 	case "etcd":
 		updateArgs["--etcd-cafile"] = filepath.Join(p.EtcdPKIDir(), "ca.crt")
