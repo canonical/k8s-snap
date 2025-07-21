30599414283a0c468b93b8e64ad74f1ecc4f92ae
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-07-21 08:10:13 +0200

    build: Build etcd dynamically with cgo enabled (#1649)

--- build-scripts/components/etcd/build.sh
+++ build-scripts/components/etcd/build.sh
@@ -6,8 +6,9 @@ INSTALL="${1}/bin"
 mkdir -p "${INSTALL}"
 
 export GOTOOLCHAIN=local
-# (berkayoz): We're hitting https://github.com/NixOS/patchelf/issues/146 with patchelf
-export CGO_ENABLED=0
-# # export GOEXPERIMENT=opensslcrypto
+export CGO_ENABLED=1
+#export GOEXPERIMENT=opensslcrypto
+export GOFLAGS="-tags=linux,cgo"
+
 make build
 cp bin/* "${INSTALL}/"
