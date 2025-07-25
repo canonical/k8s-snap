4711585ecebf215e91671ce353fc955c9c012f2b
Louise K Schmidtgen <louise.schmidtgen@canonical.com>
2025-07-25 17:22:23 +0200

    correct dir

--- tests/trivy-scan.sh
+++ tests/trivy-scan.sh
@@ -12,8 +12,8 @@ if [[ ! -f $SNAP_PATH ]]; then
 fi
 
 # Setup Trivy vulnerability scanner
-mkdir -p manual-trivy/sarifs
-pushd manual-trivy
+mkdir -p .trivy/sarifs
+pushd .trivy
 VER=$(curl --silent -qI https://github.com/aquasecurity/trivy/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');
 wget https://github.com/aquasecurity/trivy/releases/download/${VER}/trivy_${VER#v}_Linux-64bit.tar.gz
 tar -zxvf ./trivy_${VER#v}_Linux-64bit.tar.gz
