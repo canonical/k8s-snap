a418bfd8f47b25c43fc80d7889942070e6d8d2de
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-08-06 12:46:24 +0400

    Revert "test FIPS etcd"
    This reverts commit ed4e8bfaca64f6d323fb8e696541f07058997a1d.

--- build-scripts/components/etcd/build.sh
+++ build-scripts/components/etcd/build.sh
@@ -7,7 +7,7 @@ mkdir -p "${INSTALL}"
 
 export GOTOOLCHAIN=local
 export CGO_ENABLED=1
-export GOEXPERIMENT=opensslcrypto
+#export GOEXPERIMENT=opensslcrypto
 export GOFLAGS="-tags=linux,cgo"
 
 make build
