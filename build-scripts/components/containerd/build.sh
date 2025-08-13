2acae5da2633efcbf1611b664de924a7dc616a6a
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-08-13 11:47:28 +0400

    chore: Pass ms_tls13kdf Go build tag
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- build-scripts/components/containerd/build.sh
+++ build-scripts/components/containerd/build.sh
@@ -12,7 +12,7 @@ sed -i "s,^REVISION.*$,REVISION=${REVISION}," Makefile
 export GOTOOLCHAIN=local
 export GOEXPERIMENT=opensslcrypto
 export CGO_ENABLED=1
-export GO_BUILDTAGS="linux cgo"
+export GO_BUILDTAGS="linux cgo ms_tls13kdf"
 for bin in containerd; do
   make "bin/${bin}"
   cp "bin/${bin}" "${INSTALL}/${bin}"
