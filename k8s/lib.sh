9fac878453cc5f6b666d0ffc92d4ff737c0cf42e
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2025-08-29 11:47:04 +0200

    feat: add resource files on snap install

--- k8s/lib.sh
+++ k8s/lib.sh
@@ -43,6 +43,12 @@ k8s::common::is_strict() {
   fi
 }
 
+k8s::common::resources() {
+  mkdir -p "$SNAP_COMMON/etc/"
+  cp -r "$SNAP/etc/templates" "$SNAP_COMMON/etc/"
+  cp -r "$SNAP/etc/configurations" "$SNAP_COMMON/etc/"
+}
+
 # Check if FIPS is enabled on the system
 # Returns 0 (success) if FIPS is enabled, 1 (failure) otherwise
 # Example: 'k8s::common::on_fips_host && echo "FIPS is enabled"'
