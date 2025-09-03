05b4ca38438c6e9e5869dca8a446b0e383454469
Mateo Florido <mateo.florido@canonical.com>
2025-09-03 08:39:12 -0500

    fix: Fix Invalid `iproute2` JSON output in arm64

--- src/k8s/pkg/utils/netlink.go
+++ src/k8s/pkg/utils/netlink.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os/exec"
+	"regexp"
 )
 
 type VXLANInterface struct {
@@ -29,6 +30,8 @@ func ListVXLANInterfaces() ([]VXLANInterface, error) {
 	if err != nil {
 		return vxlanDevices, fmt.Errorf("running ip command failed: %s", string(out))
 	}
+
+	out = fixInvalidIproute2JSON(out)
 
 	if err := json.Unmarshal(out, &ipLinks); err != nil {
 		return vxlanDevices, fmt.Errorf("unmarshaling ip command output failed: %w", err)
@@ -62,4 +65,19 @@ func RemoveLink(name string) error {
 	}
 
 	return nil
+}
+
+// fixInvalidIproute2JSON cleans up invalid JSON output produced by the
+// iproute2 command in arm64. Currently, the Ubuntu package combines the VXLAN
+// VNI value with the fan-map extension, resulting in invalid JSON.
+func fixInvalidIproute2JSON(input []byte) []byte {
+	output := string(input)
+
+	// Target the specific case where numeric VXLAN VNI values are concatenated
+	// with text (like "0fan-map") without proper JSON quoting
+	re := regexp.MustCompile(`"id":\s*([0-9]+[a-zA-Z][a-zA-Z0-9_-]*)`)
+
+	// Enclose the entire VNI value in double quotes
+	output = re.ReplaceAllString(output, `"id":"$1"`)
+	return []byte(output)
 }
