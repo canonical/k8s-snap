26d589aa3ca97ae2d96234d5107604156add1194
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2025-09-12 08:23:15 +0300

    fix: adjust retry endpoints retry order

--- src/k8s/pkg/proxy/endpoints.go
+++ src/k8s/pkg/proxy/endpoints.go
@@ -3,6 +3,7 @@ package proxy
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/canonical/k8s/pkg/utils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,25 +24,23 @@ func getKubernetesEndpoints(ctx context.Context, kubeconfigFile string) ([]strin
 	endpointSlices, err := clientset.DiscoveryV1().EndpointSlices("default").List(ctx, metav1.ListOptions{
 		LabelSelector: "kubernetes.io/service-name=kubernetes",
 	})
-	if err != nil {
-		// Fallback to Endpoints for older clusters that do not support EndpointSlices
-		slicesErr := fmt.Errorf("failed to retrieve endpointSlices for kubernetes service: %w", err)
-
-		endpoints, err := clientset.CoreV1().Endpoints("default").Get(ctx, "kubernetes", metav1.GetOptions{})
-		if err != nil {
-			endpointsErr := fmt.Errorf("failed to retrieve endpoints for kubernetes service: %w", err)
-			return nil, fmt.Errorf("%v; %v", slicesErr, endpointsErr)
-		}
-		if endpoints == nil {
+	if err == nil {
+		if endpointSlices == nil {
 			return nil, nil
 		}
 
-		return utils.ParseEndpoints(endpoints), nil
+		return utils.ParseEndpointSlices(endpointSlices), nil
+	}
+	log.Printf("Failed to get EndpointSlices, falling back to Endpoints api: %v", err)
+
+	endpoints, err := clientset.CoreV1().Endpoints("default").Get(ctx, "kubernetes", metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve endpoints for kubernetes service: %w", err)
 	}
 
-	if endpointSlices == nil {
+	if endpoints == nil {
 		return nil, nil
 	}
 
-	return utils.ParseEndpointSlices(endpointSlices), nil
+	return utils.ParseEndpoints(endpoints), nil
 }
