17279ab7e032620bca2183b4c548b5306f2caeed
Mateo Florido <mateo.florido@canonical.com>
2025-10-16 16:37:37 -0500

    fix (release-1.32): Bump Helm version across the snap

--- build-scripts/components/helm/build.sh
+++ build-scripts/components/helm/build.sh
@@ -5,5 +5,7 @@ INSTALL="${1}/bin"
 
 mkdir -p "${INSTALL}"
 
-make VERSION="${VERSION}"
+# NOTE: Using the latest (2025-10-16) toolchain as the module
+# defaults to 1.24.0
+GOTOOLCHAIN=go1.24.9 make VERSION="${VERSION}"
 cp bin/helm "${INSTALL}/helm"
