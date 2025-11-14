2457bce0436ea41a4937050a70d1cb71e44f4afc
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-11-14 12:14:09 +0400

    feat(kubectl): Make k8s kubectl independent from k8sd, Improve error handling for kubectl binary and KUBECONFIG checks

--- src/k8s/cmd/k8s/k8s_kubectl.go
+++ src/k8s/cmd/k8s/k8s_kubectl.go
@@ -1,11 +1,11 @@
 package k8s
 
 import (
+	"os"
 	"os/exec"
 	"path/filepath"
 	"syscall"
 
-	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
 	cmdutil "github.com/canonical/k8s/cmd/util"
 	"github.com/spf13/cobra"
 )
@@ -17,27 +17,6 @@ func newKubectlCmd(env cmdutil.ExecutionEnvironment) *cobra.Command {
 		DisableFlagParsing: true,
 		PreRun:             chainPreRunHooks(hookRequireRoot(env)),
 		Run: func(cmd *cobra.Command, args []string) {
-			client, err := env.Snap.K8sdClient("")
-			if err != nil {
-				cmd.PrintErrf("Error: Failed to create a k8sd client. Make sure that the k8sd service is running.\n\nThe error was: %v\n", err)
-				env.Exit(1)
-				return
-			}
-
-			if response, initialized, err := client.NodeStatus(cmd.Context()); err != nil {
-				cmd.PrintErrf("Error: Failed to retrieve the node status.\n\nThe error was: %v\n", err)
-				env.Exit(1)
-				return
-			} else if !initialized {
-				cmd.PrintErrln("Error: The node is not part of a Kubernetes cluster. You can bootstrap a new cluster with:\n\n  sudo k8s bootstrap")
-				env.Exit(1)
-				return
-			} else if response.NodeStatus.ClusterRole == apiv1.ClusterRoleWorker {
-				cmd.PrintErrln("Error: k8s kubectl commands are not allowed on worker nodes.")
-				env.Exit(1)
-				return
-			}
-
 			binary, err := exec.LookPath("kubectl")
 			if err != nil {
 				cmd.PrintErrln("Error: kubectl binary not found")
@@ -45,10 +24,25 @@ func newKubectlCmd(env cmdutil.ExecutionEnvironment) *cobra.Command {
 				return
 			}
 
+			kubeconfigEnvKey := "KUBECONFIG"
+			adminKubeconfigPath := filepath.Join(env.Snap.KubernetesConfigDir(), "admin.conf")
+
+			if !cmdutil.ExistsInEnviron(env.Environ, kubeconfigEnvKey) {
+				if _, err := os.Stat(adminKubeconfigPath); err != nil {
+					if os.IsNotExist(err) {
+						cmd.PrintErrf("Error: %s file does not exist. Either set KUBECONFIG environment variable or ensure this node is bootstrapped as a control-plane node.\n", adminKubeconfigPath)
+					} else {
+						cmd.PrintErrf("Error: unable to access %s: %v\n", adminKubeconfigPath, err)
+					}
+					env.Exit(1)
+					return
+				}
+			}
+
 			command := append([]string{"kubectl"}, args...)
 			environ := cmdutil.EnvironWithDefaults(
 				env.Environ,
-				"KUBECONFIG", filepath.Join(env.Snap.KubernetesConfigDir(), "admin.conf"),
+				kubeconfigEnvKey, adminKubeconfigPath,
 				"EDITOR", "nano",
 			)
 			if err := syscall.Exec(binary, command, environ); err != nil {
