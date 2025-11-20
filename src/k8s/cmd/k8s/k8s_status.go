1814c7232a26f617fd29d5103e5ac1f767e7359d
ethandcosta <dcosta.e@northeastern.edu>
2025-11-20 15:27:30 -0500

    Revert "fix: Spinner CLI only triggers on k8s status if --wait-ready is enabled (#1940)"
    This reverts commit 114fbd2be60bac9104b654ae41545d3abee73dab.

--- src/k8s/cmd/k8s/k8s_status.go
+++ src/k8s/cmd/k8s/k8s_status.go
@@ -48,17 +48,11 @@ func newStatusCmd(env cmdutil.ExecutionEnvironment) *cobra.Command {
 			}
 
 			var response apiv1.ClusterStatusResponse
-			if opts.waitReady {
-				err = cmdutil.WithSpinner(ctx, cmd.ErrOrStderr(), "Waiting for cluster to become ready...", func(ctx context.Context) error {
-					var e error
-					response, e = client.ClusterStatus(ctx, opts.waitReady)
-					return e
-				})
-			} else {
+			if err := cmdutil.WithSpinner(ctx, cmd.ErrOrStderr(), "Waiting for the cluster to become ready...", func(ctx context.Context) error {
+				var err error
 				response, err = client.ClusterStatus(ctx, opts.waitReady)
-			}
-
-			if err != nil {
+				return err
+			}); err != nil {
 				cmd.PrintErrf("Error: Failed to retrieve the cluster status.\n\nThe error was: %v\n", err)
 				env.Exit(1)
 				return
