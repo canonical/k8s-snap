104df970c1d9af4b9cb0cf59b0ecc5c87609ef42
rapour <reza.abbasalipour@canonical.com>
2025-11-26 14:52:54 +0400

    tests: add cncf conformance test
    Signed-off-by: rapour <reza.abbasalipour@canonical.com>

--- tests/integration/tests/test_util/tags.py
+++ tests/integration/tests/test_util/tags.py
@@ -7,8 +7,9 @@ PULL_REQUEST = "pull_request"
 NIGHTLY = "nightly"
 WEEKLY = "weekly"
 GPU = "gpu"
+CONFORMANCE = "conformance_tests"
 
-TEST_LEVELS = [PULL_REQUEST, NIGHTLY, WEEKLY]
+TEST_LEVELS = [PULL_REQUEST, NIGHTLY, WEEKLY, CONFORMANCE]
 
 # Those tags can be used for a convenient way to run multiple test levels.
 combine_tags("up_to_nightly", PULL_REQUEST, NIGHTLY)
