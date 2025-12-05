7eef46eac8f32d768d9315bee205ca1f4ba1208d
Omar Abou Selo <omar.selo@canonical.com>
2025-12-05 13:10:39 +0000

    Remove unnecessary environment variable

--- build-scripts/components/cni/build.sh
+++ build-scripts/components/cni/build.sh
@@ -10,7 +10,6 @@ sed -i 's/^package main/package plugin_main/' plugins/*/*/*.go
 sed -i 's/^func main()/func Main()/' plugins/*/*/*.go
 
 export GOEXPERIMENT=nosystemcrypto
-export CGO_ENABLED=0
 
 go build -o cni -ldflags "-s -w -extldflags -static -X github.com/containernetworking/plugins/pkg/utils/buildversion.BuildVersion=${VERSION}" ./cni.go
 
