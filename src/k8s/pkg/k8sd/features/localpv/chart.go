df6c5f7338f798ee7ba18c9ea283de0cb86579b9
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2025-12-11 18:12:25 +0200

    add prefix
    Signed-off-by: Louise K. Schmidtgen <louise.schmidtgen@canonical.com>

--- src/k8s/pkg/k8sd/features/localpv/chart.go
+++ src/k8s/pkg/k8sd/features/localpv/chart.go
@@ -20,11 +20,11 @@ var (
 	ImageTag = "0.8.2-ck3"
 
 	// csiNodeDriverImage is the image to use for the CSI node driver.
-	csiNodeDriverImage = "ghcr.io/canonical/csi-node-driver-registrar:2.15.0"
+	csiNodeDriverImage = "ghcr.io/canonical/csi-node-driver-registrar:2.15.0-ck0"
 	// csiProvisionerImage is the image to use for the CSI provisioner.
-	csiProvisionerImage = "ghcr.io/canonical/csi-provisioner:5.3.0"
+	csiProvisionerImage = "ghcr.io/canonical/csi-provisioner:5.3.0-ck0"
 	// csiResizerImage is the image to use for the CSI resizer.
-	csiResizerImage = "ghcr.io/canonical/csi-resizer:1.14.0"
+	csiResizerImage = "ghcr.io/canonical/csi-resizer:1.14.0-ck0"
 	// csiSnapshotterImage is the image to use for the CSI snapshotter.
-	csiSnapshotterImage = "ghcr.io/canonical/csi-snapshotter:8.3.0"
+	csiSnapshotterImage = "ghcr.io/canonical/csi-snapshotter:8.3.0-ck0"
 )
