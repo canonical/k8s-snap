cf4b3b18fb3815a6875041a01cfe4bc22b8f7cc4
nhennigan <niamh.hennigan@canonical.com>
2025-12-17 11:30:45 -0800

    Check previously ignored site

--- docs/canonicalk8s/conf.py
+++ docs/canonicalk8s/conf.py
@@ -234,11 +234,11 @@ linkcheck_ignore = [
     'https://ceph.io/',
     'https://charmhub.io/k8s/',
     'https://charmhub.io/k8s-worker/',
+    'http://slack.kubernetes.io/',
     r'https://www\.stigviewer\.com/.*',
     'https://www.squid-cache.org/',
     'https://www.esd.whs.mil/portals/54/documents/dd/issuances/dodi/855101p.pdf',
     r'https://stigviewer.com/stigs/kubernetes/2024-06-10/finding/V-24',
-    'https://kubernetes.io/*',
     ]
 
 
