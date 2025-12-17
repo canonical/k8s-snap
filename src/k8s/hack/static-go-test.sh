54b81d5365d58c2c478058e53d44c12130996032
Omar Abou Selo <omar.selo@canonical.com>
2025-12-17 08:31:58 +0000

    Automate go toolchain version

--- src/k8s/hack/static-go-test.sh
+++ src/k8s/hack/static-go-test.sh
@@ -4,7 +4,9 @@ DIR="$(realpath `dirname "${0}"`)"
 
 . "${DIR}/static-dqlite.sh"
 
-GOTOOLCHAIN=go1.25.0+auto go test \
+go env -w GOTOOLCHAIN="go$(grep ^go go.mod | awk '{print $2;}')+auto"
+
+go test \
   -tags dqlite,libsqlite3 \
   -ldflags '-linkmode "external" -extldflags "-static"' \
   "${@}"
