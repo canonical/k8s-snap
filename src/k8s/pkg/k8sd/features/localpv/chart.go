5ddfd0865060559d651a1ff8ccb381a38483a822
Reza Alipour <reza.abbasalipour@canonical.com>
2026-01-12 18:03:32 +0400

    chore: update rawfile-localpv tag to include security patches (#2211)
    Signed-off-by: rapour <reza.abbasalipour@canonical.com>

--- src/k8s/pkg/k8sd/features/localpv/chart.go
+++ src/k8s/pkg/k8sd/features/localpv/chart.go
@@ -17,7 +17,7 @@ var (
 	// imageRepo is the repository to use for Rawfile LocalPV CSI.
 	imageRepo = "ghcr.io/canonical/rawfile-localpv"
 	// ImageTag is the image tag to use for Rawfile LocalPV CSI.
-	ImageTag = "0.8.2-ck3"
+	ImageTag = "0.8.3-ck0"
 
 	// csiNodeDriverImage is the image to use for the CSI node driver.
 	csiNodeDriverImage = "ghcr.io/canonical/csi-node-driver-registrar:2.15.0-ck0"
