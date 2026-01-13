94a22561a59c4dd15fc477da7e96999f5a0044b2
rapour <reza.abbasalipour@canonical.com>
2026-01-13 13:02:21 +0400

    chore: address comments
    Signed-off-by: rapour <reza.abbasalipour@canonical.com>

--- tests/integration/tests/test_clustering.py
+++ tests/integration/tests/test_clustering.py
@@ -9,7 +9,7 @@ import logging
 import os
 import subprocess
 import time
-from typing import List
+from typing import Any, List
 
 import pytest
 import yaml
@@ -502,7 +502,7 @@ def test_cert_refresh(instances: List[harness.Instance]):
 def test_join_previously_removed_node(
     instances: List[harness.Instance],
 ):
-    """Test that joining a previously removed node can lead to any issues."""
+    """Test that a previously removed node can successfully rejoin the cluster."""
 
     cluster_node = instances[0]
     joining_node_1 = instances[1]
@@ -606,7 +606,7 @@ def _get_instance_cert(
     return cert
 
 
-def _parse_join_token(token: str) -> dict[str, any]:
+def _parse_join_token(token: str) -> dict[str, Any]:
     try:
         decoded = base64.b64decode(token, validate=True).decode("utf-8")
         payload = json.loads(decoded)
