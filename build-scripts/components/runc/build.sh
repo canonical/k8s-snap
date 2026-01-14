f6128a93b4725dd7b4986e411e134d99f5bdb64e
Benjamin Schimke <benjamin.schimke@canonical.com>
2026-01-14 19:53:08 +0100

    build: Don't fail if snap tries to revert to same version (#2273)
    `snap revert` fails if it tries to revert to the current running
    version.
    This means that the containerd build script fails in cases where the
    snapcraft go revision is the same as the one required for containerd.
    
    (cherry picked from commit 155c6e01c23767c1257e1d28194f8562a976a9ec)

--- build-scripts/components/runc/build.sh
+++ build-scripts/components/runc/build.sh
@@ -22,4 +22,6 @@ cp runc "${INSTALL}/runc"
 
 # Restore the initial Go snap revision
 echo "Restoring Go snap to initial revision: ${INITIAL_GO_REVISION}"
-snap revert go --revision="${INITIAL_GO_REVISION}"
+
+# Snap revert fails if the revision is already the current one, so we ignore errors
+snap revert go --revision="${INITIAL_GO_REVISION}" || true
