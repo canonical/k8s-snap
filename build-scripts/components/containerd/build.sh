6ea7d7d01385f888efb6421808baba294d8f93cb
Benjamin Schimke <benjamin.schimke@canonical.com>
2026-01-16 09:41:10 +0100

    strip hardcoded -strict flag from Makefile

--- build-scripts/components/containerd/build.sh
+++ build-scripts/components/containerd/build.sh
@@ -18,13 +18,18 @@ REVISION=$(git rev-parse HEAD)
 sed -i "s,^VERSION.*$,VERSION=${VERSION}," Makefile
 sed -i "s,^REVISION.*$,REVISION=${REVISION}," Makefile
 
+# -static is hardcoded in the SHIM_GO_LDFLAGS Makefile variable, so we need to remove it to build dynamically linked binaries
+# See https://github.com/containerd/containerd/blob/442cb34bda9a6a0fed82a2ca7cade05c5c749582/Makefile#L105
+sed -i 's/-extldflags "-static"//' Makefile
+
 export GOTOOLCHAIN=local
 export GOEXPERIMENT=opensslcrypto
 export CGO_ENABLED=1
 export GO_BUILDTAGS="linux cgo ms_tls13kdf"
 export SHIM_CGO_ENABLED=1
 export SHIM_GO_BUILDTAGS="linux cgo ms_tls13kdf"
-for bin in containerd ctr containerd-shim-runc-v2; do
+
+for bin in containerd ctr containerd-shim-runc-v1 containerd-shim-runc-v2; do
   make "bin/${bin}"
   cp "bin/${bin}" "${INSTALL}/${bin}"
 done
