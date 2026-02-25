9fbacf624123cf5a269b507acc8eb0495d31cfc4
Benjamin Schimke <benjamin.schimke@canonical.com>
2026-02-25 16:12:44 +0100

    don't run the tests at all

--- tests/integration/tests/test_clustering.py
+++ tests/integration/tests/test_clustering.py
@@ -605,6 +605,7 @@ def test_join_worker_with_duplicate_name_rejected(instances: List[harness.Instan
 @pytest.mark.tags(tags.NIGHTLY)
 @pytest.mark.node_count(3)
 @pytest.mark.xfail(
+    run=False,
     reason="Microcluster currently does not support rejoining of removed nodes"
 )
 def test_join_previously_removed_node(
