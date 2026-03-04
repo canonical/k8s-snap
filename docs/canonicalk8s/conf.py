200c837a4c43042175b98949c37f862a2b5050b6
nhennigan <niamh.hennigan@canonical.com>
2026-03-04 15:28:16 -0800

    Updating ignore link list
    Removing the trailing / at the end of the links tells the linkchecker to essentially add a * to the link. This should make the linkchecker more stable as more links that rate limit will be ignored

--- docs/canonicalk8s/conf.py
+++ docs/canonicalk8s/conf.py
@@ -231,9 +231,9 @@ linkcheck_ignore = [
     'http://127.0.0.1:8000',
     'http://rocks.canonical.com',
     'about',
-    'https://ceph.io/',
-    'https://charmhub.io/k8s/',
-    'https://charmhub.io/k8s-worker/',
+    'https://ceph.io',
+    'https://charmhub.io/k8s',
+    'https://charmhub.io/k8s-worker',
     'http://slack.kubernetes.io/',
     r'https://www\.stigviewer\.com/.*',
     'https://www.squid-cache.org/',
@@ -243,7 +243,8 @@ linkcheck_ignore = [
     'https://developer.hashicorp.com/vault/docs',
     'https://kubernetes.io/docs/reference/command-line-tools-reference/feature-gates-removed',
     'https://cluster-api.sigs.k8s.io/user/concepts',
-    'https://kubernetes.io/docs/reference/access-authn-authz/'
+    'https://kubernetes.io/docs/reference/access-authn-authz',
+    'https://kubernetes.io/docs/concepts/services-networking/ingress',
     ]
 
 
