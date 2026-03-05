91bcec0cb0cf477307daebd5b717f457e83ac890
nhennigan <niamh.hennigan@canonical.com>
2026-03-05 15:46:46 -0800

    Add links to ignore list

--- docs/canonicalk8s/conf.py
+++ docs/canonicalk8s/conf.py
@@ -241,7 +241,8 @@ linkcheck_ignore = [
     r'https://stigviewer.com/stigs/kubernetes/2024-06-10/finding/V-24',
     'https://developer.hashicorp.com/vault/docs',
     'https://kubernetes.io/docs',
-    'https://kubernetes.io/docs',
+    'https://kubernetes.io/blog/2024/12/11/kubernetes-v1-32-release',
+    'https://kubernetes.io/releases',
     ]
 
 
