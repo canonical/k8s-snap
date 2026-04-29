canonical/k8sd

feat: enhance getClusterStatus to check CoreDNS service cluster IP for readiness
Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

pkg/k8sd/api/cluster.go
@@ -3,19 +3,25 @@ package api
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	apiv2 "github.com/canonical/k8s-snap-api/v2/api"
+	"github.com/canonical/k8sd/pkg/client/kubernetes"
 	"github.com/canonical/k8sd/pkg/k8sd/api/impl"
 	"github.com/canonical/k8sd/pkg/k8sd/database"
 	databaseutil "github.com/canonical/k8sd/pkg/k8sd/database/util"
 	"github.com/canonical/k8sd/pkg/k8sd/features"
 	"github.com/canonical/k8sd/pkg/k8sd/types"
+	"github.com/canonical/k8sd/pkg/log"
+	"github.com/canonical/k8sd/pkg/utils"
 	mctypes "github.com/canonical/microcluster/v3/microcluster/types"
 )
 
 func (e *Endpoints) getClusterStatus(s mctypes.State, r *http.Request) mctypes.Response {
+	log := log.FromContext(r.Context()).WithValues("endpoint", "getClusterStatus")
+
 	// fail if node is not initialized yet
 	if err := s.Database().IsOpen(r.Context()); err != nil {
 		return mctypes.Unavailable(fmt.Errorf("daemon not yet initialized"))
@@ -38,6 +44,14 @@ func (e *Endpoints) getClusterStatus(s mctypes.State, r *http.Request) mctypes.R
 	ready, err := client.HasReadyNodes(r.Context())
 	if err != nil {
 		return mctypes.InternalError(fmt.Errorf("failed to check if cluster has ready nodes: %w", err))
+	}
+
+	// If dns is enabled, we also check for the coredns service clusterIP before reporting cluster as "ready"
+	if config.DNS.Enabled != nil && *config.DNS.Enabled {
+		if err := e.checkKubeletClusterDNS(r.Context(), client); err != nil {
+			log.Error(err, "kubelet does not have correct --cluster-dns arg")
+			ready = false
+		}
 	}
 
 	var statuses map[types.FeatureName]types.FeatureStatus
@@ -70,4 +84,32 @@ func (e *Endpoints) getClusterStatus(s mctypes.State, r *http.Request) mctypes.R
 			LocalStorage:  statuses[features.LocalStorage].ToAPI(),
 		},
 	})
+}
+
+// checkKubeletClusterDNS checks if --cluster-dns argument of the running kubelet service
+// matches the coredns service clusterIP.
+func (e *Endpoints) checkKubeletClusterDNS(ctx context.Context, client *kubernetes.Client) error {
+	// this is similar to what we do in the coredns feature to get the cluster IP and update kubelet.
+	// note that this is a bit brittle and might break if we change e.g. the coredns service name or namespace.
+	corednsClusterIP, err := client.GetServiceClusterIP(ctx, "coredns", "kube-system")
+	if err != nil {
+		return fmt.Errorf("failed to get coredns service cluster IP: %w", err)
+	}
+
+	if corednsClusterIP == "" {
+		return errors.New("coredns does not have a cluster IP yet")
+	}
+
+	serviceArgs, err := utils.RunningServiceArgs(ctx, "kubelet")
+	if err != nil {
+		return fmt.Errorf("failed to get args for kubelet: %w", err)
+	}
+
+	argsDNS := serviceArgs["--cluster-dns"]
+
+	if argsDNS != corednsClusterIP {
+		return fmt.Errorf("kubelet --cluster-dns %q does not match coredns service clusterIP %q", argsDNS, corednsClusterIP)
+	}
+
+	return nil
 }

canonical/k8s-snap

src/k8s/pkg/k8sd/api/cluster.go
@@ -3,20 +3,26 @@ package api
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
+	"github.com/canonical/k8s/pkg/client/kubernetes"
 	"github.com/canonical/k8s/pkg/k8sd/api/impl"
 	"github.com/canonical/k8s/pkg/k8sd/database"
 	databaseutil "github.com/canonical/k8s/pkg/k8sd/database/util"
 	"github.com/canonical/k8s/pkg/k8sd/features"
 	"github.com/canonical/k8s/pkg/k8sd/types"
+	"github.com/canonical/k8s/pkg/log"
+	"github.com/canonical/k8s/pkg/utils"
 	"github.com/canonical/lxd/lxd/response"
 	"github.com/canonical/microcluster/v2/state"
 )
 
 func (e *Endpoints) getClusterStatus(s state.State, r *http.Request) response.Response {
+	log := log.FromContext(r.Context()).WithValues("endpoint", "getClusterStatus")
+
 	// fail if node is not initialized yet
 	if err := s.Database().IsOpen(r.Context()); err != nil {
 		return response.Unavailable(fmt.Errorf("daemon not yet initialized"))
@@ -39,6 +45,14 @@ func (e *Endpoints) getClusterStatus(s state.State, r *http.Request) response.Re
 	ready, err := client.HasReadyNodes(r.Context())
 	if err != nil {
 		return response.InternalError(fmt.Errorf("failed to check if cluster has ready nodes: %w", err))
+	}
+
+	// If dns is enabled, we also check for the coredns service clusterIP before reporting cluster as "ready"
+	if config.DNS.Enabled != nil && *config.DNS.Enabled {
+		if err := e.checkKubeletClusterDNS(r.Context(), client); err != nil {
+			log.Error(err, "kubelet does not have correct --cluster-dns arg")
+			ready = false
+		}
 	}
 
 	var statuses map[types.FeatureName]types.FeatureStatus
@@ -71,4 +85,32 @@ func (e *Endpoints) getClusterStatus(s state.State, r *http.Request) response.Re
 			LocalStorage:  statuses[features.LocalStorage].ToAPI(),
 		},
 	})
+}
+
+// checkKubeletClusterDNS checks if --cluster-dns argument of the running kubelet service
+// matches the coredns service clusterIP.
+func (e *Endpoints) checkKubeletClusterDNS(ctx context.Context, client *kubernetes.Client) error {
+	// this is similar to what we do in the coredns feature to get the cluster IP and update kubelet.
+	// note that this is a bit brittle and might break if we change e.g. the coredns service name or namespace.
+	corednsClusterIP, err := client.GetServiceClusterIP(ctx, "coredns", "kube-system")
+	if err != nil {
+		return fmt.Errorf("failed to get coredns service cluster IP: %w", err)
+	}
+
+	if corednsClusterIP == "" {
+		return errors.New("coredns does not have a cluster IP yet")
+	}
+
+	serviceArgs, err := utils.RunningServiceArgs(ctx, "kubelet")
+	if err != nil {
+		return fmt.Errorf("failed to get args for kubelet: %w", err)
+	}
+
+	argsDNS := serviceArgs["--cluster-dns"]
+
+	if argsDNS != corednsClusterIP {
+		return fmt.Errorf("kubelet --cluster-dns %q does not match coredns service clusterIP %q", argsDNS, corednsClusterIP)
+	}
+
+	return nil
 }