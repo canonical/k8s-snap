canonical/k8sd

feat: add ServiceArgsController to manage service argument drift and restart services
Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

pkg/k8sd/controllers/service_args.go
@@ -1,0 +1,152 @@
+package controllers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/canonical/k8sd/pkg/log"
+	"github.com/canonical/k8sd/pkg/snap"
+	snaputil "github.com/canonical/k8sd/pkg/snap/util"
+	"github.com/canonical/k8sd/pkg/utils"
+)
+
+// ServiceArgsControllerOpts holds configuration for ServiceArgsController.
+type ServiceArgsControllerOpts struct {
+	// Snap is the snap interface.
+	Snap snap.Snap
+	// Services is an optional override for the list of service names to watch.
+	// When nil, the list is determined dynamically each reconcile cycle based on
+	// whether the node is a worker or control-plane.
+	Services []string
+	// TriggerCh drives the reconciliation loop. Typically time.NewTicker(<interval>).C.
+	TriggerCh <-chan time.Time
+	// GetRunningArgs returns the parsed command-line arguments of the running process
+	// for the named service. Returns nil, nil when the process is not running.
+	// Defaults to a systemd-based implementation when nil.
+	GetRunningArgs func(ctx context.Context, serviceName string) (map[string]string, error)
+}
+
+// ServiceArgsController periodically compares each service's args file against the
+// arguments of the running process and restarts any service whose arguments have drifted.
+type ServiceArgsController struct {
+	snap           snap.Snap
+	services       []string
+	triggerCh      <-chan time.Time
+	reconciledCh   chan struct{}
+	getRunningArgs func(ctx context.Context, serviceName string) (map[string]string, error)
+}
+
+// NewServiceArgsController creates a new ServiceArgsController.
+func NewServiceArgsController(opts ServiceArgsControllerOpts) *ServiceArgsController {
+	if opts.GetRunningArgs == nil {
+		opts.GetRunningArgs = utils.RunningServiceArgs
+	}
+	return &ServiceArgsController{
+		snap:           opts.Snap,
+		services:       opts.Services,
+		triggerCh:      opts.TriggerCh,
+		reconciledCh:   make(chan struct{}, 1),
+		getRunningArgs: opts.GetRunningArgs,
+	}
+}
+
+// Run starts the controller and blocks until ctx is cancelled.
+func (c *ServiceArgsController) Run(ctx context.Context) {
+	ctx = log.NewContext(ctx, log.FromContext(ctx).WithValues("controller", "service-args"))
+	log := log.FromContext(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-c.triggerCh:
+		}
+
+		log.Info("checking service arguments for drift")
+		if err := c.reconcile(ctx); err != nil {
+			log.Error(err, "failed to reconcile service arguments")
+		}
+
+		select {
+		case c.reconciledCh <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (c *ServiceArgsController) reconcile(ctx context.Context) error {
+	log := log.FromContext(ctx)
+
+	services := c.services
+	if services == nil {
+		isWorker, err := snaputil.IsWorker(c.snap)
+		if err != nil {
+			return fmt.Errorf("failed to determine node type: %w", err)
+		}
+		if isWorker {
+			services = snaputil.WorkerServices()
+		} else {
+			services = snaputil.ControlPlaneServices()
+		}
+	}
+
+	for _, svc := range services {
+		differs, err := c.serviceArgsDiffer(ctx, svc)
+		if err != nil {
+			log.Error(err, "failed to compare arguments", "service", svc)
+			continue
+		}
+		if !differs {
+			continue
+		}
+
+		log.Info("service arguments have drifted from args file, restarting", "service", svc)
+		if err := c.snap.RestartServices(ctx, []string{svc}); err != nil {
+			log.Error(err, "failed to restart service", "service", svc)
+		}
+	}
+
+	return nil
+}
+
+// serviceArgsDiffer returns true if the desired args (from the args file) differ
+// from the arguments the running service was started with.
+// Returns false if the args file does not exist or the process is not running.
+func (c *ServiceArgsController) serviceArgsDiffer(ctx context.Context, serviceName string) (bool, error) {
+	argsFilePath := filepath.Join(c.snap.ServiceArgumentsDir(), serviceName)
+	desiredArgs, err := utils.ParseArgumentFile(argsFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to read args file for %q: %w", serviceName, err)
+	}
+
+	actualArgs, err := c.getRunningArgs(ctx, serviceName)
+	if err != nil {
+		if errors.Is(err, utils.ErrUnitNotRunning) {
+			// service is not running, args don't differ technically
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to get running args for %q: %w", serviceName, err)
+	}
+
+	if len(desiredArgs) != len(actualArgs) {
+		return true, nil
+	}
+	for key, desiredVal := range desiredArgs {
+		if actualArgs[key] != desiredVal {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// ReconciledCh returns the channel that receives a value after each reconciliation loop.
+func (c *ServiceArgsController) ReconciledCh() <-chan struct{} {
+	return c.reconciledCh
+}

pkg/k8sd/controllers/service_args_test.go
@@ -1,0 +1,255 @@
+package controllers_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/canonical/k8sd/pkg/k8sd/controllers"
+	"github.com/canonical/k8sd/pkg/snap/mock"
+	"github.com/canonical/k8sd/pkg/utils"
+	. "github.com/onsi/gomega"
+)
+
+func TestServiceArgsController(t *testing.T) {
+	newCtrl := func(s *mock.Snap, services []string, getRunningArgs func(context.Context, string) (map[string]string, error)) (*controllers.ServiceArgsController, chan time.Time) {
+		triggerCh := make(chan time.Time)
+		ctrl := controllers.NewServiceArgsController(controllers.ServiceArgsControllerOpts{
+			Snap:           s,
+			Services:       services,
+			TriggerCh:      triggerCh,
+			GetRunningArgs: getRunningArgs,
+		})
+		return ctrl, triggerCh
+	}
+
+	t.Run("NoRestartWhenArgsMatch", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("RestartsWhenArgValueDiffers", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"new-val\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "old-val"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("RestartsWhenArgMissingFromRunningProcess", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{}, nil // process has no args
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("RestartsWhenRunningProcessHasExtraArg", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		// args file has only --foo; running process still has stale --removed arg
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar", "--removed": "old"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("NoRestartWhenProcessNotRunning", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return nil, utils.ErrUnitNotRunning
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("NoRestartWhenArgsFileMissing", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: t.TempDir()}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("ContinuesAfterGetRunningArgsError", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		for _, svc := range []string{"kubelet", "containerd"} {
+			g.Expect(os.WriteFile(filepath.Join(dir, svc), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+		}
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet", "containerd"}, func(_ context.Context, svc string) (map[string]string, error) {
+			if svc == "kubelet" {
+				return nil, fmt.Errorf("proc read error")
+			}
+			return map[string]string{"--foo": "old-val"}, nil // differs → should restart
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		// kubelet error was swallowed, containerd should be restarted
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("containerd"))
+	})
+
+	t.Run("OnlyDriftingServicesRestarted", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		for _, svc := range []string{"kubelet", "containerd"} {
+			g.Expect(os.WriteFile(filepath.Join(dir, svc), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+		}
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet", "containerd"}, func(_ context.Context, svc string) (map[string]string, error) {
+			if svc == "kubelet" {
+				return map[string]string{"--foo": "bar"}, nil // matches
+			}
+			return map[string]string{"--foo": "old-val"}, nil // drifted
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("containerd"))
+	})
+
+	t.Run("StopsOnContextCancellation", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: t.TempDir()}}
+		ctrl, _ := newCtrl(s, nil, nil)
+
+		done := make(chan struct{})
+		go func() {
+			ctrl.Run(ctx)
+			close(done)
+		}()
+
+		cancel()
+		select {
+		case <-done:
+		case <-time.After(channelSendTimeout):
+			g.Fail("controller did not stop after context cancellation")
+		}
+	})
+}

canonical/k8s-snap

src/k8s/pkg/k8sd/controllers/service_args.go
@@ -1,0 +1,152 @@
+package controllers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/canonical/k8s/pkg/log"
+	"github.com/canonical/k8s/pkg/snap"
+	snaputil "github.com/canonical/k8s/pkg/snap/util"
+	"github.com/canonical/k8s/pkg/utils"
+)
+
+// ServiceArgsControllerOpts holds configuration for ServiceArgsController.
+type ServiceArgsControllerOpts struct {
+	// Snap is the snap interface.
+	Snap snap.Snap
+	// Services is an optional override for the list of service names to watch.
+	// When nil, the list is determined dynamically each reconcile cycle based on
+	// whether the node is a worker or control-plane.
+	Services []string
+	// TriggerCh drives the reconciliation loop. Typically time.NewTicker(<interval>).C.
+	TriggerCh <-chan time.Time
+	// GetRunningArgs returns the parsed command-line arguments of the running process
+	// for the named service. Returns nil, nil when the process is not running.
+	// Defaults to a systemd-based implementation when nil.
+	GetRunningArgs func(ctx context.Context, serviceName string) (map[string]string, error)
+}
+
+// ServiceArgsController periodically compares each service's args file against the
+// arguments of the running process and restarts any service whose arguments have drifted.
+type ServiceArgsController struct {
+	snap           snap.Snap
+	services       []string
+	triggerCh      <-chan time.Time
+	reconciledCh   chan struct{}
+	getRunningArgs func(ctx context.Context, serviceName string) (map[string]string, error)
+}
+
+// NewServiceArgsController creates a new ServiceArgsController.
+func NewServiceArgsController(opts ServiceArgsControllerOpts) *ServiceArgsController {
+	if opts.GetRunningArgs == nil {
+		opts.GetRunningArgs = utils.RunningServiceArgs
+	}
+	return &ServiceArgsController{
+		snap:           opts.Snap,
+		services:       opts.Services,
+		triggerCh:      opts.TriggerCh,
+		reconciledCh:   make(chan struct{}, 1),
+		getRunningArgs: opts.GetRunningArgs,
+	}
+}
+
+// Run starts the controller and blocks until ctx is cancelled.
+func (c *ServiceArgsController) Run(ctx context.Context) {
+	ctx = log.NewContext(ctx, log.FromContext(ctx).WithValues("controller", "service-args"))
+	log := log.FromContext(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-c.triggerCh:
+		}
+
+		log.Info("checking service arguments for drift")
+		if err := c.reconcile(ctx); err != nil {
+			log.Error(err, "failed to reconcile service arguments")
+		}
+
+		select {
+		case c.reconciledCh <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (c *ServiceArgsController) reconcile(ctx context.Context) error {
+	log := log.FromContext(ctx)
+
+	services := c.services
+	if services == nil {
+		isWorker, err := snaputil.IsWorker(c.snap)
+		if err != nil {
+			return fmt.Errorf("failed to determine node type: %w", err)
+		}
+		if isWorker {
+			services = snaputil.WorkerServices()
+		} else {
+			services = snaputil.ControlPlaneServices()
+		}
+	}
+
+	for _, svc := range services {
+		differs, err := c.serviceArgsDiffer(ctx, svc)
+		if err != nil {
+			log.Error(err, "failed to compare arguments", "service", svc)
+			continue
+		}
+		if !differs {
+			continue
+		}
+
+		log.Info("service arguments have drifted from args file, restarting", "service", svc)
+		if err := c.snap.RestartServices(ctx, []string{svc}); err != nil {
+			log.Error(err, "failed to restart service", "service", svc)
+		}
+	}
+
+	return nil
+}
+
+// serviceArgsDiffer returns true if the desired args (from the args file) differ
+// from the arguments the running service was started with.
+// Returns false if the args file does not exist or the process is not running.
+func (c *ServiceArgsController) serviceArgsDiffer(ctx context.Context, serviceName string) (bool, error) {
+	argsFilePath := filepath.Join(c.snap.ServiceArgumentsDir(), serviceName)
+	desiredArgs, err := utils.ParseArgumentFile(argsFilePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to read args file for %q: %w", serviceName, err)
+	}
+
+	actualArgs, err := c.getRunningArgs(ctx, serviceName)
+	if err != nil {
+		if errors.Is(err, utils.ErrUnitNotRunning) {
+			// service is not running, args don't differ technically
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to get running args for %q: %w", serviceName, err)
+	}
+
+	if len(desiredArgs) != len(actualArgs) {
+		return true, nil
+	}
+	for key, desiredVal := range desiredArgs {
+		if actualArgs[key] != desiredVal {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// ReconciledCh returns the channel that receives a value after each reconciliation loop.
+func (c *ServiceArgsController) ReconciledCh() <-chan struct{} {
+	return c.reconciledCh
+}

src/k8s/pkg/k8sd/controllers/service_args_test.go
@@ -1,0 +1,255 @@
+package controllers_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/canonical/k8s/pkg/k8sd/controllers"
+	"github.com/canonical/k8s/pkg/snap/mock"
+	"github.com/canonical/k8s/pkg/utils"
+	. "github.com/onsi/gomega"
+)
+
+func TestServiceArgsController(t *testing.T) {
+	newCtrl := func(s *mock.Snap, services []string, getRunningArgs func(context.Context, string) (map[string]string, error)) (*controllers.ServiceArgsController, chan time.Time) {
+		triggerCh := make(chan time.Time)
+		ctrl := controllers.NewServiceArgsController(controllers.ServiceArgsControllerOpts{
+			Snap:           s,
+			Services:       services,
+			TriggerCh:      triggerCh,
+			GetRunningArgs: getRunningArgs,
+		})
+		return ctrl, triggerCh
+	}
+
+	t.Run("NoRestartWhenArgsMatch", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("RestartsWhenArgValueDiffers", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"new-val\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "old-val"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("RestartsWhenArgMissingFromRunningProcess", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{}, nil // process has no args
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("RestartsWhenRunningProcessHasExtraArg", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		// args file has only --foo; running process still has stale --removed arg
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar", "--removed": "old"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("kubelet"))
+	})
+
+	t.Run("NoRestartWhenProcessNotRunning", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		g.Expect(os.WriteFile(filepath.Join(dir, "kubelet"), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return nil, utils.ErrUnitNotRunning
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("NoRestartWhenArgsFileMissing", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: t.TempDir()}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet"}, func(_ context.Context, _ string) (map[string]string, error) {
+			return map[string]string{"--foo": "bar"}, nil
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(BeEmpty())
+	})
+
+	t.Run("ContinuesAfterGetRunningArgsError", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		for _, svc := range []string{"kubelet", "containerd"} {
+			g.Expect(os.WriteFile(filepath.Join(dir, svc), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+		}
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet", "containerd"}, func(_ context.Context, svc string) (map[string]string, error) {
+			if svc == "kubelet" {
+				return nil, fmt.Errorf("proc read error")
+			}
+			return map[string]string{"--foo": "old-val"}, nil // differs → should restart
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		// kubelet error was swallowed, containerd should be restarted
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("containerd"))
+	})
+
+	t.Run("OnlyDriftingServicesRestarted", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		dir := t.TempDir()
+		for _, svc := range []string{"kubelet", "containerd"} {
+			g.Expect(os.WriteFile(filepath.Join(dir, svc), []byte("--foo=\"bar\"\n"), 0o600)).To(Succeed())
+		}
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: dir}}
+		ctrl, triggerCh := newCtrl(s, []string{"kubelet", "containerd"}, func(_ context.Context, svc string) (map[string]string, error) {
+			if svc == "kubelet" {
+				return map[string]string{"--foo": "bar"}, nil // matches
+			}
+			return map[string]string{"--foo": "old-val"}, nil // drifted
+		})
+		go ctrl.Run(ctx)
+
+		triggerCh <- time.Now()
+		select {
+		case <-ctrl.ReconciledCh():
+		case <-time.After(channelSendTimeout):
+			g.Fail("timed out waiting for reconciliation")
+		}
+
+		g.Expect(s.RestartServicesCalledWith).To(HaveLen(1))
+		g.Expect(s.RestartServicesCalledWith[0]).To(ConsistOf("containerd"))
+	})
+
+	t.Run("StopsOnContextCancellation", func(t *testing.T) {
+		g := NewWithT(t)
+		ctx, cancel := context.WithCancel(context.Background())
+
+		s := &mock.Snap{Mock: mock.Mock{ServiceArgumentsDir: t.TempDir()}}
+		ctrl, _ := newCtrl(s, nil, nil)
+
+		done := make(chan struct{})
+		go func() {
+			ctrl.Run(ctx)
+			close(done)
+		}()
+
+		cancel()
+		select {
+		case <-done:
+		case <-time.After(channelSendTimeout):
+			g.Fail("controller did not stop after context cancellation")
+		}
+	})
+}