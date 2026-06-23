87732ef5d00f58634b76613aa3173663c519d4af
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2026-06-23 17:34:32 +0400

    fix: tests
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- tests/integration/tests/test_annotations.py
+++ tests/integration/tests/test_annotations.py
@@ -114,7 +114,7 @@ def test_disable_separate_feature_upgrades(
 
     start_branch = util.previous_track(config.SNAP)
     # install previous track on the cluster node
-    cluster_node.exec(f"snap install k8s --classic --channel={start_branch}".split())
+    util.setup_k8s_snap(cluster_node, start_branch)
     # install the current snap on the joining cp
     util.setup_k8s_snap(joining_cp)
 
@@ -163,7 +163,8 @@ def test_disable_separate_feature_upgrades(
 
     # Refresh first node, no upgrade CRD should be created.
     util.setup_k8s_snap(cluster_node, config.SNAP)
-    cluster_node.exec("k8s set gateway.enabled=false".split())
+    util.wait_until_k8s_ready(cluster_node, instances)
+    cluster_node.exec("k8s set gateway.enabled=true".split())
     util.wait_until_k8s_ready(cluster_node, instances)
 
     upgrades = json.loads(
