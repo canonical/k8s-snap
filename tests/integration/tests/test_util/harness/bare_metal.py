3f67cebc264e13f2a821b87d3e41c4bb55932d6f
ethandcosta <ethan.dcosta@canonical.com>
2026-08-19 15:12:10 -0400

    remove circular import

--- tests/integration/tests/test_util/harness/bare_metal.py
+++ tests/integration/tests/test_util/harness/bare_metal.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import List
 
 from test_util import config
-from test_util.harness import Harness, HarnessError, Instance
+from test_util.harness.base import Harness, HarnessError, Instance
 from test_util.util import run
 
 LOG = logging.getLogger(__name__)
